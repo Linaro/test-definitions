a846c5215a8dec0398b422f97562eb591c5a03e2
Senthil Kumaran <senthil.kumaran@linaro.org>
2013-01-23 18:40:06 +0530

    Add binder test definitions.

---
+++ android/scripts/binder.sh
@@ -0,0 +1,30 @@
+#!/system/bin/sh
+#
+# Binder test.
+#
+# Copyright (C) 2012, Linaro Limited.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# Author: Amit Pundir <amit.pundir@linaro.org>
+#
+
+chmod 777 /data/nativebenchmark/binderAddInts
+/data/nativebenchmark/binderAddInts -n 10
+if [ $? -eq 0 ]; then
+	echo "binder=pass"
+else
+	echo "binder=fail"
+fi
