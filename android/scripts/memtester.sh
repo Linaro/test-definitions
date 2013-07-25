c30a43f0a3d4568395655c00850d322aa6557f05
Senthil Kumaran <senthil.kumaran@linaro.org>
2013-07-25 19:42:08 +0530

    Add memtester test definition to android.

---
+++ android/scripts/memtester.sh
@@ -0,0 +1,28 @@
+#!/system/bin/sh
+#
+# memtester test
+#
+# Copyright (C) 2013, Linaro Limited.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51   Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+# USA.
+#
+# owner: harigopal.gollamudi@linaro.org
+#
+##############################################################################
+
+memtester 1M 1 | sed -n '/ ok/p' | sed -e 's/ok/pass/g' &> stdout.log
+cat stdout.log
+rm -rf stdout.log
