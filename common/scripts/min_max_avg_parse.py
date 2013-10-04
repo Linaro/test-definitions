1ef4f1dd009d980736ae901e45ae03f860109236
Anders Roxell <anders.roxell@linaro.org>
2013-10-04 16:42:36 +0200

    added lmbench and generalize hackbench_parse script
    1. move hackbench_parse.py to common/scripts/min_max_avg_parsh.sh
       and try to generalize it so lmbench can use it as well.
    2. add new lmbench.yaml file to OE
    
    Change-Id: I9ce50fb5830d2f1a6f45ca043f93da70f32041b1
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

---
+++ common/scripts/min_max_avg_parse.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import re
+import sys
+from numpy import *
+
+values = []
+
+r = re.compile(sys.argv[2]+"\s+(?P<measurement>[0-9.])")
+f = open(sys.argv[1], "r")
+for line in f.readlines():
+    search = r.search(line)
+    if search:
+        values.append(float(search.group('measurement')))
+
+if "hackbench" in sys.argv[1]:
+    # Usually the first value is inexplicably high
+    values.pop(0)
+
+try:
+    sys.argv[4]
+except IndexError:
+    cmd_options = ""
+else:
+    cmd_options = sys.argv[4]
+
+np_array = array(values)
+
+format = "%-16s%-16s%-16s%-16s"
+print format % (sys.argv[1].split('.', 1)[0]+cmd_options+"_min:", str(min(np_array)),    sys.argv[3], "pass")
+print format % (sys.argv[1].split('.', 1)[0]+cmd_options+"_max:", str(max(np_array)),    sys.argv[3], "pass")
+print format % (sys.argv[1].split('.', 1)[0]+cmd_options+"_avg:", str(mean(np_array)),   sys.argv[3], "pass")
+print format % (sys.argv[1].split('.', 1)[0]+cmd_options+"_mdn:", str(median(np_array)), sys.argv[3], "pass")
