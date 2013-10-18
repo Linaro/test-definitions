e6c97cf9bdc162070d9b81aa6e73b4b38f754e1e
Anders Roxell <anders.roxell@linaro.org>
2013-10-18 13:53:03 +0200

    added lmbench to kvm-benchmark.yaml test for OE
    Removed kvm-lava.conf, create this file in test-kvm.sh because a lot is
    the same between kvm.yaml and kvm-benchmark.yaml
    Using common/scripts/lmbench.sh to run inside the guest
    
    Change-Id: Icfb5ae1ebe46400f018cd591aa4ba97aed56a044
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- common/scripts/kvm/test-kvm.sh
+++ common/scripts/kvm/test-kvm.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-dmesg|grep 'Hyp mode initialized successfully' && echo kvm-init-1: pass || echo kvm-init-1: fail
+KVM_HOST_NET="kvm-host-net-1:"
+KVM_GUEST_NET="kvm-guest-net-1:"
+KVM_INIT="kvm-init-1:"
+KVM_BOOT="kvm-boot-1:"
+if [ $1 = "benchmark" ]; then
+    KVM_HOST_NET="$KVM_HOST_NET 0 none"
+    KVM_GUEST_NET="$KVM_GUEST_NET 0 none"
+    KVM_INIT="$KVM_INIT 0 none"
+    KVM_BOOT="$KVM_BOOT 0 none"
+fi
+
+dmesg|grep 'Hyp mode initialized successfully' && echo "$KVM_INIT pass" || echo "$KVM_INIT fail"
 
 wget --no-check-certificate http://snapshots.linaro.org/kernel-hwpack/linux-vexpress-kvm/linux-vexpress-kvm/kvm.qcow2.gz
 gunzip kvm.qcow2.gz
@@ -11,10 +22,31 @@ mount /dev/nbd0p2 /mnt/
 
 cp /mnt/boot/vmlinuz-*-linaro-vexpress ./zImage
 cp /mnt/lib/firmware/*-linaro-vexpress/device-tree/vexpress-v2p-ca15-tc1.dtb .
-cp common/scripts/kvm/kvm-lava.conf  /mnt/etc/init/kvm-lava.conf
-cp /usr/bin/hackbench /mnt/usr/bin/hackbench
-cp common/scripts/kvm/test-rt-tests.sh /mnt/root/test-rt-tests.sh
-chmod 777 /mnt/root/test-rt-tests.sh
+# Build up file kvm-lava.conf
+cat >> /mnt/etc/init/kvm-lava.conf <<EOF
+start on runlevel [23]
+stop on runlevel [!23]
+console output
+script
+    echo "$KVM_BOOT pass"
+    ping -W 4 -c 10 192.168.1.10 && echo "$KVM_GUEST_NET pass" || echo "$KVM_GUEST_NET fail"
+EOF
+if [ $1 = "benchmark" ]; then
+    cp /usr/bin/lat_ctx /mnt/usr/bin/lat_ctx
+    cp common/scripts/lmbench.sh /mnt/root/lmbench.sh
+    echo "    echo 'Test lmbench on guest'" >>/mnt/etc/init/kvm-lava.conf
+    echo "    sh /root/lmbench.sh" >>/mnt/etc/init/kvm-lava.conf
+else
+    cp /usr/bin/hackbench /mnt/usr/bin/hackbench
+    cp common/scripts/kvm/test-rt-tests.sh /mnt/root/test-rt-tests.sh
+    echo "    echo 'Test hackbench on guest'" >>/mnt/etc/init/kvm-lava.conf
+    echo "    sh /root/test-rt-tests.sh" >>/mnt/etc/init/kvm-lava.conf
+fi
+
+echo "    poweroff" >>/mnt/etc/init/kvm-lava.conf
+echo "end script" >>/mnt/etc/init/kvm-lava.conf
+
+
 umount /mnt
 sync
 qemu-nbd -d /dev/nbd0
@@ -27,7 +59,8 @@ ifconfig tap0 0.0.0.0 up
 brctl addif br0 eth0
 brctl addif br0 tap0
 udhcpc -t 10 -i br0
-ping -W 4 -c 10 192.168.1.10 && echo "kvm-host-net-1: pass" || echo "kvm-host-net-1: fail"
+
+ping -W 4 -c 10 192.168.1.10 && echo "$KVM_HOST_NET pass" || echo "$KVM_HOST_NET fail"
 
 qemu-system-arm -smp 2 -m 1024 -cpu cortex-a15 -M vexpress-a15 \
 	-kernel ./zImage -dtb ./vexpress-v2p-ca15-tc1.dtb \
@@ -40,5 +73,5 @@ qemu-system-arm -smp 2 -m 1024 -cpu cortex-a15 -M vexpress-a15 \
 
 if ! grep -q "kvm-boot-1:" kvm-log.txt
 then
-    echo "kvm-boot-1: fail"
+    echo "$KVM_BOOT fail"
 fi
