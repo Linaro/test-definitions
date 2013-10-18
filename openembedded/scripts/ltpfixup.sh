2132db61391a194ce1ba68741b8db9a5365943c8
mike-holmes <mike.holmes@linaro.org>
2013-10-18 12:58:29 -0400

    openembedded/ltp: add additional tests (fs,hugetlb)
    Change-Id: Ifbaac94607da78bc740025f908bf1696d3cde08a

--- openembedded/scripts/ltpfixup.sh
+++ openembedded/scripts/ltpfixup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /opt/ltp
-./runltp -p -q -f syscalls,mm,math,timers,fcntl-locktests,ipc,fsx
+./runltp -p -q -f syscalls,mm,math,timers,fcntl-locktests,ipc,fsx,fs,hugetlb
 find ./results -name "LTP_RUN_ON*" -print0 |xargs -0 cat
 exit 0
