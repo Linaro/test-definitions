7f3be206af198cebee10720bc59d7392164cdedf
Anders Roxell <anders.roxell@linaro.org>
2013-10-18 13:53:03 +0200

    added lmbench to kvm-benchmark.yaml test for OE
    simplified kvm-lava.conf, and construct tests in test-guest.sh
    to be called from both kvm.yaml and kvm-benchmark.yaml
    Using common/scripts/lmbench.sh to run inside the guest
    
    parse_lmbench.sh: had to add --text to the grep command because
    kvm-log.txt becomes in data and not text format.
    
    Modifications by Riku: keep kvm-lava.conf around
    
    Change-Id: Icfb5ae1ebe46400f018cd591aa4ba97aed56a044
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>
    Signed-off-by: Riku Voipio <riku.voipio@linaro.org>

--- common/scripts/parse_lmbench.sh
+++ common/scripts/parse_lmbench.sh
@@ -3,8 +3,8 @@
 LOG_FILE=$1
 OUTPUT_FILE=$3
 TMP_FILE="$1.$$"
-BEGIN=$(grep -n "^BEGIN LMBENCH" $LOG_FILE | awk -F ':' '{print $1}')
-END=$(grep -n "^END LMBENCH" $LOG_FILE | awk -F ':' '{print $1}')
+BEGIN=$(grep --text -n "^BEGIN LMBENCH" $LOG_FILE | awk -F ':' '{print $1}')
+END=$(grep --text -n "^END LMBENCH" $LOG_FILE | awk -F ':' '{print $1}')
 BEGINNING=$(expr $(expr $END - $BEGIN) + 1)
 head -n $END $LOG_FILE | tail -n $BEGINNING >$TMP_FILE
 
