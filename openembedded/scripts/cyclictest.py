7cb7aab755655d77e872e1b8700fd522c8916e59
Vincent Hsu <vincent.hsu@linaro.org>
2013-11-12 11:25:43 +0800

    openembedded/cyclictest: get lantency bounds form lava job parameters
    Change-Id: Id4ba8fd7f9c234f4b4d0fc3347665354f4d9232d
    Signed-off-by: Vincent Hsu <vincent.hsu@linaro.org>

--- openembedded/scripts/cyclictest.py
+++ openembedded/scripts/cyclictest.py
@@ -3,26 +3,23 @@ import re
 import sys
 import os
 
-max_threshold = 15000
-avg_threshold = 20
-min_threshold = 10
+max_threshold = int(sys.argv[1])
+avg_threshold = int(sys.argv[2])
 pass_max_threshold = True
 pass_avg_threshold = True
-pass_min_threshold = True
 max_lentency = 0
 avg_lentency = 0
-min_lentency = 0
 
-#parse format:
+# parse format:
 # T:49 ( 4518) P:31 I:4243600 C:      2 Min:      8 Act:    8 Avg:    8 Max:       9
 parser = re.compile("(?P<T>\d+)\D+(?P<T1>\d+)\D+(?P<P>\d+)\D+(?P<I>\d+)\D+(?P<C>\d+)\D+(?P<Min>\d+)\D+(?P<Act>\d+)\D+(?P<Avg>\d+)\D+(?P<Max>\d+)")
 
 data = sys.stdin.readlines()
 
 if len(data) == 0:
-    print "test_case_id:Test program running result:fail measurement:0 units:none"
+    print "Test program running fail"
 else:
-    print "test_case_id:Test program running result:pass measurement:0 units:none"
+    print "Test program running success"
 
     for line in data:
         result = parser.search(line)
@@ -33,29 +30,19 @@ else:
             if int(result.group('Avg')) > avg_threshold:
                 pass_avg_threshold = False
 
-            if int(result.group('Min')) > min_threshold:
-                pass_min_threshold = False
-
             if int(result.group('Max')) > max_lentency:
                 max_lentency = int(result.group('Max'))
 
             if int(result.group('Avg')) > avg_lentency:
                 avg_lentency = int(result.group('Avg'))
 
-            if int(result.group('Min')) > min_lentency:
-                min_lentency = int(result.group('Min'))
-
     if pass_max_threshold is True:
-        print "test_case_id:Max latency bound (<15ms) result:pass measurement:" + str(max_lentency) + " units:usecs"
+        print "test_case_id:Max latency bound (<" + str(max_threshold) + "us) result:pass measurement:" + str(max_lentency) + " units:usecs"
     else:
-        print "test_case_id:Max latency bound (<15ms) result:fail measurement:" + str(max_lentency) + " units:usecs"
+        print "test_case_id:Max latency bound (<" + str(max_threshold) + "us) result:fail measurement:" + str(max_lentency) + " units:usecs"
 
     if pass_avg_threshold is True:
-        print "test_case_id:Avg latency bound (<20ns) result:pass measurement:" + str(avg_lentency) + " units:usecs"
+        print "test_case_id:Avg latency bound (<" + str(avg_threshold) + "us) result:pass measurement:" + str(avg_lentency) + " units:usecs"
     else:
-        print "test_case_id:Avg latency bound (<20ns) result:fail measurement:" + str(avg_lentency) + " units:usecs"
+        print "test_case_id:Avg latency bound (<" + str(avg_threshold) + "us) result:fail measurement:" + str(avg_lentency) + " units:usecs"
 
-    if pass_min_threshold is True:
-        print "test_case_id:Min latency bound (<10ns) result:pass measurement:" + str(min_lentency) + " units:usecs"
-    else:
-        print "test_case_id:Min latency bound (<10ns) result:fail measurement:" + str(min_lentency) + " units:usecs"
