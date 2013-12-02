1c7ce8f8c44e711aacae6ad06b1d2cd20ce2eac3
Anders Roxell <anders.roxell@linaro.org>
2013-12-02 17:28:45 +0100

    OE/ltp: parameterize cmdfiles
    Change-Id: Ifd4dd6213ff17b30624601bdb56230a4f845864a
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- openembedded/scripts/ltpfixup.sh
+++ openembedded/scripts/ltpfixup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /opt/ltp
-./runltp -p -q -f syscalls,mm,math,timers,fcntl-locktests,ipc,fsx,fs,hugetlb
+./runltp -p -q -f $1
 find ./results -name "LTP_RUN_ON*" -print0 |xargs -0 cat
 exit 0
