a65a7a4ee8003ed03e5a2bfe5abacf0bcc0805f0
Anders Roxell <anders.roxell@linaro.org>
2014-02-21 12:38:46 +0100

    netperf-server: if netserver started pass else start it
    Change-Id: I2efc59ca64d8fb69d2ca93e300e8418c100fdd1b
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- common/scripts/netperf-server.sh
+++ common/scripts/netperf-server.sh
@@ -2,4 +2,12 @@
 
 set -x
 
-netserver && echo 'test_case_id:netserver units:none measurement:0 result:pass' || echo 'test_case_id:netserver units:none measurement:0 result:fail'
+PRINT_PASS='test_case_id:netserver units:none measurement:0 result:pass'
+PRINT_FAIL='test_case_id:netserver units:none measurement:0 result:fail'
+
+pgrep netserver
+if [ $? -eq 0 ]; then
+    echo ${PRINT_PASS}
+else
+    netserver && echo ${PRINT_PASS} || echo ${PRINT_FAIL}
+fi
