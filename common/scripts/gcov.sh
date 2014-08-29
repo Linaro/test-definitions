5d2b3f3ff6deb6b072b95991ad63debe383f5090
Riku Voipio <riku.voipio@linaro.org>
2014-08-29 10:54:52 +0300

    Add gcov definition
    Implements CARD-1488 , GCOV tests in lava.
    
    Change-Id: Ie1da545187a2d8bc7732176d7a756c793b77b800

---
+++ common/scripts/gcov.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+df -h
+
+echo -n "LAVA gcov-enabled: "
+[ -e /sys/kernel/debug/gcov/ ] && echo "pass" || echo "fail"
+
+echo -n "LAVA gcov-collecting: "
+kdir=`find /sys/kernel/debug/gcov/ -type d|grep kernel/gcov`
+[ -e $kdir/base.gcda ] && echo "pass" || echo "fail"
+
+BUILD_NUMBER=`wget -q --no-check-certificate -O - https://ci.linaro.org/jenkins/job/linux-gcov/hwpack=arndale,label=kernel_cloud/lastSuccessfulBuild/buildNumber`
+wget -nv http://snapshots.linaro.org/kernel-hwpack/linux-gcov-arndale/${BUILD_NUMBER}/gcov-arndale-rootfs.tar.gz -O -|tar xzf - --strip-components=1 -C /
+
+df -h
+
+./build/runltp
+
+lcov -c -o coverage.info && echo "LAVA gcov-read: pass" || echo "LAVA gcov-read: fail"
+
+genhtml coverage.info -o boot_and_ltp && echo "LAVA gcov-html: pass" || echo "LAVA gcov-html: fail"
+tar czf gcov-results.tar.gz boot_and_ltp
+
+mkdir /srv/scratch
+mount -t nfs hackbox:/nfs/scratch/ /srv/scratch
+cp gcov-results.tar.gz /srv/scratch/
+sync
+umount /srv/scratch
