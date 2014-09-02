5f73dfdf4bd11555453d2de055f808e3e3f38797
Botao Sun <botao.sun@linaro.org>
2014-09-02 21:59:42 +1000

    Add SATA Test Phase II for Linux Linaro ubuntu
    Test code has been validated locally on my Host PC with a Seagate SATA drive.
    
    Signed-off by: Botao Sun <botao.sun@linaro.org>
    
    Change-Id: I872e29fe2f239f6aa61d9a113763513c5aa1983c

--- ubuntu/scripts/sata-partition-rw.py
+++ ubuntu/scripts/sata-partition-rw.py
@@ -22,6 +22,7 @@
 #
 import sys
 import os
+import time
 import commands
 
 # Switch to home path of current user to avoid any permission issue
@@ -210,6 +211,189 @@ def sata_ext4_format():
         else:
             print testcase_name + ": FAIL" + " 0" + " Inapplicable" + " - Could not find ext4 partition on " + device_name
 
+def sata_ext4_mount_umount():
+    testcase_mount = "sata_ext4_mount"
+    testcase_umount = "sata_ext4_umount"
+    target_partition_name = device_name + "2"
+    mount_point = "/media"
+    ext4_mount = "sudo mount " + target_partition_name + " " + mount_point
+    ext4_umount = "sudo umount " + mount_point
+
+    # umount everything from mount point - cleaning
+    umount_clean = "sudo umount " + mount_point
+    commands.getstatusoutput(umount_clean)
+    time.sleep(5)
+    print "mount point cleaned!"
+
+    # Test mount command
+    ext4_mount_return = commands.getstatusoutput(ext4_mount)
+    time.sleep(5)
+    if ext4_mount_return[0] != 0:
+        print testcase_mount + ": FAIL" + " 0" + " Inapplicable" + " Failures on ext4 partition mount!"
+    else:
+        commands.getstatusoutput("sudo mount > mount_log.txt 2>&1")
+        logfile = open("mount_log.txt", "r")
+        logcontent = logfile.readlines()
+        positive_counter = 0
+        for i in range(0, len(logcontent)):
+            if target_partition_name in logcontent[i]:
+                positive_counter = positive_counter + 1
+        logfile.close()
+        if positive_counter > 0:
+            print testcase_mount + ": PASS" + " 0" + " Inapplicable"
+        else:
+            print testcase_mount + ": FAIL" + " 0" + " Inapplicable" + " - Could not find ext4 partition in mount log!"
+
+    # Test umount command
+    ext4_umount_return = commands.getstatusoutput(ext4_umount)
+    time.sleep(5)
+    if ext4_umount_return[0] != 0:
+        print testcase_umount + ": FAIL" + " 0" + " Inapplicable" + " Failures on ext4 partition umount!"
+    else:
+        commands.getstatusoutput("sudo mount > umount_log.txt 2>&1")
+        logfile = open("umount_log.txt", "r")
+        logcontent = logfile.readlines()
+        positive_counter = 0
+        for i in range(0, len(logcontent)):
+            if target_partition_name in logcontent[i]:
+                positive_counter = positive_counter + 1
+        logfile.close()
+        if positive_counter > 0:
+            print testcase_umount + ": FAIL" + " 0" + " Inapplicable" + " - ext4 partition umount failed from " + mount_point
+        else:
+            print testcase_umount + ": PASS" + " 0" + " Inapplicable"
+
+def sata_ext4_file_fill_edit():
+    testcase_filefill = "sata_ext4_file_fill"
+    testcase_fileedit = "sata_ext4_file_edit"
+    target_partition_name = device_name + "2"
+    mount_point = "/media"
+    ext4_mount = "sudo mount " + target_partition_name + " " + mount_point
+    ext4_umount = "sudo umount " + mount_point
+
+    # umount everything from mount point - cleaning
+    umount_clean = "sudo umount " + mount_point
+    commands.getstatusoutput(umount_clean)
+    time.sleep(5)
+    print "mount point cleaned!"
+    commands.getstatusoutput(ext4_mount)
+    time.sleep(5)
+
+    # Create a 1MB file with 0x00 filled in
+    file_creation_1M = "sudo dd if=/dev/zero of=/media/file_1MB bs=4k count=256"
+    file_creation_1M_return = commands.getstatusoutput(file_creation_1M)
+    if file_creation_1M_return[0] != 0:
+        print testcase_filefill + ": FAIL" + " 0" + " Inapplicable" + " failed to create a 1MB file filled with 0x00!"
+    elif os.path.isfile("/media/file_1MB") == False:
+        print testcase_filefill + ": FAIL" + " 0" + " Inapplicable" + " file_1MB can not be found!"
+    elif os.path.getsize("/media/file_1MB") != 1048576:
+        print testcase_filefill + ": FAIL" + " 0" + " Inapplicable" + " file_1MB size incorrect!"
+    else:
+        print testcase_filefill + ": PASS" + " 0" + " Inapplicable"
+
+    # Create an empty file then write something into it
+    testfile_name = "/media/test_file.txt"
+    if os.path.isfile(testfile_name) == True:
+        os.system("sudo rm -rf " + testfile_name)
+    file_creation_empty = "sudo touch " + testfile_name
+    file_creation_empty_return = commands.getstatusoutput(file_creation_empty)
+    if file_creation_empty_return[0] != 0:
+        print testcase_fileedit + ": FAIL" + " 0" + " Inapplicable" + " failed to create an empty file!"
+    elif os.path.isfile(testfile_name) == False:
+        print testcase_fileedit + ": FAIL" + " 0" + " Inapplicable" + " " + testfile_name + " can not be found!"
+    else:
+        os.system("sudo chmod 777 " + testfile_name)
+        test_string = "This is a test file"
+        testfile = open(testfile_name, "a")
+        testfile.write(test_string)
+        testfile.close()
+        # Validating
+        testfile = open(testfile_name, "r")
+        return_string = testfile.read()
+        if return_string != test_string:
+            print testcase_fileedit + ": FAIL" + " 0" + " Inapplicable" + " file content doesn't match!"
+        else:
+            print testcase_fileedit + ": PASS" + " 0" + " Inapplicable"
+        testfile.close()
+
+    # umount everything from mount point - cleaning
+    umount_clean = "sudo umount " + mount_point
+    commands.getstatusoutput(umount_clean)
+    time.sleep(5)
+    print "mount point cleaned!"
+
+def sata_ext4_dd_write_read():
+    testcase_dd_write = "sata_ext4_dd_write"
+    testcase_dd_read = "sata_ext4_dd_read"
+    target_partition_name = device_name + "2"
+    mount_point = "/media"
+    ext4_mount = "sudo mount " + target_partition_name + " " + mount_point
+    ext4_umount = "sudo umount " + mount_point
+
+    # umount everything from mount point - cleaning
+    umount_clean = "sudo umount " + mount_point
+    commands.getstatusoutput(umount_clean)
+    time.sleep(5)
+    print "mount point cleaned!"
+    commands.getstatusoutput(ext4_mount)
+    time.sleep(5)
+
+    # Test write speed by using dd, direct write
+    dd_write_1G = "sudo dd if=/dev/zero of=/media/file_1GB oflag=direct bs=4k count=262144 > dd_write_1GB_stdout.txt 2>&1"
+    dd_write_1G_return = commands.getstatusoutput(dd_write_1G)
+    if dd_write_1G_return[0] != 0:
+        print testcase_dd_write + ": FAIL" + " 0" + " Inapplicable" + " failed to create a 1GB file using dd!"
+    elif os.path.isfile("/media/file_1GB") == False:
+        print testcase_dd_write + ": FAIL" + " 0" + " Inapplicable" + " file_1GB can not be found!"
+    elif os.path.getsize("/media/file_1GB") != 1073741824:
+        print testcase_dd_write + ": FAIL" + " 0" + " Inapplicable" + " file_1GB size incorrect!"
+    else:
+        target_list = []
+        writelog = open("dd_write_1GB_stdout.txt", "r")
+        writelog_content = writelog.readlines()
+        for item in writelog_content:
+            if str(1073741824) in item:
+                target_list = item.split(",")[-1].strip("\n").split(" ")
+                break
+        if target_list == []:
+            print testcase_dd_write + ": FAIL" + " 0" + " Inapplicable" + " can not find write performance result."
+        elif len(target_list) < 2:
+            print testcase_dd_write + ": FAIL" + " 0" + " Inapplicable" + " write test result parsing failed, please check the log output!"
+        else:
+            print testcase_dd_write + ": PASS" + " " + str(target_list[-2]) + " " + target_list[-1]
+        writelog.close()
+
+    # Test read speed by using dd, direct read
+    if os.path.isfile("/media/file_1GB") == False:
+        print testcase_dd_read + ": FAIL" + " 0" + " Inapplicable" + " can not find the target file to read."
+    else:
+        dd_read_1G = "sudo dd if=/media/file_1GB iflag=direct of=/dev/null bs=4k count=262144 > dd_read_1GB_stdout.txt 2>&1"
+        dd_read_1G_return = commands.getstatusoutput(dd_read_1G)
+        if dd_read_1G_return[0] != 0:
+            print testcase_dd_read + ": FAIL" + " 0" + " Inapplicable" + " failed to read the target file!"
+        else:
+            target_list = []
+            readlog = open("dd_read_1GB_stdout.txt", "r")
+            readlog_content = readlog.readlines()
+            for item in readlog_content:
+                if str(1073741824) in item:
+                    target_list = item.split(",")[-1].strip("\n").split(" ")
+                    break
+            if target_list == []:
+                print testcase_dd_read + ": FAIL" + " 0" + " Inapplicable" + " can not find read performance result."
+            elif len(target_list) < 2:
+                print testcase_dd_read + ": FAIL" + " 0" + " Inapplicable" + " read test result parsing failed, please check the log output!"
+            else:
+                print testcase_dd_read + ": PASS" + " " + str(target_list[-2]) + " " + target_list[-1]
+            readlog.close()
+
+    # umount and clean
+    os.system("sudo rm -rf /media/file_1GB")
+    umount_clean = "sudo umount " + mount_point
+    commands.getstatusoutput(umount_clean)
+    time.sleep(5)
+    print "mount point cleaned!"
+
 # Run all test
 sata_device_existence()
 sata_mklabel_msdos()
@@ -218,3 +402,6 @@ sata_first_ext2_partition()
 sata_second_ext2_partition()
 sata_ext3_format()
 sata_ext4_format()
+sata_ext4_mount_umount()
+sata_ext4_file_fill_edit()
+sata_ext4_dd_write_read()
