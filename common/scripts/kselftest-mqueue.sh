e90ce6ca483f7b955f9e4bad38f51d856aa38bd0
Arthur She <arthur.she@linaro.org>
2014-10-05 09:47:25 -0700

    kselftest: Add in kernel selftests
    Add cpu-hotplug, efivarfs, memory-hotplug, mqueue, net, ptrace, vm
    tests for both openembedded and ubuntu
    
    Change-Id: Ia0b77e6e287b66b07db295a6c9efcf56027cb969

---
+++ common/scripts/kselftest-mqueue.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+BASEDIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
+LAVA_ROOT="${BASEDIR}/../../"
+TEST_DIR="${LAVA_ROOT}/kselftest/mqueue"
+
+#### Test mq_open_tests ########
+echo -e "\nTest mq_open_tests\n"
+cd ${TEST_DIR}
+gcc -O2 mq_open_tests.c -o mq_open_tests -lrt
+./mq_open_tests /test1 || echo "mq_open_tests: FAIL"
+
+#### Test mq_perf_tests ########
+echo -e "\nTest mq_perf_tests\n"
+#   Build libpopt
+cd ${LAVA_ROOT}
+wget http://rpm5.org/files/popt/popt-1.16.tar.gz -O - | tar zxf -
+cd popt-1.16
+#   Due to the config.guess doesn't support aarch64 yet. We have to specify system type
+[ `uname -m` = "aarch64" ] && BUILD="--build=aarch64-unknown-linux-gnu"
+./configure ${BUILD} --prefix=/usr
+make install
+cd ${TEST_DIR}
+gcc -O2 -o mq_perf_tests mq_perf_tests.c -lrt -lpthread -lpopt
+./mq_perf_tests || echo "mq_perf_tests: FAIL"
+
---
+++ common/scripts/kselftest-runner.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+TEST_NAME=$1
+COMMAND=$(basename $2)
+DIR=$(dirname $2)
+LOG="result.log";
+
+cd ${DIR}
+(/bin/bash ${COMMAND} 2>&1 || echo "${TEST_NAME}: [FAIL]") | tee ${LOG}
+if [ -n "`grep \"skip\" ${LOG}`" ]; then
+    echo "${TEST_NAME}: [SKIP]";
+elif [ -z "`grep \"SKIP\|FAIL\" ${LOG}`" ]; then
+    echo "${TEST_NAME}: [PASS]"
+fi
+
+while read l;
+do
+    [ -n "`echo $l|grep 'running'`" ] && test="`echo $l|sed 's/running //'`"
+    [ -n "`echo $l|grep \"\[PASS\|FAIL\|SKIP\"`" ] && result=$l
+    [ "${test}" -a "${result}" ] && echo "${test}: ${result}" && unset test && unset result
+done < ${LOG}
+
