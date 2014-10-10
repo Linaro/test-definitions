63db3bb525fb958002c654eebdbdc84d10a9afa8
Chase Qi <chase.qi@linaro.org>
2014-10-10 18:21:04 +0800

    Add Ubuntu multiple network interfaces test case
    YAML and script files validated in LAVA
    
    Signed-off by: Chase Qi <chase.qi@linaro.org>
    
    Change-Id: I1600ee836f304579d5d7c06c1caeec8b4c09632c

---
+++ ubuntu/scripts/multiple-network-interfaces-test.sh
@@ -0,0 +1,140 @@
+#!/bin/sh
+#
+# Multiple network interfaces test for ubuntu
+#
+# Copyright (C) 2013, Linaro Limited.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# Author: Chase Qi <chase.qi@linaro.org>
+
+GATEWAY=10.0.0.1
+
+# Display all interfaces currently available
+echo "All interfaces currently available"
+echo "================================="
+ifconfig -a
+
+# Correction of ARP flux
+address_arp_flux(){
+echo "==============="
+echo "Address ARP flux"
+for i in all default $(ls /proc/sys/net/ipv4/conf/ | grep eth)
+do
+    echo 0 > /proc/sys/net/ipv4/conf/$i/rp_filter
+    echo "$i rp_filter: `cat /proc/sys/net/ipv4/conf/$i/rp_filter`"
+    echo 1 > /proc/sys/net/ipv4/conf/$i/arp_ignore
+    echo "$i arp_ignore: `cat /proc/sys/net/ipv4/conf/$i/arp_ignore`"
+    echo 2 > /proc/sys/net/ipv4/conf/$i/arp_announce
+    echo "$i arp_announce: `cat /proc/sys/net/ipv4/conf/$i/arp_announce`"
+done
+
+if [ $? -ne 0 ]
+then
+    echo "address-arp-flux:" "fail"
+    return 1
+else
+    echo "address-arp-flux:" "pass"
+fi
+
+}
+
+# Interface enable test
+interface_enable_test(){
+echo "========================="
+echo "$i interface enable test"
+ifconfig $i up
+
+if [ $? -ne 0 ]
+then
+    echo "$i-interface-enable-test:" "fail"
+    return 1
+else
+    echo "$i-interface-enable-test:" "pass"
+fi
+
+}
+
+# Link detect
+link_detect(){
+echo "===================="
+echo "$i link detect test"
+link=`cat /sys/class/net/$i/carrier`
+
+if [ $link -ne 1 ]
+then
+    echo "Please check $i LAN cable"
+    echo "$i-link-detect:" "fail"
+    return 1
+else
+    echo "Link detected: yes"
+    echo "$i-link-detect:" "pass"
+fi
+
+}
+
+# IP not empty test
+ip_not_empty(){
+echo "====================="
+echo "$i-ip-not-empty test"
+dhclient $i
+IP=$(ifconfig $i | grep "inet addr" | awk '{print $2}')
+
+if [ -z $IP ]
+then
+    echo "$i have no IP address"
+    echo "$i-ip-not-empty:" "fail"
+    return 1
+else
+    echo "$i IP $IP"
+    echo "$i-ip-not-empty:" "pass"
+fi
+
+}
+
+# ping test
+ping_test(){
+echo "============="
+echo "$i ping test"
+ping -c 5 -I $i $GATEWAY
+
+if [ $? -ne 0 ]
+then
+    echo "Ping test through $i failed"
+    echo "$i-ping-test:" "fail"
+    return 1
+else
+    echo "$i-ping-test:" "pass"
+fi
+
+}
+
+# Run the tests
+address_arp_flux
+for i in $(ls /proc/sys/net/ipv4/conf/ | grep eth)
+do
+  if test "$i" = "eth0"
+  then
+      ping_test
+  else
+      interface_enable_test
+      link_detect
+      ip_not_empty
+      ping_test
+  fi
+done
+
+# clean exit so lava-test can trust the results
+exit 0
