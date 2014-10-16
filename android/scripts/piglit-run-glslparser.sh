63b4b8c1af4c701f0925c653859b5c3156f170ef
Botao Sun <botao.sun@linaro.org>
2014-10-17 03:33:56 +1100

    piglit-run-glslparser.sh, piglit-run-shader.sh: POSIX Compliant Update.
    This change is to make the test script to be POSIX compliant.
    
    Original patch comes from:
    
    Lucas Dutra Nunes <ldnunes@ossystems.com.br>
    
    Signed-off by: Botao Sun <botao.sun@linaro.org>
    
    Change-Id: Ib72d14d43de0a4ac93dabb0052bbb929cf9a8370

--- android/scripts/piglit-run-glslparser.sh
+++ android/scripts/piglit-run-glslparser.sh
@@ -1,9 +1,27 @@
 #!/system/bin/sh
+#
+# Copyright (C) 2010 - 2014, Linaro Limited.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# Maintainer: Botao Sun <botao.sun@linaro.org>
 
 # find and loop over the vert and frag tests found
 # looks recursively down the directory tree
 
-find ${2} -name "*.frag" -or -name "*.vert" -print0 | while read -d $'\0' file
+find ${2} -name "*.frag" -or -name "*.vert" | while read file
 do
    RESULTFOUND=$(grep expect_result ${file} )
    case $RESULTFOUND in
@@ -25,7 +43,7 @@ do
             echo "glslparser ${file}: pass";;
 
       *"$SSTRING"*) echo "glslparser ${file}: skip";;
-  
+
       *"$FSTRING"*)
             echo "glslparser ${file}: fail";;
 
--- android/scripts/piglit-run-shader.sh
+++ android/scripts/piglit-run-shader.sh
@@ -1,9 +1,26 @@
 #!/system/bin/sh
+# Copyright (C) 2010 - 2014, Linaro Limited.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# Maintainer: Botao Sun <botao.sun@linaro.org>
 
 # find and loop over the shader tests found
 # recursively in the named directory
 
-find ${2} -name *.shader_test -print0 | while read -d $'\0' file
+find ${2} -name *.shader_test | while read file
 do
    RESULT=$( ${1} ${file} -auto )
 
@@ -15,7 +32,7 @@ do
      *"$PSTRING"*) echo "${file}: pass";;
 
      *"$SSTRING"*) echo "${file}: skip";;
-  
+
      *"$FSTRING"*) echo "${file}: fail";;
 
      *) echo "${file}: fail";;
