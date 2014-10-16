a87ba31e1082899b519f446a56dd0d25fcf3b22f
Botao Sun <botao.sun@linaro.org>
2014-10-17 03:11:45 +1100

    iozone.sh, iozoneparser.sh: POSIX Compliant Update
    This change is to make the test script to be POSIX compliant.
    
    Original patch comes from:
    
    Lucas Dutra Nunes <ldnunes@ossystems.com.br>
    
    Signed-off by: Botao Sun <botao.sun@linaro.org>
    
    Change-Id: Iee6cffa3e48112c3e742d0290e7b0da1e239d9cc

--- android/scripts/iozone/iozone.sh
+++ android/scripts/iozone/iozone.sh
@@ -2,7 +2,7 @@
 #
 # iozone test
 #
-# Copyright (C) 2013, Linaro Limited.
+# Copyright (C) 2010 - 2014, Linaro Limited.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -16,12 +16,10 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51   Franklin Street, Fifth Floor, Boston, MA  02110-1301,
-# USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
-# owner: harigopal.gollamudi@linaro.org
-#
-##############################################################################
+# Author: Harigopal Gollamudi <harigopal.gollamudi@linaro.org>
+# Maintainer: Botao Sun <botao.sun@linaro.org>
 
 # $1 is the testing location. The -b must be specified for parser.
 # the file itself does not matter as it is not used.
@@ -35,6 +33,6 @@
 # stdout and can be parsed
 
 iozone_cmd="iozone -a -i 0 -i 2 -s 16m -V teststring "
-${iozone_cmd} &> stdout.log
-sh $PWD/android/scripts/iozone/iozoneparser.sh
+${iozone_cmd} > stdout.log 2>&1
+sh $PWD/iozoneparser.sh
 rm -rf stdout.log
