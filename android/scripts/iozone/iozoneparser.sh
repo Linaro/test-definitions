fec875d4c9f98845cd0f79492ee008146bf368e8
Botao Sun <botao.sun@linaro.org>
2014-10-17 03:11:45 +1100

    iozone.sh, iozoneparser.sh: POSIX Compliant Update
    This change is to make the test script to be POSIX compliant.
    
    Original patch comes from:
    
    Lucas Dutra Nunes <ldnunes@ossystems.com.br>
    
    Signed-off by: Botao Sun <botao.sun@linaro.org>
    
    Change-Id: Iee6cffa3e48112c3e742d0290e7b0da1e239d9cc

--- android/scripts/iozone/iozoneparser.sh
+++ android/scripts/iozone/iozoneparser.sh
@@ -2,7 +2,7 @@
 #
 # iozoneparser
 #
-# Copyright (C) 2013, Linaro Limited.
+# Copyright (C) 2010 - 2014, Linaro Limited.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -16,32 +16,32 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51   Franklin Street, Fifth Floor, Boston, MA  02110-1301,
-# USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
-# owner: harigopal.gollamudi@linaro.org
-#
-##############################################################################
+# Author: Harigopal Gollamudi <harigopal.gollamudi@linaro.org>
+# Maintainer: Botao Sun <botao.sun@linaro.org>
 
 grep -A 13 "KB  reclen   write rewrite"  stdout.log | sed '1d' > temp.log
+
 while read line
 do
- var="$(echo $line | cut -d' ' -f2)"
- 
- Write_val="$(echo $line | cut -d' ' -f3)"
- ReWrite_val="$(echo $line | cut -d' ' -f4)"
- RandomRead_val="$(echo $line | cut -d' ' -f5)"
- RandomWrite_val="$(echo $line | cut -d' ' -f6)"
- 
- Write_string='iozone_Write_KB_16384_rclen'_$var' '$Write_val' 'Kbytes/sec' 'pass
- RandomRead_string='iozone_Random_read_KB_16384_rclen'_$var' '$RandomRead_val' 'Kbytes/sec' 'pass
- RandomWrite_string='iozone_Random_write_KB_16384_rclen'_$var' '$RandomWrite_val' 'Kbytes/sec' 'pass
- ReWrite_string='iozone_Rewrite_KB_16384_rclen'_$var' '$ReWrite_val' 'Kbytes/sec' 'pass
+    var="$(echo $line | cut -d' ' -f2)"
 
- echo $Write_string
- echo $RandomRead_string
- echo $RandomWrite_string
- echo $ReWrite_string
+    Write_val="$(echo $line | cut -d' ' -f3)"
+    ReWrite_val="$(echo $line | cut -d' ' -f4)"
+    RandomRead_val="$(echo $line | cut -d' ' -f5)"
+    RandomWrite_val="$(echo $line | cut -d' ' -f6)"
+
+    Write_string='iozone_Write_KB_16384_rclen'_$var' '$Write_val' 'Kbytes/sec' 'pass
+    RandomRead_string='iozone_Random_read_KB_16384_rclen'_$var' '$RandomRead_val' 'Kbytes/sec' 'pass
+    RandomWrite_string='iozone_Random_write_KB_16384_rclen'_$var' '$RandomWrite_val' 'Kbytes/sec' 'pass
+    ReWrite_string='iozone_Rewrite_KB_16384_rclen'_$var' '$ReWrite_val' 'Kbytes/sec' 'pass
+
+    echo $Write_string
+    echo $RandomRead_string
+    echo $RandomWrite_string
+    echo $ReWrite_string
 
 done < temp.log
+
 rm -rf temp.log
