edb027ab7d9f318b63bedeedcfea9fb646bf99c1
Botao Sun <botao.sun@linaro.org>
2014-10-20 23:48:21 +1100

    toolchain.sh: POSIX Compliant Update.
    This change is to make the test script to be POSIX compliant.
    
    Original patch comes from:
    
    Lucas Dutra Nunes <ldnunes@ossystems.com.br>
    
    Signed-off by: Botao Sun <botao.sun@linaro.org>
    
    Change-Id: I817153ab9778f405e08511ae6645c995c975d61a

--- openembedded/scripts/toolchain.sh
+++ openembedded/scripts/toolchain.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 #
-# Toolchain test.
+# Toolchain Test on OpenEmbedded.
 #
-# Copyright (C) 2012, Linaro Limited.
+# Copyright (C) 2010 - 2014, Linaro Limited.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -16,10 +16,10 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
 # Author: Senthil Kumaran <senthil.kumaran@linaro.org>
-#
+# Maintainer: Botao Sun <botao.sun@linaro.org>
 
 cat /proc/version | grep -i "gcc.*linaro" > toolchain.log
 
