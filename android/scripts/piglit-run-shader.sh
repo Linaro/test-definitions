2a004ab911639fd6802afbd3d7a72436741de620
Yongqin Liu <yongqin.liu@linaro.org>
2014-11-07 22:44:12 +0800

    android piglit test: update test definitions for piglit test
    update the test definition files for piglit test,
    also rename the files' name with piglit as prefix
    
    Change-Id: I44c6b6168d6fd164d20deb8fa8dd3b077adfbd64
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- android/scripts/piglit-run-shader.sh
+++ android/scripts/piglit-run-shader.sh
@@ -3,13 +3,19 @@
 # find and loop over the shader tests found
 # recursively in the named directory
 
-find ${2} -name *.shader_test -print0 | while read -d $'\0' file
-do
-   RESULT=$( ${1} ${file} -auto )
+export PIGLIT_PLATFORM=android
+bin_path="/system/xbin/piglit/piglit-shader-test/shader_runner"
+data_dir="/data/piglit/shader"
+glsl_es1_data_dir="${data_dir}/glsl-es-1.00/"
+glsl_es3_data_dir="${data_dir}/glsl-es-3.00/"
 
-   PSTRING="PIGLIT: {'result': 'pass'"
-   SSTRING="PIGLIT: {'result': 'skip'"
-   FSTRING="PIGLIT: {'result': 'fail'"
+/system/bin/busybox find ${data_dir} -name *.shader_test -print0 | while read -d $'\0' file
+do
+   RESULT=$(${bin_path} ${file} -auto )
+
+   PSTRING='PIGLIT: {"result": "pass"'
+   SSTRING='PIGLIT: {"result": "skip"'
+   FSTRING='PIGLIT: {"result": "fail"'
 
    case $RESULT in
      *"$PSTRING"*) echo "${file}: pass";;
