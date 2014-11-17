68ea57d16eb523ce3c8108dc505d2b330817d6d1
Chase Qi <chase.qi@linaro.org>
2014-11-17 22:00:58 +0800

    Add Ubuntu USB test case
    Test code has been validated in LAVA.
    
    Signed-off by: Chase Qi <chase.qi@linaro.org>
    
    Change-Id: I58845cc31647b666aecc78895eea0e4871ee72fa

---
+++ ubuntu/scripts/usb-test-basic.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+# generate test result with lava-test-case
+test_result(){
+if [ $? -eq 0 ]; then
+    lava-test-case $1 --result pass
+else
+    lava-test-case $1 --result fail
+fi
+}
+
+# get the usb devices/hubs list
+echo "========"
+lsusb
+test_result list-all-usb-devices
+
+# examine all usb devices/hubs
+if [ -d /dev/bus/usb/ ]; then
+    for bus in `ls /dev/bus/usb/`; do
+        for device in `ls /dev/bus/usb/$bus/`; do
+            echo "========"
+            echo "Bus $bus, device $device"
+            lsusb -D /dev/bus/usb/$bus/$device
+            status=$?
+
+            if [ $status -ne 0 ]; then
+                echo "Bus$bus-Device$device examination failed"
+                break 2
+            fi
+
+        done
+    done
+
+    if [ $status -eq 0 ]; then
+        lava-test-case examine-all-usb-devices --result pass
+    else
+        lava-test-case examine-all-usb-devices --result fail
+    fi
+
+else
+    echo "/dev/bus/usb/ not exists"
+    lava-test-case examine-all-usb-devices --result fail
+fi
+
+# print supported usb protocols
+echo "========"
+if [ -z "`lsusb -v | grep -i bcdusb`" ]; then
+    lava-test-case print-supported-protocols --result fail
+else
+    lsusb -v | grep -i bcdusb | sort | uniq
+    test_result print-supported-protocols
+fi
+
+# print supported speeds
+echo "========"
+lsusb -t
+test_result print-supported-speeds
+
+# clean exit so lava-test can trust the results
+exit 0
