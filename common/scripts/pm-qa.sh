6b753f51be17e0280c2bb5165aa92db47ff80ffd
Lisa Nguyen <lisa.nguyen@linaro.org>
2015-01-31 13:34:09 -0800

    common/scripts/pm-qa.sh: Update pm-qa.sh script for Android
    Remove unnecessary busybox symbolic links, replace suspend
    test module with cputopology, and simply code further.
    
    Change-Id: I52e5b43ec7c83e6a1d2389dd42177793ed76036f
    Signed-off-by: Lisa Nguyen <lisa.nguyen@linaro.org>

--- common/scripts/pm-qa.sh
+++ common/scripts/pm-qa.sh
@@ -7,35 +7,21 @@ test_func(){
        exit
    fi
 
-   mkdir /data/bin/
-   cd /data/bin
+   bin_dir="/data/bin"
 
-   busybox ln -s -f /system/bin/busybox awk
-   busybox ln -s -f /system/bin/busybox basename
-   busybox ln -s -f /system/bin/busybox chmod
-   busybox ln -s -f /system/bin/busybox chown
-   busybox ln -s -f /system/bin/busybox cp
-   busybox ln -s -f /system/bin/busybox diff
-   busybox ln -s -f /system/bin/busybox find
-   busybox ln -s -f /system/bin/busybox grep
-   busybox ln -s -f /system/bin/busybox rm
-   busybox ln -s -f /system/bin/busybox seq
-   busybox ln -s -f /system/bin/busybox taskset
-   busybox ln -s -f /system/bin/busybox tee
-   busybox ln -s -f /system/bin/busybox printf
-   busybox ln -s -f /system/bin/busybox wc
+   if [ ! -d $bin_dir ]; then
+        mkdir $bin_dir
+   fi
 
-   busybox ln -s -f /system/bin/fake_command command
-   busybox ln -s -f /system/bin/fake_sudo sudo
-   busybox ln -s -f /system/bin/fake_udevadm udevadm
+   cd ${bin_dir}
 
-   export PATH=/data/bin:$PATH
+   export PATH=${bin_dir}:$PATH
 
    cd "${scripts_dir}"
 
    pwd_dir=$PWD
    echo $pwd
-   tests_dirs="cpuidle cpufreq cpuhotplug suspend thermal"
+   tests_dirs="cpuidle cpufreq cpuhotplug cputopology thermal"
 
    for dir in $tests_dirs; do
        var=$dir'_sanity.sh'
@@ -53,7 +39,7 @@ test_func(){
            continue
        fi
 
-       for file in `find . -name "*.sh"`; do
+       for file in `find . -name "*.sh" | sort`; do
            path=$file
            echo $path
            /system/bin/sh $path
