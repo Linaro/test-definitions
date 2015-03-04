f087c1d990975edb55fd0a99f9430c7505df8295
Naresh Kamboju <naresh.kamboju@linaro.org>
2015-03-04 17:36:19 +0530

    LTP : ltpfixup: skipfile can be an URL from json
    ltp test definitions have been updated to accept skipfile from an
    open accessible location via URL as a parameter from LAVA json job file
    and still supports old implementation as a file from test-definitions
    NOTE: busybox wget may not work with https link so pefer to use http
    
    Change-Id: Idf9b93799326e4fc6650bded8396b15a48c63f3b
    Signed-off-by: Naresh Kamboju <naresh.kamboju@linaro.org>

--- common/scripts/ltpfixup.sh
+++ common/scripts/ltpfixup.sh
@@ -15,7 +15,16 @@ LTP_PATH=/opt/ltp
 while getopts T:S:P: arg
     do case $arg in
         T) TST_CMDFILES="$OPTARG";;
-        S) SKIPFILE="-S $SCRIPTPATH/ltp/$OPTARG";;
+        S) OPT=`echo $OPTARG | grep "http"`
+           if [ -z $OPT ] ; then
+             SKIPFILE="-S $SCRIPTPATH/ltp/$OPTARG"
+           else
+             #wget http://people.linaro.org/~naresh.kamboju/skipfile
+             wget $OPTARG
+             SKIPFILE=`echo "${OPTARG##*/}"`
+             SKIPFILE="-S `pwd`/$SKIPFILE"
+           fi
+           ;;
         P) LTP_PATH=$OPTARG;;
     esac
 done
