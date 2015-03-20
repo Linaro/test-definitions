c058de275e948c8fe1d3671a0efbd23527c5b3aa
Arthur She <arthur.she@linaro.org>
2015-03-19 22:24:28 -0700

    ubuntu / openembedded: Fix kselftest-net.yaml problem
    Run test by using original Makefile.
    
    Change-Id: I39f57f45ce8628dca831d2a1eaa71effccf3c1b6

--- common/scripts/kselftest-runner.sh
+++ common/scripts/kselftest-runner.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 TEST_NAME=$1
-COMMAND=$(basename $2)
-DIR=$(dirname $2)
+COMMAND=$(basename "$2")
+DIR=$(dirname "$2")
 LOG="result.log";
 
 cd ${DIR}
-chmod a+x ${COMMAND}
-(./${COMMAND} 2>&1 || echo "${TEST_NAME}: [FAIL]") | tee ${LOG}
+ls ${COMMAND} > /dev/null 2>&1 && chmod a+x ${COMMAND}
+export PATH=.:${PATH}
+(${COMMAND} 2>&1 || echo "${TEST_NAME}: [FAIL]") | tee ${LOG}
 if [ -n "`grep \"skip\" ${LOG}`" ]; then
     echo "${TEST_NAME}: [SKIP]";
 elif [ -z "`grep \"SKIP\|FAIL\" ${LOG}`" ]; then
