272ac7af6ee5bfbb52ee4e4d611193513baac804
esla <timothy.anzaku@linaro.org>
2015-06-10 10:41:33 +0900

    Add support to existing ltp.yaml and ltpfixup.sh scripts to enable LTP-DDT tests to be ran also.
    Change-Id: If7696fed60bd2743cd7953b4cbbe6c4fc5c3aa76

--- common/scripts/ltpfixup.sh
+++ common/scripts/ltpfixup.sh
@@ -9,12 +9,17 @@ echo "Script path is: $SCRIPTPATH"
 TST_CMDFILES=""
 # List of test cases to be skipped
 SKIPFILE=""
+# Used only for ltp-ddt tests. Only run test cases which match PATTERNS. Patterns are 
+# seperated by a comma
+PATTERNS=""
 
 LTP_PATH=/opt/ltp
 
-while getopts T:S:P: arg
+while getopts T:S:P:s: arg
     do case $arg in
-        T) TST_CMDFILES="$OPTARG";;
+        T) 
+            TST_CMDFILES="$OPTARG"
+            LOG_FILE=`echo $OPTARG| sed 's,\/,_,'`;;
         S) OPT=`echo $OPTARG | grep "http"`
            if [ -z $OPT ] ; then
              SKIPFILE="-S $SCRIPTPATH/ltp/$OPTARG"
@@ -25,6 +30,7 @@ while getopts T:S:P: arg
            fi
            ;;
         P) LTP_PATH=$OPTARG;;
+        s) PATTERNS="-s $OPTARG";;
     esac
 done
 
@@ -32,16 +38,16 @@ cd $LTP_PATH
 RESULT=pass
 
 exec 4>&1
-error_statuses="`((./runltp -p -q -f $TST_CMDFILES -l $SCRIPTPATH/LTP_$TST_CMDFILES.log -C $SCRIPTPATH/LTP_$TST_CMDFILES.failed $SKIPFILE ||  echo "0:$?" >&3) |
-        (tee $SCRIPTPATH/LTP_$TST_CMDFILES.out ||  echo "1:$?" >&3)) 3>&1 >&4`"
+error_statuses="`((./runltp -p -q -f $TST_CMDFILES -l $SCRIPTPATH/LTP_$LOG_FILE.log -C $SCRIPTPATH/LTP_$LOG_FILE.failed $SKIPFILE $PATTERNS ||  echo "0:$?" >&3) |
+        (tee $SCRIPTPATH/LTP_$LOG_FILE.out ||  echo "1:$?" >&3)) 3>&1 >&4`"
 exec 4>&-
 
 ! echo "$error_statuses" | grep '0:' >/dev/null
 if [ $? -ne 0 ]; then
     RESULT=fail
 fi
-lava-test-case LTP_$TST_CMDFILES --result $RESULT
+lava-test-case LTP_$LOG_FILE --result $RESULT
 cat $SCRIPTPATH/LTP_*.log
-tar czfv $SCRIPTPATH/LTP_$TST_CMDFILES.tar.gz $SCRIPTPATH/LTP*
-lava-test-case-attach LTP_$TST_CMDFILES $SCRIPTPATH/LTP_$TST_CMDFILES.tar.gz
+tar czfv $SCRIPTPATH/LTP_$LOG_FILE.tar.gz $SCRIPTPATH/LTP*
+lava-test-case-attach LTP_$LOG_FILE $SCRIPTPATH/LTP_$LOG_FILE.tar.gz
 exit 0
