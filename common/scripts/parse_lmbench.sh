6256b67e0f1bc496c51a8198aee0117852edd90e
Anders Roxell <anders.roxell@linaro.org>
2015-08-12 15:00:40 +0200

    common/scripts/parse_lmbench: busybox grep don't support --text
    Change-Id: Ib7353e14da01306a405581a1ffc92526389314bc
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- common/scripts/parse_lmbench.sh
+++ common/scripts/parse_lmbench.sh
@@ -3,8 +3,8 @@
 LOG_FILE=$1
 OUTPUT_FILE=$3
 TMP_FILE="$1.$$"
-BEGIN=$(grep --text -n "^BEGIN LMBENCH" $LOG_FILE | awk -F ':' '{print $1}')
-END=$(grep --text -n "^END LMBENCH" $LOG_FILE | awk -F ':' '{print $1}')
+BEGIN=$(cat $LOG_FILE | grep -n "^BEGIN LMBENCH" | awk -F ':' '{print $1}')
+END=$(cat $LOG_FILE | grep -n "END LMBENCH" | awk -F ':' '{print $1}')
 BEGINNING=$(expr $(expr $END - $BEGIN) + 1)
 head -n $END $LOG_FILE | tail -n $BEGINNING >$TMP_FILE
 
