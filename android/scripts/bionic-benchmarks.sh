5512ed152a0bef1350183604bff9ea56839ea3ac
Yongqin Liu <yongqin.liu@linaro.org>
2015-08-28 14:59:23 +0800

    android: update script for bionic-benchmark test
    since there is no crash caused by the bionic-benchmark test now,
    here removing the workaround for bad cases
    
    Change-Id: I0715ab018c602da7b9e2fed2307aa1abf88fa23a
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- android/scripts/bionic-benchmarks.sh
+++ android/scripts/bionic-benchmarks.sh
@@ -10,40 +10,26 @@ test_bionic_benchmark(){
         echo "The specified $arch is not specified!"
         return
     fi
-    local excluded_test=$2
     local cmd="bionic-benchmarks${arch}"
     if [ -n "$(which ${cmd})" ]; then
-        for line in $(${cmd} --help 2>&1|grep BM_); do
-            if [ -n "$excluded_test" ]; then
-                if echo "${excluded_test}"|grep -q ${line}; then
-                    output_test_result "${arch}_${line}" "skip"
-                    continue
-                fi
-            fi
-            local hasResult=false
-            for  res_line in $(${cmd} "^${line}$"|grep "BM_"|tr -s ' '|tr ' ' ','); do
-                output_test_result "${arch}_${line}" "pass"
-                local key=$(echo $res_line|cut -d, -f1|tr '/' '_')
-                local iterations=$(echo $res_line|cut -d, -f2)
-                local ns_time=$(echo $res_line|cut -d, -f3)
-                local throughput=$(echo $res_line|cut -d, -f4)
-                local throughput_units=$(echo $res_line|cut -d, -f5)
-                output_test_result "${arch}_${key}_time" "pass" "${ns_time}"  "ns/op"
-                if [ -n "${throughput_units}" ];then
-                    output_test_result "${arch}_${key}_throught" "pass" "${throughput}" "${throughput_units}"
-                fi
-                hasResult=true
-            done
-            if ! $hasResult; then
-                output_test_result "${arch}_${line}" "fail"
+        for  res_line in $(${cmd}|grep "BM_"|tr -s ' '|tr ' ' ','); do
+            local key=$(echo $res_line|cut -d, -f1|tr '/' '_')
+            local iterations=$(echo $res_line|cut -d, -f2)
+            local ns_time=$(echo $res_line|cut -d, -f3)
+            local throughput=$(echo $res_line|cut -d, -f4)
+            local throughput_units=$(echo $res_line|cut -d, -f5)
+            output_test_result "${arch}_${key}" "pass"
+            output_test_result "${arch}_${key}_time" "pass" "${ns_time}"  "ns/op"
+            if [ -n "${throughput_units}" ];then
+                output_test_result "${arch}_${key}_throught" "pass" "${throughput}" "${throughput_units}"
             fi
         done
     fi
 }
 
 test_func(){
-    test_bionic_benchmark "64" "BM_property_serial"
-    test_bionic_benchmark "32" "BM_property_serial BM_property_read"
+    test_bionic_benchmark "64"
+    test_bionic_benchmark "32"
 }
 
 main(){
