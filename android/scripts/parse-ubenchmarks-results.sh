89f520163c13bce0a8198d8d3401aa8928a181f1
Vishal Bhoj <vishal.bhoj@linaro.org>
2015-10-09 20:23:35 +0530

    record ubenchmark results
    Change-Id: I1be482a24e6ad56c94f6631114aaf42bda7052fb
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

---
+++ android/scripts/parse-ubenchmarks-results.sh
@@ -0,0 +1,12 @@
+grep -i "benchmark" $1 > results.txt
+while IFS= read -r score; do
+for i in 1 2 3 4 5 6
+do
+export param_$i=`echo $score | awk -v x=$i '{print $x}'`
+done
+lava-test-case $param_1.min --result pass --measurement $param_2
+lava-test-case $param_1.max --result pass --measurement $param_3
+lava-test-case $param_1.mean --result pass --measurement $param_4
+lava-test-case $param_1.stdev --result pass --measurement $param_5
+lava-test-case $param_1.stdev% --result pass --measurement $param_6
+done <  results.txt
