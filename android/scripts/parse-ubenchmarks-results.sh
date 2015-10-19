c38233d37b5815544e0822c4bf7308ebf3aac7c4
Vishal Bhoj <vishal.bhoj@linaro.org>
2015-10-19 22:06:41 +0530

    art-microbenchmarks: Record mode as well in results
    Change-Id: Ie4f9a4a084c58f4ca7d2c6623cb727a53b70cf1c
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- android/scripts/parse-ubenchmarks-results.sh
+++ android/scripts/parse-ubenchmarks-results.sh
@@ -7,9 +7,9 @@ for i in 1 2 3 4 5 6
 do
 export param_$i=`echo $score | awk -v x=$i '{print $x}'`
 done
-lava-test-case $param_1.min --result pass --measurement $param_2
-lava-test-case $param_1.max --result pass --measurement $param_3
-lava-test-case $param_1.mean --result pass --measurement $param_4
-lava-test-case $param_1.stdev --result pass --measurement $param_5
-lava-test-case $param_1.stdev% --result pass --measurement $param_6
+lava-test-case $param_1-$MODE.min --result pass --measurement $param_2
+lava-test-case $param_1-$MODE.max --result pass --measurement $param_3
+lava-test-case $param_1-$MODE.mean --result pass --measurement $param_4
+lava-test-case $param_1-$MODE.stdev --result pass --measurement $param_5
+lava-test-case $param_1-$MODE.stdev% --result pass --measurement $param_6
 done <  res.txt
