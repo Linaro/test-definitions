3ab5a3a269ab109c2f7511687f01a0f2b6a74216
Vishal Bhoj <vishal.bhoj@linaro.org>
2015-11-06 23:46:55 +0530

    nexus9: microbenchmarks: reduce operating frequency
    Change-Id: Id31351b008e289493e494fccbd2baa1fd3dc72db
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- android/scripts/configure-nexus9-for-benchmarks.sh
+++ android/scripts/configure-nexus9-for-benchmarks.sh
@@ -3,5 +3,5 @@ echo "configuring Nexus9 on: $IPADDR"
 adb -s $IPADDR wait-for-device
 adb -s $IPADDR shell stop
 adb -s $IPADDR shell "echo userspace > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
-adb -s $IPADDR shell "echo  2295000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq"
-adb -s $IPADDR shell "echo  2295000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq"
+adb -s $IPADDR shell "echo  1224000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq"
+adb -s $IPADDR shell "echo  1224000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq"
