1577e55647b249bea7b979f10463b97014a6fedf
Naresh Kamboju <naresh.kamboju@linaro.org>
2015-11-19 23:28:29 +0530

    Android: art-microbenchmarks install dependency packages
    art-microbenchmarks test is been updated to install dependency packages
    at runtime on host side.
    
    Change-Id: I719430c24d7e3a54c1fb64f4bbe97a9f7835b222
    Signed-off-by: Naresh Kamboju <naresh.kamboju@linaro.org>

---
+++ android/scripts/art-install-deps.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Example:
+# ./art-install-deps.sh android-tools-adb android-tools-fastboot
+# Wait for unlock apt function waits for the apt-get process to be completed
+# If in case it is already running
+
+wait_for_unlock_apt () {
+    RET=1
+    while [ $RET -eq 1 ]; do
+        PID=`pgrep apt-get`
+        if [ -z $PID ]; then
+            RET=0
+            break
+        fi
+        echo "apt-get still running PID: $PID"
+        sleep 5
+    done
+}
+
+# Read each package name from command line arguments
+for pkg in ${@}
+do
+    # Check if packages are already installed
+    STATUS=`dpkg-query -W -f='${Status} \n' $pkg | awk '{print $1}'`
+
+    if [ "$STATUS" == "install" ]; then
+        echo "==== $pkg package is already installed ===="
+    else
+        wait_for_unlock_apt
+        echo "==== Install $pkg package ===="
+        apt-get install $pkg
+    fi
+done
+exit 0
