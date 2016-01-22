fd1071a40301d5c6ae21f86609f319994ba972bb
Vishal Bhoj <vishal.bhoj@linaro.org>
2016-01-22 16:47:25 +0530

    android/art-microbenchmarks: dump battery status before and after tests
    Change-Id: I344cb8bef2a4d4641cfe2871bb70044a0d26637b
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- android/scripts/configure-nexus9-for-benchmarks.sh
+++ android/scripts/configure-nexus9-for-benchmarks.sh
@@ -3,6 +3,7 @@ set -x
 echo "configuring Nexus9 on: $IPADDR"
 adb -s $IPADDR wait-for-device
 adb -s $IPADDR shell stop
+adb -s $IPADDR shell dumpsys battery
 adb -s $IPADDR shell "echo userspace > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
 sleep 2
 adb -s $IPADDR shell "echo  1224000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq"
