9c013f724a9aafacc9af8c72ffa7beca69d52549
Naresh Kamboju <naresh.kamboju@linaro.org>
2016-08-17 17:11:30 +0530

    ui-browser-test: Adding UI browser testing
    Ui browser test developed on top of Robot frame work.
    Test runs chromium, chrome and firefox test cases.
    
    On LAVA we have access to serial console and we have to run ui tests
    from this serial console, this is possible by changing user from
    root to linaro and exporting the DISPLAY=:0. because linaro owns the
    X display process.
    
    Change-Id: I43bda407f468120839b0a4c8248777303945529e
    Signed-off-by: Naresh Kamboju <naresh.kamboju@linaro.org>

---
+++ ubuntu/scripts/run-ui-browser-tests.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+#
+# Run ui browser tests by using Robot framework
+#
+# Copyright (C) 2010 - 2016, Linaro Limited.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# Author: Naresh Kamboju <naresh.kamboju@linaro.org>
+
+. ./common/scripts/include/sh-test-lib
+LANG=C
+export LANG
+WD="$(pwd)"
+RESULT_FILE="${WD}/results.txt"
+
+usage()
+{
+    echo "usage:"
+    echo "    $0 $1 (username)"
+    echo "ex: $0 ${USER}"
+    echo "ex: $0 linaro"
+    echo "the user should own X process and DISPLAY=:0"
+}
+
+if [  $# -ne 1 ]; then
+    echo "param missing!"
+    usage
+    exit 1
+fi
+
+# Default user is linaro
+TESTUSER=$1
+
+pkgs="git python2.7 python-pip python-lxml"
+install_deps "${pkgs}"
+
+[ -f "${RESULT_FILE}" ] && \
+mv "${RESULT_FILE}" "${RESULT_FILE}_$(date +%Y%m%d%H%M%S)"
+echo
+
+# Tests should runs by linaro users because X owned by linaro user.
+# linaro user can not create output files in /root
+# so change directory to /tmp
+cd /tmp
+git clone http://git.linaro.org/qa/robot-framework-tests.git --depth=1
+./robot-framework-tests/install-on-debian.sh
+su ${TESTUSER} -c ./robot-framework-tests/run-robot-tests.sh
+python ./robot-framework-tests/robot-results-parser.py output.xml >> ${RESULT_FILE}
+cd -
