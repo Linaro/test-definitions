d74d6118b3445fa3a9f30bdfde27226fb20c4b8f
Chase Qi <chase.qi@linaro.org>
2016-08-18 13:18:50 +0800

    v2: initial commit
    - Added utils, bin, and lib folders and related tools
    - Added Linux smoke test case as an example
    - Added test plan example that used to test test-runner
    
    Change-Id: I73693108db219cb67afc99525d80cab93f1eb9ea
    Signed-off-by: Chase Qi <chase.qi@linaro.org>

---
+++ automated/linux/smoke/smoke.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+. ../../lib/sh-test-lib
+OUTPUT="$(pwd)/output"
+mkdir -p "${OUTPUT}"
+RESULT_FILE="${OUTPUT}/result.txt"
+export RESULT_FILE
+
+usage() {
+    echo "Usage: $0 [-s <true|false>]" 1>&2
+    exit 1
+}
+
+while getopts "s:" o; do
+  case "$o" in
+    s) SKIP_INSTALL="${OPTARG}" ;;
+    *) usage ;;
+  esac
+done
+
+install() {
+    dist_name
+    case "${dist}" in
+      Debian|Ubuntu) pkgs="lsb-release usbutils" ;;
+      Fedora|CentOS) pkgs="redhat-lsb-core usbutils" ;;
+    esac
+
+    install_deps "${pkgs}" "${SKIP_INSTALL}"
+}
+
+run() {
+    local test="$1"
+    test_case_id="$(echo "${test}" | awk '{print $1}')"
+    echo
+    info_msg "Running ${test_case_id} test..."
+    eval "${test}"
+    check_return "${test_case_id}"
+}
+
+# Test run.
+[ -f "${OUTPUT}" ] && \
+mv "${OUTPUT}" "${OUTPUT}_$(date +%Y%m%d%H%M%S)"
+
+install
+run "pwd"
+run "lsb_release -a"
+run "uname -a"
+run "ip a"
+run "lscpu"
+run "vmstat"
+run "lsblk"
+run "lsusb"
