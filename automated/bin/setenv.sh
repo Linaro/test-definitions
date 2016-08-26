ec59ef3e1418e61b3356344d807045338992ae51
Chase Qi <chase.qi@linaro.org>
2016-08-26 14:47:16 +0800

    v2: linux: add dd write/read speed test
    - Added Linux dd write/read speed test
    - Optimized sh-test-lib
    - Fixed issues in smoke.sh
    
    Change-Id: I3bccfda212535e9a36b8e231b69c0dd2367a88f6
    Signed-off-by: Chase Qi <chase.qi@linaro.org>

--- automated/bin/setenv.sh
+++ automated/bin/setenv.sh
@@ -4,7 +4,7 @@ export REPO_PATH="$(pwd)"
 echo "REPO_PATH: ${REPO_PATH}"
 
 if ! [ -d "${REPO_PATH}/automated/bin" ]; then
-    echo "ERROR: Please execute the below command from test-definitions DIR"
+    echo "ERROR: Please execute the below command from 'test-definitions' DIR"
     echo "    . ./automated/bin/setenv.sh"
     exit 1
 fi
