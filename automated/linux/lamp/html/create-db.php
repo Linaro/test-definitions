ee4d20dc380ccac36a3799325634bf9e5f11c237
Chase Qi <chase.qi@linaro.org>
2016-09-06 17:22:46 +0800

    v2: linux: add lamp test
    Change-Id: I94bf06cf0b18082276688a871e0be7e76ddf370e
    Signed-off-by: Chase Qi <chase.qi@linaro.org>

---
+++ automated/linux/lamp/html/create-db.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "lamptest";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
