9a85dd7ebd66faa430110ffc6157b6d7d5595b3e
erin.liu <erin.liu@linaro.org>
2016-09-30 07:43:30 +0100

    android monkey test:add monkey testcase
    Change-Id: I3eeabe9bdeb74cc90d9f49d18359a4d7a304388e
    Signed-off-by: erin.liu <erin.liu@linaro.org>

---
+++ automated/android/monkey/monkey.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+. ../../lib/sh-test-lib
+. ../../lib/android-test-lib
+
+HOST_OUTPUT="$(pwd)/output"
+DEVICE_OUTPUT="/data/monkey-test"
+RESULT_FILE="${HOST_OUTPUT}/result.txt"
+
+usage() {
+    echo "Usage: $0 [-s <sn>] [-b <blacklist>] [-p <monkeypara>] [-e <eventnum>] [-t <throttle>]" 1>&2
+    exit 0
+}
+
+while getopts "s:b:p:e:t:" opt; do
+    case "$opt" in
+        s) SN="${OPTARG}" ;;
+        b) BLACKLIST="${OPTARG}" ;;
+        p) MONKEYPARA="${OPTARG}" ;;
+        e) EVENTNUM="${OPTARG}" ;;
+        t) THROTTLE="${OPTARG}" ;;
+        *) usage ;;
+    esac
+done
+
+initialize_adb
+detect_abi
+
+[ -d "${HOST_OUTPUT}" ] && mv "${HOST_OUTPUT}" "${HOST_OUTPUT}-$(date +%Y%m%d%H%M%S)"
+mkdir -p "${HOST_OUTPUT}"
+
+#####read blacklist and write to blacklist.txt
+BLACKLISTTXT="${HOST_OUTPUT}/blacklist.txt"
+arr=$(echo $BLACKLIST|tr "," "\n")
+for s in $arr
+do
+    echo $s
+    echo "$s" >> $BLACKLISTTXT
+done
+
+adb -s ${SN} push $BLACKLISTTXT /data
+BLACKLIST="/data/blacklist.txt"
+info_msg "About to run dd monkey test on device ${SN}"
+
+adb -s ${SN} shell monkey ${MONKEYPARA} --pkg-blacklist-file ${BLACKLIST} --throttle ${THROTTLE} ${EVENTNUM} 2>&1 \
+    | tee "${HOST_OUTPUT}/monkey-test-output.txt"
+
+grep "Monkey finished" "${HOST_OUTPUT}/monkey-test-output.txt"
+check_return "monkey-test"
+#parse_output "monkey-test"
+
