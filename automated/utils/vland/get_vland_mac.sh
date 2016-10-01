7079d75349b35d2428a338be9e8a183eecb951ee
Anders Roxell <anders.roxell@linaro.org>
2016-10-01 17:26:54 +0200

    automated/utils: add vland helper functions.
    get_vland_interface and get_vland_mac
    
    Change-Id: I32e84c985ca4e59eb695bcc8312da9d6de1643a4
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

---
+++ automated/utils/vland/get_vland_mac.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+rootdir=$(readlink -f $(dirname $0))
+
+vland_name=${1}
+
+iface=$(${rootdir}/get_vland_interface.sh ${vland_name})
+if [ -z "${iface}" ]; then
+    exit 1
+fi
+cat /sys/class/net/${iface}/address
