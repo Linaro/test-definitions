38d9fcae98fdf3f1d9b7ded5d640f03f1f0e679e
Chase Qi <chase.qi@linaro.org>
2016-10-19 16:28:54 +0800

    v2: linux: add disk partitioning test
    * migrate common/disk-partitioning-test.sh to v2
    * added support for test skipping
    
    Change-Id: I3e8ed7fd5e62f4e511c01f9e91770cd17c927603
    Signed-off-by: Chase Qi <chase.qi@linaro.org>

---
+++ automated/linux/disk/disk-partitioning.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+. ../../lib/sh-test-lib
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+DISKLABEL="gpt"
+FILESYSTEM="ext4"
+
+usage() {
+    echo "Usage: $0 [-d <device>] [-l <disklabel>] [-f <filesystem>] [-s <true|false>]" 1>&2
+    exit 1
+}
+
+while getopts "d:l:f:r:s:" o; do
+  case "$o" in
+    # The existing disk label on the device will be destroyed,
+    # and all data on this disk will be lost.
+    d) DEVICE="${OPTARG}" ;;
+    l) DISKLABEL="${OPTARG}" ;;
+    f) FILESYSTEM="${OPTARG}" ;;
+    s) SKIP_INSTALL="${OPTARG}" ;;
+    *) usage ;;
+  esac
+done
+
+create_disklabel() {
+    echo
+    echo "Creating ${DEVICE} disklabel: ${DISKLABEL}"
+    umount "${DEVICE}*" > /dev/null 2>&1
+    # If mklabel fails, skip the following tests.
+    skip_list="create-partition format-partition mount-partition umount-partition"
+    parted -s "${DEVICE}" mklabel "${DISKLABEL}"
+    exit_on_fail "create-disklabel" "${skip_list}"
+
+    sync
+    sleep 10
+}
+
+create_partition() {
+    echo
+    echo "Creating partition: ${DEVICE}1"
+    skip_list="format-partition mount-partition umount-partition"
+    parted -s "${DEVICE}" mkpart primary 0% 100%
+    exit_on_fail "create-partition" "${skip_list}"
+
+    sync
+    sleep 10
+}
+
+format_partition() {
+    echo
+    echo "Formatting ${DEVICE}1 to ${FILESYSTEM}"
+    skip_list="mount-partition umount-partition"
+    if [ "${FILESYSTEM}" = "fat32" ]; then
+        echo "y" | mkfs -t vfat -F 32 "${DEVICE}1"
+    else
+        echo "y" | mkfs -t "${FILESYSTEM}" "${DEVICE}1"
+    fi
+    exit_on_fail "format-partition" "${skip_list}"
+
+    sync
+    sleep 10
+}
+
+smoke_test() {
+    echo
+    echo "Running mount/umoun tests..."
+    umount /mnt > /dev/null 2>&1
+    skip_list="umount-partition"
+    mount "${DEVICE}1" /mnt
+    exit_on_fail "mount-partition" "${skip_list}"
+
+    umount "${DEVICE}1"
+    check_return "umount-partition"
+}
+
+# Test run.
+[ -z "${DEVICE}" ] && error_msg "Please specify test device with '-d'"
+! check_root && error_msg "You need to be root to run this script."
+[ -d "${OUTPUT}" ] && mv "${OUTPUT}" "${OUTPUT}_$(date +%Y%m%d%H%M%S)"
+mkdir -p "${OUTPUT}"
+
+pkgs="parted e2fsprogs dosfstools"
+install_deps "${pkgs}" "${SKIP_INSTALL}"
+
+create_disklabel
+create_partition
+format_partition
+smoke_test
