b1e48f27703cc996785d0883b997bb8ea3a3c4f8
Chase Qi <chase.qi@linaro.org>
2016-10-21 09:00:15 +0800

    v2: send-to-lava.sh: move 'command -v' out of while loop
    Change-Id: Iff916da54cee93664f24bcdd1c14970b5a5db984
    Signed-off-by: Chase Qi <chase.qi@linaro.org>

--- automated/utils/send-to-lava.sh
+++ automated/utils/send-to-lava.sh
@@ -2,13 +2,16 @@
 
 RESULT_FILE="$1"
 
+command -v lava-test-case
+lava_test_case="$?"
+
 if [ -f "${RESULT_FILE}" ]; then
     while read line; do
         if echo "${line}" | egrep -iq ".* +(pass|fail|skip)$"; then
             test="$(echo "${line}" | awk '{print $1}')"
             result="$(echo "${line}" | awk '{print $2}')"
 
-            if command -v lava-test-case; then
+            if [ "${lava_test_case}" -eq 0 ]; then
                 lava-test-case "${test}" --result "${result}"
             else
                 echo "<TEST_CASE_ID=${test} RESULT=${result}>"
@@ -19,7 +22,7 @@ if [ -f "${RESULT_FILE}" ]; then
             measurement="$(echo "${line}" | awk '{print $3}')"
             units="$(echo "${line}" | awk '{print $4}')"
 
-            if command -v lava-test-case; then
+            if [ "${lava_test_case}" -eq 0 ]; then
                 lava-test-case "${test}" --result "${result}" --measurement "${measurement}" --units "${units}"
             else
                echo "<TEST_CASE_ID=${test} RESULT=${result} UNITS=${units} MEASUREMENT=${measurement}>"
