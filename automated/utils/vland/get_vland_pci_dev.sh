4a850a70773311e8c192265110afaf74c94d6ecb
Josep Puigdemont <josep.puigdemont@linaro.org>
2016-10-21 17:34:33 +0200

    get_vland_pci_dev.sh: return the PCI device
    Change-Id: If0ac3a702528d97f536513e35a1f65be43f15a27
    Signed-off-by: Josep Puigdemont <josep.puigdemont@linaro.org>

---
+++ automated/utils/vland/get_vland_pci_dev.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+rootdir=$(readlink -f "$(dirname "$0")")
+
+vland_name=${1}
+
+iface=$("${rootdir}/get_vland_interface.sh" "${vland_name}")
+if [ -L "/sys/class/net/${iface}/device" ]; then
+	basename "$(readlink -f "/sys/class/net/${iface}/device")"
+else
+	exit 1
+fi
