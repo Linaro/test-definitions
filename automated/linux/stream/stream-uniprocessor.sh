26c5b6b918e60991abcf243cc89b0d4be252a417
Chase Qi <chase.qi@linaro.org>
2016-10-28 15:17:30 +0800

    v2: linux: add stream uniprocessor test
    Change-Id: I9777b61faa63753c8890a4b3197118e82189320d
    Signed-off-by: Chase Qi <chase.qi@linaro.org>

---
+++ automated/linux/stream/stream-uniprocessor.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -e
+
+. ../../lib/sh-test-lib
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+TEST_LOG="${OUTPUT}/stream-output.txt"
+
+[ -d "${OUTPUT}" ] && mv "${OUTPUT}" "${OUTPUT}_$(date +%Y%m%d%H%M%S)"
+mkdir -p "${OUTPUT}"
+
+# Run Test.
+detect_abi
+# shellcheck disable=SC2154
+./bin/"${abi}"/stream 2>&1 | tee "${TEST_LOG}"
+
+# Parse output.
+for test in Copy Scale Add Triad; do
+    grep "^${test}" "${TEST_LOG}" \
+      | awk -v test="${test}" \
+        '{printf("stream-uniprocessor-%s pass %s MB/s\n", test, $2)}' \
+      | tee -a "${RESULT_FILE}"
+done
