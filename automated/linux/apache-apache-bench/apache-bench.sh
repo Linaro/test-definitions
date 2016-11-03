a4141038280171d80adea6ef1297ee396277fb7c
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2016-11-03 23:51:53 +0000

    automated: added apache-bench
    This tests runs apache-bench (ab) against apache2 webserver.
    
    Change-Id: I9dc13a17d8e1db3f23f88cd77e7e89c227411025
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

---
+++ automated/linux/apache-apache-bench/apache-bench.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+. ../../lib/sh-test-lib
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+LOGFILE="${OUTPUT}/ablog.txt"
+
+usage() {
+    echo "Usage: $0 [-s <true|false>]" 1>&2
+    exit 1
+}
+
+NUMBER=1000
+CONCURENT=100
+
+while getopts "s:n:c" o; do
+  case "$o" in
+    s) NUMBER="${OPTARG}" ;;
+    s) CONCURENT="${OPTARG}" ;;
+    s) SKIP_INSTALL="${OPTARG}" ;;
+    *) usage ;;
+  esac
+done
+
+! check_root && error_msg "This script must be run as root"
+[ -d "${OUTPUT}" ] && mv "${OUTPUT}" "${OUTPUT}_$(date +%Y%m%d%H%M%S)"
+mkdir -p "${OUTPUT}"
+
+# Install apache and use systemctl for service management. Tested on Ubuntu 16.04,
+# Debian 8, CentOS 7 and Fedora 24. systemctl should available on newer releases
+# as well.
+if [ "${SKIP_INSTALL}" = "True" ] || [ "${SKIP_INSTALL}" = "true" ]; then
+    warn_msg "Apache package installation skipped"
+else
+    dist_name
+    # shellcheck disable=SC2154
+    case "${dist}" in
+      Debian|Ubuntu)
+        pkgs="apache2 apache2-utils"
+        install_deps "${pkgs}"
+        systemctl restart apache2
+        ;;
+      CentOS|Fedora)
+        pkgs="httpd httpd-tools"
+        install_deps "${pkgs}"
+        systemctl start httpd.service
+        ;;
+      *)
+        error_msg "Unsupported distribution!"
+    esac
+fi
+
+cp ./html/* /var/www/html/
+
+# Test Apachebench on Apache.
+ab -n "${NUMBER}" -c "${CONCURENT}" "http://localhost/index.html" > "${LOGFILE}"
+# shellcheck disable=SC2129
+grep "Time taken for tests:" "${LOGFILE}" | awk '{print "Time_taken_for_tests pass " $5 " s"}' >> "${RESULT_FILE}"
+# shellcheck disable=SC2129
+grep "Complete requests:" "${LOGFILE}" | awk '{print "Complete_requests pass " $3 " items"}' >> "${RESULT_FILE}"
+# shellcheck disable=SC2129
+grep "Failed requests:" "${LOGFILE}" | awk '{ORS=""} {print "Failed_requests "; if ($3==0) {print "pass "} else {print "fail "}; print $3 " items\n" }' >> "${RESULT_FILE}"
+# shellcheck disable=SC2129
+grep "Write errors:" "${LOGFILE}" | awk '{ORS=""} {print "Write_errors "; if ($3==0) {print "pass "} else {print "fail "}; print $3 " items\n" }' >> "${RESULT_FILE}"
+# shellcheck disable=SC2129
+grep "Total transferred:" "${LOGFILE}" | awk '{print "Total_transferred pass " $3 " bytes"}' >> "${RESULT_FILE}"
+# shellcheck disable=SC2129
+grep "HTML transferred:" "${LOGFILE}" | awk '{print "HTML_transferred pass " $3 " bytes"}' >> "${RESULT_FILE}"
+# shellcheck disable=SC2129
+grep "Requests per second:" "${LOGFILE}" | awk '{print "Requests_per_second  pass " $4 " #/s"}' >> "${RESULT_FILE}"
+# shellcheck disable=SC2129
+grep "Time per request:" "${LOGFILE}" | grep -v "across" | awk '{print "Time_per_request_mean pass " $4 " ms"}' >> "${RESULT_FILE}"
+# shellcheck disable=SC2129
+grep "Time per request:" "${LOGFILE}" | grep "across" | awk '{print "Time_per_request_concurent pass " $4 " ms"}' >> "${RESULT_FILE}"
+# shellcheck disable=SC2129
+grep "Transfer rate:" "${LOGFILE}" | awk '{print "Transfer_rate pass " $3 " kb/s"}' >> "${RESULT_FILE}"
