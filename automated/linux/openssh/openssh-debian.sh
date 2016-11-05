e448d043151d474dc7151001d6d932c8bceda32d
Naresh Kamboju <naresh.kamboju@linaro.org>
2016-11-05 15:10:51 +0530

    linux: openssh debian test
    Change-Id: I63d8a44c0bf004da0547e1cb0b87d5afaa60a4fc
    Signed-off-by: Naresh Kamboju <naresh.kamboju@linaro.org>

---
+++ automated/linux/openssh/openssh-debian.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+. ../../lib/sh-test-lib
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+RESULT_LOG="${OUTPUT}/result_log.txt"
+TEST_LOG="${OUTPUT}/test_log.txt"
+
+parse_output() {
+    egrep "^failed|^ok" "${TEST_LOG}"  2>&1 | tee -a "${RESULT_LOG}"
+    sed -i -e 's/ok/pass/g' "${RESULT_LOG}"
+    sed -i -e 's/failed/fail/g' "${RESULT_LOG}"
+    awk '{for (i=2; i<NF; i++) printf $i "-"; print $NF " " $1}' "${RESULT_LOG}" 2>&1 | tee -a "${RESULT_FILE}"
+}
+
+while getopts "s:" o; do
+  case "$o" in
+    s) SKIP_INSTALL="${OPTARG}" ;;
+    *) usage ;;
+  esac
+done
+
+# Test run.
+! check_root && error_msg "This script must be run as root"
+[ -d "${OUTPUT}" ] && mv "${OUTPUT}" "${OUTPUT}_$(date +%Y%m%d%H%M%S)"
+mkdir -p "${OUTPUT}"
+
+pkgs="build-essential"
+install_deps "${pkgs}" "${SKIP_INSTALL}"
+
+
+info_msg "About to run dd test..."
+info_msg "Output directory: ${OUTPUT}"
+
+apt-get update
+apt-get source openssh
+VERSION=$(dpkg -l | grep openssh-client |awk '{print $3}'|cut -d- -f 1 | cut -d: -f2)
+# shellcheck disable=SC2164
+cd openssh-"${VERSION}"
+./configure
+make
+make install
+make tests  2>&1 | tee -a "${TEST_LOG}"
+
+parse_output
