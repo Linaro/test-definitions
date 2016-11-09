eeb48e3df41a9b13204a3886eea2d1d709629bb8
Chase Qi <chase.qi@linaro.org>
2016-11-09 19:10:24 +0800

    automated: added Linux LEMP test
    Change-Id: I48d54b6c4c89e7a5c4d0bac0b30a1b48b26e3ca0
    Signed-off-by: Chase Qi <chase.qi@linaro.org>

---
+++ automated/linux/lemp/html/create-db.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "lemptest";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
