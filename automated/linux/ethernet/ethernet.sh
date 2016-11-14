1cd3ea36ed5d27ea5ae5df2c894591303fb26a02
Naresh Kamboju <naresh.kamboju@linaro.org>
2016-11-14 15:43:59 +0530

    automated: added Linux ethernet test
    Change-Id: I55bd54367e98aba150c01cd957fb8ca90c3a0d10
    Signed-off-by: Naresh Kamboju <naresh.kamboju@linaro.org>

---
+++ automated/linux/ethernet/ethernet.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+export RESULT_FILE
+
+# Default ethernet interface
+INTERFACE="eth0"
+
+usage() {
+    echo "Usage: $0 [-i <eth0|wlan0>]" 1>&2
+    exit 1
+}
+
+while getopts "s:i:" o; do
+  case "$o" in
+    s) SKIP_INSTALL="${OPTARG}" ;;
+    # Ethernet interface
+    i) INTERFACE="${OPTARG}" ;;
+    *) usage ;;
+  esac
+done
+
+# Test run.
+! check_root && error_msg "This script must be run as root"
+[ -d "${OUTPUT}" ] && mv "${OUTPUT}" "${OUTPUT}_$(date +%Y%m%d%H%M%S)"
+mkdir -p "${OUTPUT}"
+
+pkgs="net-tools"
+install_deps "${pkgs}" "${SKIP_INSTALL}"
+
+testcase="ethernet-ip-addr-${INTERFACE}"
+
+# Print all network interface status
+ip addr
+# Print given network interface status
+ip addr show "${INTERFACE}"
+
+# Get IP address of given interface
+IP_ADDR=$(ip addr show "${INTERFACE}" | grep "${INTERFACE}" | tail -1 | awk '{print $2}' | cut -f1 -d'/')
+
+# Validate IP address by ping
+testcase_cmd="ping ${IP_ADDR} -c 5"
+
+# Run the test
+run_test_case "${testcase_cmd}" "${testcase}"
