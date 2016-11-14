644f211b7e8a256b5b6639366c9edfd225f30d38
Naresh Kamboju <naresh.kamboju@linaro.org>
2016-11-14 15:43:59 +0530

    automated: added Linux ethernet test
    Ethernet test to validate given network interface,
    * check interface state is up
    * ping to default Gateway
    
    Change-Id: I55bd54367e98aba150c01cd957fb8ca90c3a0d10
    Signed-off-by: Naresh Kamboju <naresh.kamboju@linaro.org>

---
+++ automated/linux/ethernet/ethernet.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+export RESULT_FILE
+
+# Default ethernet interface
+INTERFACE="eth0"
+
+usage() {
+    echo "Usage: $0 [-i <ethernet-interface> -s <true|false>]" 1>&2
+    exit 1
+}
+
+while getopts "s:i:" o; do
+  case "$o" in
+    s) SKIP_INSTALL="${OPTARG}" ;;
+    # Ethernet interface
+    i) INTERFACE="${OPTARG}" ;;
+    *) usage ;;
+  esac
+done
+
+# Test run.
+! check_root && error_msg "This script must be run as root"
+[ -d "${OUTPUT}" ] && mv "${OUTPUT}" "${OUTPUT}_$(date +%Y%m%d%H%M%S)"
+mkdir -p "${OUTPUT}"
+
+pkgs="net-tools"
+install_deps "${pkgs}" "${SKIP_INSTALL}"
+
+# Print all network interface status
+ip addr
+# Print given network interface status
+ip addr show "${INTERFACE}"
+
+# Get IP address of a given interface
+IP_ADDR=$(ip addr show "${INTERFACE}" | grep -a2 "state UP" | tail -1 | awk '{print $2}' | cut -f1 -d'/')
+
+[ -n "${IP_ADDR}" ]
+exit_on_fail "ethernet-ping-state-UP" "ethernet-ping-route"
+
+# Get default Route IP address of a given interface
+ROUTE_ADDR=$(ip route list  | grep default | awk '{print $3}')
+
+# Run the test
+run_test_case "ping ${ROUTE_ADDR} -c 5" "ethernet-ping-route"
