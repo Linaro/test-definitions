13ea9b16bf2813cd0ae6897a273189e5f4e33a55
Ravineet Singh <ravineet.singh@linaro.org>
2016-12-02 15:49:08 +0100

    Add isolation test, verifies that an appl. is isolated.
     Verify that an application is isolated and that the kernel configuraion
     is sane for the applcation to be run without interference from user/kernel
     space tasks.
    
    Change-Id: Ie1c267b6d50026a080f6a0d6d25d518f2ad6f4ab
    Signed-off-by: Ravineet Singh <ravineet.singh@linaro.org>

---
+++ automated/linux/isolate-task/isolate-task-verify.sh
@@ -0,0 +1,40 @@
+#!/bin/sh -e
+
+usage() {
+    echo "Usage: $0 <-c cpus> <-s [true|false]>" 1>&2
+    echo " -c CPU's to isolate and verify"
+    echo " -s Skip dependenciy installs"
+    echo " example:"
+    echo " $0 -c 1,2 -s false"
+    exit 1
+}
+
+while getopts "c:s:" o; do
+    case "$o" in
+	c) CPUS="${OPTARG}" ;;
+	s) SKIP_INSTALL="${OPTARG}" ;;
+	*) usage ;;
+    esac
+done
+
+[ -z "${CPUS}" ] && usage
+[ -z "${SKIP_INSTALL}" ] && usage
+
+. ../../lib/sh-test-lib
+
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+TEST_LOG="${OUTPUT}/isolation-test-output.txt"
+
+[ -d "${OUTPUT}" ] && mv "${OUTPUT}" "${OUTPUT}_$(date +%Y%m%d%H%M%S)"
+mkdir -p "${OUTPUT}"
+
+if [ "${SKIP_INSTALL}" = "false" ] || [ "${SKIP_INSTALL}" = "False" ]; then
+    install_deps "git stress cpuset gzip"
+    git clone git://git.linaro.org/lng/task-isolation.git
+fi
+
+cd task-isolation
+./isolate-task.sh -v -c "${CPUS}" sleep "10" 2>&1 | tee "${TEST_LOG}"
+
+grep TEST_ISOLATION_CORE_ "${TEST_LOG}" > "${RESULT_FILE}"
