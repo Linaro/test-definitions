0020cbc352f61bbc099898e53371b6330550ce74
Daniel Díaz <daniel.diaz@linaro.org>
2017-03-09 08:39:35 -0600

    [rfc] automated: add bionic-libc tests
    Change-Id: I3dcac7741c0f9aaeb76a9d0eadbc21d5cbf98cfe
    Signed-off-by: Daniel Díaz <daniel.diaz@linaro.org>

---
+++ automated/android/bionic-libc-tests/bionic-libc-tests.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+HOST_OUTPUT="$(pwd)/output"
+DEVICE_OUTPUT="/data/local/tmp/result.txt"
+export RESULT_FILE
+TIMEOUT=300
+
+usage() {
+    echo "Usage: $0" 1>&2
+    exit 1
+}
+
+if [ $# -gt 1 ]; then
+    usage
+fi
+
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+. ../../lib/android-test-lib
+
+# Test run.
+create_out_dir "${HOST_OUTPUT}"
+
+initialize_adb
+wait_boot_completed "${TIMEOUT}"
+adb_push "./device-script.sh" "/data/local/tmp/"
+
+info_msg "About to run bionic-libc-tests on device ${SN}"
+adb -s "${SN}" shell /data/local/tmp/device-script.sh 2>&1 \
+    | tee "${HOST_OUTPUT}"/device-run.log
+
+adb_pull "${DEVICE_OUTPUT}" "${HOST_OUTPUT}"
