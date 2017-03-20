0589137dcc9c372163a290d9cdefc3bbcd49da2c
Chase Qi <chase.qi@linaro.org>
2017-03-20 21:09:12 +0800

    automated: android: add stringbench test
    Change-Id: Iadb21d6f75c5b40a614d5f29d697933286e78808
    Signed-off-by: Chase Qi <chase.qi@linaro.org>

---
+++ automated/android/stringbench/stringbench.sh
@@ -0,0 +1,53 @@
+#!/bin/sh -ex
+
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+export RESULT_FILE
+ANDROID_SERIAL=""
+BOOT_TIMEOUT="300"
+
+usage() {
+    echo "Usage: $0 [-s <android_serial>] [-t <boot_timeout>]" 1>&2
+    exit 1
+}
+
+while getopts ":p:i:s:t:" o; do
+  case "$o" in
+    # Specify device serial number when more than one device connected.
+    s) ANDROID_SERIAL="${OPTARG}" ;;
+    t) BOOT_TIMEOUT="${OPTARG}" ;;
+    *) usage ;;
+  esac
+done
+
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+# shellcheck disable=SC1091
+. ../../lib/android-test-lib
+
+initialize_adb
+wait_boot_completed "${BOOT_TIMEOUT}"
+create_out_dir "${OUTPUT}"
+
+parser() {
+    logfile="$1"
+    case "$2" in
+        stringbench) prefix="32bit" ;;
+        stringbench64) prefix="64bit" ;;
+    esac
+
+    while read -r line; do
+        test_case_id=$(echo "${line}" | cut -d: -f1 | tr -c '[:alnum:]:.' '_' | tr -s '_' | sed 's/_$//')
+        test_case_id="${prefix}_${test_case_id}"
+        measurement=$(echo "${line}" | awk '{print $(NF-1)}')
+        units=$(echo "${line}" | awk '{print $NF}')
+        add_metric "${test_case_id}" "pass" "${measurement}" "${units}"
+    done < "${logfile}"
+}
+
+for test in stringbench stringbench64; do
+    info_msg "device-${ANDROID_SERIAL}: About to run ${test}"
+    check_cmd_existence "${test}"
+    adb shell "${test}" | tee "${OUTPUT}/${test}.log"
+    parser "${OUTPUT}/${test}.log" "${test}"
+done
