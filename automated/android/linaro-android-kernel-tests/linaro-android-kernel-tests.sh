6255e54db02fe7f82726a189d4295885d41e9d73
Chase Qi <chase.qi@linaro.org>
2017-03-21 14:34:02 +0800

    automated: android: add linaro android kernel tests
    Change-Id: I2957d29e2172d8342bfe7a8a6aba22097601d5fb
    Signed-off-by: Chase Qi <chase.qi@linaro.org>

---
+++ automated/android/linaro-android-kernel-tests/linaro-android-kernel-tests.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -e
+
+OUTPUT="$(pwd)/output"
+LOGFILE="${OUTPUT}/linaro-android-kernel-tests.log"
+RESULT_FILE="${OUTPUT}/result.txt"
+TEST_SCRIPT="linaro-android-kernel-tests.sh"
+
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+# shellcheck disable=SC1091
+. ../../lib/android-test-lib
+
+initialize_adb
+adb_root
+wait_boot_completed "300"
+create_out_dir "${OUTPUT}"
+
+# Run test script.
+if test -n "$(adb -s "${SN}" shell "which ${TEST_SCRIPT}")"; then
+    adb -s "${SN}" shell "${TEST_SCRIPT}" | tee "${LOGFILE}"
+else
+    warn_msg "${TEST_SCRIPT} NOT found"
+    report_fail "test-script-existence-check"
+    exit 0
+fi
+
+# Parse test log.
+grep -E "test (passed|failed|skipped)" "${LOGFILE}" \
+    | sed 's/[]*:[]//g; s/^0 //g' \
+    | sed 's/passed/pass/; s/failed/fail/; s/skipped/skip/' \
+    | awk '{printf("%s %s\n", $1, $NF)}' \
+    | tee -a "${RESULT_FILE}"
