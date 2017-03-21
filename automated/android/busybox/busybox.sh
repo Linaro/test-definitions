f9fbcba7a2baaf8eaa2758838134261a2f3770f0
Chase Qi <chase.qi@linaro.org>
2017-03-21 13:17:18 +0800

    automated: android: add busybox smoke test
    Change-Id: I4ea462ff2e643d9016c9f2a25d85e14a6deb6fe7
    Signed-off-by: Chase Qi <chase.qi@linaro.org>

---
+++ automated/android/busybox/busybox.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+# Busybox smoke tests.
+
+OUTPUT="$(pwd)/output"
+
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+# shellcheck disable=SC1091
+. ../../lib/android-test-lib
+
+initialize_adb
+wait_boot_completed "300"
+create_out_dir "${OUTPUT}"
+
+adb_push "./device-script.sh" "/data/local/tmp/bin/"
+
+adb -s "${SN}" shell '/data/local/tmp/bin/device-script.sh 2>&1' \
+                          | tee "${OUTPUT}/device-stdout.log"
+
+adb_pull "/data/local/tmp/busybox/result.txt" "${OUTPUT}/"
