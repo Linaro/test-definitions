013542450bd6fc4a29b23c267bc9ccccb97de943
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2017-03-30 18:54:46 +0100

    automated: android: add refactored apk-automation
    Change-Id: I5a81b3c4317dc56f90df37c5a8f83d2547542fab
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

---
+++ automated/android/apk-automation/javawhetstone.py
@@ -0,0 +1,63 @@
+import re
+import time
+from common import ApkTestRunner
+from com.dtmilano.android.viewclient import ViewNotFoundException
+
+
+class ApkRunnerImpl(ApkTestRunner):
+    def __init__(self, config):
+        self.config = config
+        self.config['apk_file_name'] = 'JavaBenchmark/pure-java-benchmarks/01-Java_Whetstone.apk'
+        self.config['apk_package'] = 'com.roywhet'
+        self.config['activity'] = 'com.roywhet/.JavaWhetstoneActivity'
+        super(ApkRunnerImpl, self).__init__(self.config)
+
+    def execute(self):
+        self.dump_always()
+        btn_run = self.vc.findViewByIdOrRaise("com.roywhet:id/startButton")
+        btn_run.touch()
+        time.sleep(2)
+
+        finished = False
+        while not finished:
+            try:
+                time.sleep(30)
+                self.dump_always()
+                self.jws_results = self.vc.findViewByIdOrRaise("com.roywhet:id/displayDetails")
+                if re.search('Total Elapsed Time', self.jws_results.getText()):
+                    finished = True
+                    self.logger.info('benchmark finished')
+            except ViewNotFoundException:
+                pass
+            except RuntimeError:
+                pass
+            except ValueError:
+                pass
+
+    def parseResult(self):
+        key_unit_hash = {
+            "N1": "MFLOPS",
+            "N2": "MFLOPS",
+            "N3": "MOPS",
+            "N4": "MOPS",
+            "N5": "MOPS",
+            "N6": "MFLOPS",
+            "N7": "MOPS",
+            "N8": "MOPS",
+            "MWIPS": "MFLOPS"
+        }
+
+        for line in self.jws_results.getText().split('\n'):
+            line = str(line.strip())
+            elements = re.split(r'\s+', line)
+            if line.startswith('MWIPS'):
+                units = key_unit_hash['MWIPS']
+                key = "MWIPS"
+                value = elements[1]
+            elif line.startswith('N'):
+                units = key_unit_hash[elements[0]]
+                key = "%s-%s" % (elements[0], elements[1])
+                value = elements[2]
+            else:
+                continue
+            self.report_result('javawhetstone-%s' % key, 'pass', value, units)
---
+++ automated/android/apk-automation/rl-sqlite.py
@@ -0,0 +1,61 @@
+import time
+from common import ApkTestRunner
+
+
+class ApkRunnerImpl(ApkTestRunner):
+    def __init__(self, config):
+        self.config = config
+        self.config['apk_file_name'] = 'RL_Benchmark_SQLite_v1.3.apk'
+        self.config['apk_package'] = 'com.redlicense.benchmark.sqlite'
+        self.config['activity'] = 'com.redlicense.benchmark.sqlite/.Main'
+        super(ApkRunnerImpl, self).__init__(self.config)
+
+    def execute(self):
+        self.dump_always()
+        btn_start = self.vc.findViewWithTextOrRaise(u'Start')
+        btn_start.touch()
+
+        finished = False
+        while(not finished):
+            self.dump_always()
+            overall_result = self.vc.findViewWithText(u'Overall')
+            if overall_result:
+                finished = True
+                self.logger.info("benchmark finished")
+
+    def __get_score_with_text(self, text):
+        found_score_view = False
+        while not found_score_view:
+            linear_layout = self.vc.findViewByIdOrRaise("com.redlicense.benchmark.sqlite:id/stats")
+            for ch in linear_layout.children:
+                subitem = self.vc.findViewWithText(text, ch)
+                if subitem:
+                    subitem_result = self.vc.findViewByIdOrRaise("com.redlicense.benchmark.sqlite:id/test_result", ch)
+                    score = subitem_result.getText().replace("sec", "").strip()
+                    score_in_ms = float(score) * 1000
+                    self.report_result("RL-sqlite-" + text.replace(" ", "-"), 'pass', str(score_in_ms), "ms")
+                    found_score_view = True
+                    break
+            if subitem is None:
+                self.logger.info("%s not found, need to pageup" % text)
+                self.device.press('DPAD_UP')
+                time.sleep(2)
+                self.device.press('DPAD_UP')
+                time.sleep(2)
+                self.dump_always()
+
+    def parseResult(self):
+        self.__get_score_with_text("Overall")
+        self.__get_score_with_text("DROP TABLE")
+        self.__get_score_with_text("DELETE with an index")
+        self.__get_score_with_text("DELETE without an index")
+        self.__get_score_with_text("INSERTs from a SELECT")
+        self.__get_score_with_text("25000 UPDATEs with an index")
+        self.__get_score_with_text("1000 UPDATEs without an index")
+        self.__get_score_with_text("5000 SELECTs with an index")
+        self.__get_score_with_text("Creating an index")
+        self.__get_score_with_text("100 SELECTs on a string comparison")
+        self.__get_score_with_text("100 SELECTs without an index")
+        self.__get_score_with_text("25000 INSERTs into an indexed table in a transaction")
+        self.__get_score_with_text("25000 INSERTs in a transaction")
+        self.__get_score_with_text("1000 INSERTs")
