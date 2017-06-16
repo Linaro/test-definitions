c0235206dd28e9c67cda6c6ca0216d1e43828018
Chase Qi <chase.qi@linaro.org>
2017-06-16 16:09:30 +0800

    linux: docker: replace docker-engine with docker.io
    Replace docker-engine with docker.io to fix
    https://bugs.linaro.org/show_bug.cgi?id=3044
    
    Change-Id: I3eab1c0ee7ed1e1fdd320043e9f9e0a6bf5703e7
    Signed-off-by: Chase Qi <chase.qi@linaro.org>

--- automated/linux/docker/docker.sh
+++ automated/linux/docker/docker.sh
@@ -25,7 +25,7 @@ create_out_dir "${OUTPUT}"
 dist_name
 # shellcheck disable=SC2154
 case "${dist}" in
-    debian|ubuntu) pkgs="docker-engine" ;;
+    debian|ubuntu) pkgs="docker.io" ;;
     fedora|centos) pkgs="docker" ;;
     *) error_msg "Unsupported distribution" ;;
 esac
