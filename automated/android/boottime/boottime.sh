bc98f34b2a70f3ffe7c2d73a2df062f00a62599e
Yongqin Liu <yongqin.liu@linaro.org>
2017-09-27 18:05:10 +0800

    andorid automated boottime: some improvements
    1. print more message for debug
       help to investigate why the analyse part is failed.
    
       some times the files collected lost when try to analyze like here:
       https://staging.validation.linaro.org/scheduler/job/191519#L13962
    
       seems some problem there with the logcat_all_1.log file
    2. add _avg suffix for the statistic result which we are interested in
    
    3. ignore the case when grep keyword from logcat or dmesg
    
    4. call disablesuspend.sh on device side
       to wait the launcher display log printed in logcat message
    
    Change-Id: I8006995397d407d7188a2137e8f4190bea86b4e0
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- automated/android/boottime/boottime.sh
+++ automated/android/boottime/boottime.sh
@@ -26,8 +26,13 @@ while getopts ":S:s:t:o:n:" o; do
   esac
 done
 
+PKG_DEPS="usbutils"
+install_deps "${PKG_DEPS}"
+
 initialize_adb
-wait_boot_completed "${BOOT_TIMEOUT}"
+# wait till the launcher displayed
+wait_homescreen "${BOOT_TIMEOUT}"
+
 create_out_dir "${OUTPUT}"
 install_deps 'curl tar xz-utils' "${SKIP_INSTALL}"
 
