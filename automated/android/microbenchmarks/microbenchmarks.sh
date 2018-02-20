8b5e87df5de4d631ea627f9df3fc1e575a41c982
Vishal Bhoj <vishal.bhoj@linaro.org>
2018-02-20 21:16:11 +0530

    android: microbenchmarks: reducing logging during run
    Change-Id: I47fb1bda78ab5a4445e810f8319e6dc6194837cd
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- automated/android/microbenchmarks/microbenchmarks.sh
+++ automated/android/microbenchmarks/microbenchmarks.sh
@@ -54,7 +54,7 @@ wget  -q "${SNAPSHOTS_URL}"/"${BUILD_TARBALL}"
 git config --global user.email "ci_notify@linaro.org"
 git config --global user.name "Linaro CI"
 git config --global --add color.ui auto
-tar -xvf "${BUILD_TARBALL}"
+tar -xf "${BUILD_TARBALL}"
 export PATH=${PWD}/out/host/linux-x86/bin/:${PATH}
 
 # FIXME removing latest adb from build since it is not working well from container
@@ -63,9 +63,13 @@ initialize_adb
 adb_root
 curl https://storage.googleapis.com/git-repo-downloads/repo > "${PWD}"/out/host/linux-x86/bin/repo
 chmod a+x "${PWD}"/out/host/linux-x86/bin/repo
-repo init -u https://android.googlesource.com/platform/manifest
+repo init -q -u https://android.googlesource.com/platform/manifest
 cp ../manifest.xml  .repo/manifest.xml
-repo sync -j16 -c
+repo sync -j16 -c -q --no-tags
+lava-test-case "test-progress" --result pass
+sed -i "s| /data/local/tmp/system| /data/local/tmp/system > /dev/null|g" scripts/benchmarks/benchmarks_run_target.sh
+sed -i "s| /data/art-test| /data/art-test > /dev/null|g" scripts/benchmarks/benchmarks_run_target.sh
+sed -i "s|mode \"\$1\"|mode \"\$1\" --noverbose|g" scripts/benchmarks/benchmarks_run_target.sh
 export OUT=${PWD}/out/target/product/${LUNCH_TARGET}/
 ./scripts/benchmarks/benchmarks_run_target.sh  --skip-build true --iterations "${ITERATIONS}" --mode "${MODE}"
 git clone https://git.linaro.org/people/vishal.bhoj/pbr.git; mkdir -p pbr/artifacts/
