dff9d5120ca946e0862448f4b7204a7d59d49f5d
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2018-02-22 08:44:41 +0000

    automated: workload-automation: allow for custom output path
    In order to make result postprocessing possible in the LAVA job, WA
    results can now be saved in the custom path that can be set as parameter
    
    Change-Id: If9faac3d20fa10297ed8dc65aa5b2f3a0dae0ea4
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

--- automated/android/workload-automation/workload-automation.sh
+++ automated/android/workload-automation/workload-automation.sh
@@ -3,8 +3,6 @@
 
 TEST_DIR=$(dirname "$(realpath "$0")")
 OUTPUT="${TEST_DIR}/output"
-RESULT_FILE="${OUTPUT}/result.txt"
-export RESULT_FILE
 SKIP_INSTALL="false"
 ANDROID_SERIAL=""
 BOOT_TIMEOUT="300"
@@ -22,7 +20,7 @@ usage() {
     exit 1
 }
 
-while getopts ":s:S:t:T:r:g:c:a:b:w:p:" opt; do
+while getopts ":s:S:t:T:r:g:c:a:b:w:p:o:" opt; do
     case "${opt}" in
         s) SKIP_INSTALL="${OPTARG}" ;;
         S) ANDROID_SERIAL="${OPTARG}" ;;
@@ -35,6 +33,7 @@ while getopts ":s:S:t:T:r:g:c:a:b:w:p:" opt; do
         b) BUILD_TOOLS_URL="${OPTARG}" ;;
         w) WA_HOME_URL="${OPTARG}" ;;
         p) PROBE="${OPTARG}" ;;
+        o) NEW_OUTPUT="${OPTARG}" ;;
         *) usage ;;
     esac
 done
@@ -43,7 +42,12 @@ done
 . "${TEST_DIR}/../../lib/android-test-lib"
 
 cd "${TEST_DIR}"
+if [ ! -z "${NEW_OUTPUT}" ]; then
+    OUTPUT="${NEW_OUTPUT}"
+fi
 create_out_dir "${OUTPUT}"
+RESULT_FILE="${OUTPUT}/result.txt"
+export RESULT_FILE
 
 if [ "${SKIP_INSTALL}" = "true" ] || [ "${SKIP_INSTALL}" = "True" ]; then
     info_msg "WA installation skipped"
