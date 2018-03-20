584800b4209a9f318afb74d0c13effbfb9a5050f
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2018-03-20 07:35:08 +0000

    workload-automation: only install adb if it's missing
    Test jobs in LAVA use adb from the latest Google SDK. Installing adb
    package breaks adb. This patch makes the package installation
    conditional.
    
    Change-Id: I97d875aad65127b675b2a12f34363f5030bb8002
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

--- automated/android/workload-automation/workload-automation.sh
+++ automated/android/workload-automation/workload-automation.sh
@@ -48,10 +48,12 @@ create_out_dir "${OUTPUT}"
 if [ "${SKIP_INSTALL}" = "true" ] || [ "${SKIP_INSTALL}" = "True" ]; then
     info_msg "WA installation skipped"
 else
-    PKGS="git wget zip tar xz-utils python python-yaml python-lxml python-setuptools python-numpy python-colorama python-pip sqlite3 lib32stdc++6 lib32z1 lib32gcc1 lib32ncurses5 aapt android-tools-adb time sysstat python-jinja2 curl"
+    PKGS="git wget zip tar xz-utils python python-yaml python-lxml python-setuptools python-numpy python-colorama python-pip sqlite3 lib32stdc++6 lib32z1 lib32gcc1 lib32ncurses5 aapt time sysstat python-jinja2 curl"
     ! check_root && error_msg "Please run this test as root."
     dpkg --add-architecture i386
     install_deps "${PKGS}"
+    # only install adb if it's not already available
+    which adb || install_deps adb
     pip install --upgrade --quiet pip && hash -r
     pip install --upgrade --quiet setuptools
     pip install --quiet pexpect pyserial pyyaml docutils python-dateutil
