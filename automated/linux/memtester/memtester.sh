196f6f7b463447991838370d279e553589749223
Chase Qi <chase.qi@linaro.org>
2018-05-03 17:14:27 +0800

    automated/linux: add memtester
    Change-Id: Ie534e88ab125fe9f6c57f99afb68ad1d42932d42
    Signed-off-by: Chase Qi <chase.qi@linaro.org>

---
+++ automated/linux/memtester/memtester.sh
@@ -0,0 +1,61 @@
+#!/bin/sh -e
+
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+export RESULT_FILE
+MEMORY='1M'
+ITERATIONS=1
+
+usage() {
+    echo "Usage: $0 [-s <true|false>] [-m memory] [-i iterations]" 1>&2
+    exit 1
+}
+
+while getopts "s:m:i:h" o; do
+  case "$o" in
+    s) SKIP_INSTALL="${OPTARG}" ;;
+    m) MEMORY="${OPTARG}" ;;
+    i) ITERATIONS="${OPTARG}" ;;
+    *) usage ;;
+  esac
+done
+
+install() {
+    dist_name
+    # shellcheck disable=SC2154
+    case "${dist}" in
+      debian|ubuntu) install_deps "memtester" "${SKIP_INSTALL}";;
+      unknown) warn_msg "Unsupported distro: package install skipped" ;;
+    esac
+}
+
+parser() {
+    test_log="$1"
+    if [ "${ITERATIONS}" -gt 1 ]; then
+        suffix="-iter$2"
+    else
+        suffix=""
+    fi
+
+    # The follow lines:
+    # Remove strings like '0^H^H^H^H^H^H^H^H^H^H^Htesting' between ':' and 'ok'.
+    # Remove redundant spaces.
+    # Replace ok with pass.
+    # Replace space in test case name with minus.
+    # Save test result like 'Stuck-Address pass' to result file.
+        sed 's/ *:.*ok/:pass/g' "${test_log}"\
+        | grep ':pass' \
+        | sed 's/^ *//g; s/ /-/g' \
+        | awk -v suffix="${suffix}" -F':' '{printf("%s%s %s\n",$1,suffix,$2)}' \
+        | tee -a "$RESULT_FILE"
+}
+
+create_out_dir "${OUTPUT}"
+install
+for i in $(seq "${ITERATIONS}"); do
+    output="${OUTPUT}/memtester-iter$i.txt"
+    memtester "${MEMORY}" 1 | tee "${output}"
+    parser "${output}" "$i"
+done
