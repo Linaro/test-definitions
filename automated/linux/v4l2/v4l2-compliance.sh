cb74c1fe7144c2cc4d6e9305fee5728e5f8cca0e
Chase Qi <chase.qi@linaro.org>
2018-09-14 15:27:37 +0800

    linux/v4l2-compliance: support to modprobe driver and install v4l-utils
    * Add hikey to supported board list.
    * Add support to modprobe driver module.
    * Try to install v4l-utils when v4l2-compliance command not found.
    * Set MEDIA_DEVICE to empty by default as old version v4l-utils has no
      support for media device test.
    * Bug fixes.
    
    Change-Id: I8518a06e04a1f842f4ebacf32c9b63eaf58ef5cb
    Signed-off-by: Chase Qi <chase.qi@linaro.org>

--- automated/linux/v4l2/v4l2-compliance.sh
+++ automated/linux/v4l2/v4l2-compliance.sh
@@ -2,32 +2,49 @@
 
 . ../../lib/sh-test-lib
 OUTPUT="$(pwd)/output"
-RESULT_FILE="${OUTPUT}/result.txt"
+export RESULT_FILE="${OUTPUT}/result.txt"
 TEST_SUITE="v4l2-compliance"
 
+VIDEO_DEVICE="/dev/video0"
+MEDIA_DEVICE=""
+VIDEO_DRIVER=""
+
 usage() {
-    echo "Usage: $0 [-d <video device>" 1>&2
+    echo "Usage: $0 [-d <video device> -m <media device> -D <video driver>" 1>&2
     exit 1
 }
 
-while getopts "d:h:m:" o; do
+while getopts "d:m:D:h" o; do
   case "$o" in
     d) VIDEO_DEVICE="${OPTARG}" ;;
-    m) MEDIA_DEVICE_NUM="${OPTARG}"
-       MEDIA_DEVICE="/dev/media${OPTARG}" ;;
+    m) MEDIA_DEVICE="${OPTARG}" ;;
+    D) VIDEO_DRIVER="${OPTARG}" ;;
     h|*) usage ;;
   esac
 done
 
 echo VIDEO_DEVICE="${VIDEO_DEVICE}"
 echo MEDIA_DEVICE="${MEDIA_DEVICE}"
-echo MEDIA_DEVICE_NUM="${MEDIA_DEVICE_NUM}"
+echo VIDEO_DRIVER="${VIDEO_DRIVER}"
 
 # Test run.
 create_out_dir "${OUTPUT}"
 
+# Try to install v4l-utils when v4l2-compliance not found.
+# install_deps() skips installation on unsupported distro
+# like OE based builds by default.
+command -v v4l2-compliance || install_deps "v4l-utils"
 command -v v4l2-compliance
 exit_on_fail "v4l2-existence-check"
+
+if [ -n "${VIDEO_DRIVER}" ] && ! lsmod | grep "${VIDEO_DRIVER%.*}"; then
+    check_root || error_msg "Please run this script as root to modprobe driver module!"
+    ln -s "$(find "/lib/modules/$(uname -r)" -name "${VIDEO_DRIVER}*")" \
+        "/lib/modules/$(uname -r)"
+    depmod -a
+    modprobe "${VIDEO_DRIVER%.*}" no_error_inj=1
+    exit_on_fail "modprobe-${VIDEO_DRIVER%.*}"
+fi
 
 if [ ! -z "${VIDEO_DEVICE}" ] && [ -e "${VIDEO_DEVICE}" ]; then
   info_msg "Running v4l2-compliance device test..."
@@ -42,7 +59,7 @@ fi
 if [ ! -z "${MEDIA_DEVICE}" ] && [ -e "${MEDIA_DEVICE}" ]; then
   info_msg "Running v4l2-compliance media test..."
   LOG_FILE="${OUTPUT}/${TEST_SUITE}-output.txt"
-  test_cmd="v4l2-compliance -v -m${MEDIA_DEVICE_NUM} 2>&1"
+  test_cmd="v4l2-compliance -v -m ${MEDIA_DEVICE_NUM} 2>&1"
   pipe0_status "${test_cmd}" "tee ${LOG_FILE}"
   check_return "v4l2-compliance"
 else
@@ -57,4 +74,4 @@ grep -e FAIL -e OK "${LOG_FILE}" | \
       -e 's/ /_/g' \
       -e 's/:_/ /' \
       -e 's/ OK/ PASS/' \
-      > "${RESULT_FILE}"
+      >> "${RESULT_FILE}"
