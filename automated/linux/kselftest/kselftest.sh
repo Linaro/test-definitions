7085aabae15012f516d024c4a214c4a7b816475f
Naresh Kamboju <naresh.kamboju@linaro.org>
2018-11-20 14:34:33 +0530

    kselftest: fix typo replace OPTARGS with OPTARG
    Change-Id: I39c7d5dba7627583b387fb4cb39c48b61110981b
    Signed-off-by: Naresh Kamboju <naresh.kamboju@linaro.org>

--- automated/linux/kselftest/kselftest.sh
+++ automated/linux/kselftest/kselftest.sh
@@ -61,7 +61,7 @@ while getopts "t:s:u:p:L:S:b:g:e:h" opt; do
            #  SKIPFILE="${SCRIPTPATH}/${SKIPFILE}"
            #fi
 
-           if [ -z "${OPTARGS}" ]; then
+           if [ -z "${OPTARG}" ]; then
              continue
            fi
            if [ -z "${OPTARG##*http*}" ]; then
