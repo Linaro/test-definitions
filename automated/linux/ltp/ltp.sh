502f154acdcf4d8a688ca709c724b77dc3e6a4f9
Anders Roxell <anders.roxell@linaro.org>
2019-04-03 05:31:02 +0200

    ltp: remove redundant 'exit 1'
    error_msg does 'exit 1' so the 'exit 1' after is redundant.
    
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- automated/linux/ltp/ltp.sh
+++ automated/linux/ltp/ltp.sh
@@ -60,7 +60,6 @@ while getopts "M:T:S:b:g:e:s:v:R:" arg; do
           # Download LTP skipfile from specified URL
           if ! wget "${OPTARG}" -O "${SKIPFILE_TMP}"; then
             error_msg "Failed to fetch ${OPTARG}"
-            exit 1
           fi
         elif [ "${OPTARG##*.}" = "yaml" ]; then
           # yaml skipfile; use skipgen to generate a skipfile
@@ -98,7 +97,6 @@ if [ -n "${SKIPFILE_YAML}" ]; then
     generate_skipfile
     if [ ! -f "${SKIPFILE_PATH}" ]; then
         error_msg "Skipfile ${SKIPFILE} does not exist";
-        exit 1
     fi
     SKIPFILE="-S ${SKIPFILE_PATH}"
 fi
