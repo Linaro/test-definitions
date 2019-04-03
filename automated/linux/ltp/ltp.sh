279b6889ed80541df41c7317cff5a484fc4c0408
Anders Roxell <anders.roxell@linaro.org>
2019-04-03 05:30:21 +0200

    ltp: fix shellcheck optargs
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- automated/linux/ltp/ltp.sh
+++ automated/linux/ltp/ltp.sh
@@ -86,6 +86,10 @@ while getopts "M:T:S:b:g:e:s:v:R:" arg; do
      # Slow machines need more timeout Default is 5min and multiply * MINUTES
      M) export LTP_TIMEOUT_MUL="${OPTARG}";;
      R) export PASSWD="${OPTARG}";;
+     *)
+        usage
+        error_msg "No flag ${OPTARG}"
+        ;;
   esac
 done
 
