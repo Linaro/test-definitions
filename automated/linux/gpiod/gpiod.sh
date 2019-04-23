4a32921b3e951c7103178399a7ac249ee20b2d49
Anders Roxell <anders.roxell@linaro.org>
2019-04-23 20:39:44 +0200

    automated: gpiod: remove prefix '-'
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- automated/linux/gpiod/gpiod.sh
+++ automated/linux/gpiod/gpiod.sh
@@ -7,6 +7,6 @@ gpiod-test 2>&1| tee tmp.txt
 sed 's/\[[0-9;]*m//g'  tmp.txt \
 	| grep '\[TEST\]' \
 	| sed 's/\[TEST\]//' \
-	| sed -r "s/'//g; s/^ *//; s/-//; s/[^a-zA-Z0-9]/-/g; s/--+/-/g; s/-PASS/ pass/; s/-FAIL/ fail/; s/-SKIP/ skip/;" 2>&1 \
+	| sed -r "s/'//g; s/^ *//; s/-//; s/[^a-zA-Z0-9]/-/g; s/--+/-/g; s/-PASS/ pass/; s/-FAIL/ fail/; s/-SKIP/ skip/; s/-//;" 2>&1 \
 	| tee -a result.txt
 rm tmp.txt
