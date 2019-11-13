2a390b78e5bbcf964caa9e132b69af301e304944
Zhao Zheng <zhao.zheng@nxp.com>
2019-11-13 18:21:38 +0800

    Update kselftest tarball
    Signed-off-by: Zhao Zheng <zhao.zheng@nxp.com>

--- automated/linux/kselftest/kselftest.sh
+++ automated/linux/kselftest/kselftest.sh
@@ -141,7 +141,8 @@ else
       wget "${TESTPROG_URL}" -O kselftest.tar.gz
     elif [ -n "${TESTPROG}" ]; then
       # Download and extract kselftest tarball.
-      wget http://testdata.validation.linaro.org/tests/kselftest/"${TESTPROG}" -O kselftest.tar.gz
+      #wget http://testdata.validation.linaro.org/tests/kselftest/"${TESTPROG}" -O kselftest.tar.gz
+      wget http://sun.ap.freescale.net/images/b27563/kselftest.tar.gz -O kselftest.tar.gz
     fi
     tar zxf "kselftest.tar.gz"
     # shellcheck disable=SC2164
