8a47c9df16cfccccba4cee601c8f7e769afaafa8
Yongqin Liu <yongqin.liu@linaro.org>
2019-12-01 18:25:29 +0800

    tradefed and noninteractive-tradefed: install different openjdk version
    for different android version cts/vts test.
    
    Now only aosp master version needs to use openjdk-11,
    other older versions need to use openjdk8
    
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- automated/android/tradefed/setup.sh
+++ automated/android/tradefed/setup.sh
@@ -6,7 +6,14 @@
 . ../../lib/android-test-lib
 
 JDK="openjdk-8-jdk-headless"
-PKG_DEPS="usbutils curl wget zip xz-utils python-lxml python-setuptools python-pexpect aapt lib32z1-dev libc6-dev-i386 lib32gcc1 libc6:i386 libstdc++6:i386 libgcc1:i386 zlib1g:i386 libncurses5:i386 python-dev python-protobuf protobuf-compiler python-virtualenv python-pip python-pexpect psmisc"
+if [ -n "${ANDROID_VERSION}" ] && echo "${ANDROID_VERSION}" | grep -q  "aosp-master"; then
+    # only use openjdk-11 for aosp master version
+    JDK="openjdk-11-jdk-headless"
+#elif 8.1/9.0/android10
+#   JDK="openjdk-8-jdk-headless"
+fi
+
+PKG_DEPS="coreutils usbutils curl wget zip xz-utils python-lxml python-setuptools python-pexpect aapt lib32z1-dev libc6-dev-i386 lib32gcc1 libc6:i386 libstdc++6:i386 libgcc1:i386 zlib1g:i386 libncurses5:i386 python-dev python-protobuf protobuf-compiler python-virtualenv python-pip python-pexpect psmisc"
 
 dist_name
 case "${dist}" in
