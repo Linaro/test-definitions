327be5237744c391d71398bc8e905cdf73993860
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2020-03-02 20:54:23 +0000

    test.sh: update check parameters to match sanity-check
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

--- test.sh
+++ test.sh
@@ -7,6 +7,6 @@ if [ -z "$1" ] || [ ! -e "test/Dockerfile.${1}" ]; then
     exit 1
 fi
 
-python3 validate.py -g -s SC1091
+python3 validate.py -p E501 W503 -s SC1091 SC2230
 docker build -f test/Dockerfile."${1}" -t erp-"${1}" . && docker run --rm -it -v "$(pwd)":/work erp-"${1}"
 
