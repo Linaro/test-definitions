cb86fba162fab52b0ad0ee7439f00f117b6258d3
Ryan Harkin <ryan.harkin@linaro.org>
2020-03-05 10:06:37 +0000

    iperf: split into client and server
    Split the iperf.sh script so it runs as either server of client, but not
    both.
    
    If the caller wishes to run the test with a local server, they must
    invoke the script twice.
    
    This commit also creates a YAML for the server.
    
    Additional parameters are added to the client to allow CPU affinity, or
    to reverse the test, so the client is the receiver instead of the
    transmitter.
    
    Signed-off-by: Ryan Harkin <ryan.harkin@linaro.org>

--- automated/linux/iperf/iperf.sh
+++ automated/linux/iperf/iperf.sh
@@ -5,27 +5,34 @@
 OUTPUT="$(pwd)/output"
 RESULT_FILE="${OUTPUT}/result.txt"
 LOGFILE="${OUTPUT}/iperf.txt"
-# Test localhost by default, which tests the efficiency of TCP/IP stack.
-# To test physical network bandwidth, specify remote test server with '-c'.
-# Execute 'iperf3 -s' on remote host to run iperf3 test server.
-SERVER="127.0.0.1"
+# If SERVER is blank, we are the server, otherwise
+# If we are the client, we set SERVER to the ipaddr of the server
+SERVER=""
 # Time in seconds to transmit for
 TIME="10"
 # Number of parallel client streams to run
 THREADS="1"
 # Specify iperf3 version for CentOS.
 VERSION="3.1.4"
+# By default, the client sends to the server,
+# Setting REVERSE="-R" means the server sends to the client
+REVERSE=""
+# CPU affinity is blank by default, meaning no affinity.
+# CPU numbers are zero based, eg AFFINITY="-A 0" for the first CPU
+AFFINITY=""
 
 usage() {
-    echo "Usage: $0 [-c server] [-t time] [-p number] [-v version] [-s true|false]" 1>&2
+    echo "Usage: $0 [-c server] [-t time] [-p number] [-v version] [-A cpu affinity] [-R] [-s true|false]" 1>&2
     exit 1
 }
 
-while getopts "c:t:p:v:s:h" o; do
+while getopts "A:c:t:p:v:s:Rh" o; do
   case "$o" in
+    A) AFFINITY="-A ${OPTARG}" ;;
     c) SERVER="${OPTARG}" ;;
     t) TIME="${OPTARG}" ;;
     p) THREADS="${OPTARG}" ;;
+    R) REVERSE="-R" ;;
     v) VERSION="${OPTARG}" ;;
     s) SKIP_INSTALL="${OPTARG}" ;;
     h|*) usage ;;
@@ -57,22 +64,31 @@ else
 fi
 
 # Run local iperf3 server as a daemon when testing localhost.
-[ "${SERVER}" = "127.0.0.1" ] && iperf3 -s -D
+if [ "${SERVER}" = "" ]; then
+    # We are running in server mode.
+    # Start the server and report pass/fail
+    cmd="iperf3 -s -D"
+    ${cmd}
+    if pgrep -f "${cmd}" > /dev/null; then
+        result="pass"
+    else
+        result="fail"
+    fi
+    echo "iperf3_server_started ${result}" | tee -a "${RESULT_FILE}"
+else
+    # We are running in client mode
+    # Run iperf test with unbuffered output mode.
+    stdbuf -o0 iperf3 -c "${SERVER}" -t "${TIME}" -P "${THREADS}" "${REVERSE}" "${AFFINITY}" 2>&1 \
+        | tee "${LOGFILE}"
 
-# Run iperf test with unbuffered output mode.
-stdbuf -o0 iperf3 -c "${SERVER}" -t "${TIME}" -P "${THREADS}" 2>&1 \
-    | tee "${LOGFILE}"
-
-# Parse logfile.
-if [ "${THREADS}" -eq 1 ]; then
-    grep -E "(sender|receiver)" "${LOGFILE}" \
-        | awk '{printf("iperf-%s pass %s %s\n", $NF,$7,$8)}' \
-        | tee -a "${RESULT_FILE}"
-elif [ "${THREADS}" -gt 1 ]; then
-    grep -E "[SUM].*(sender|receiver)" "${LOGFILE}" \
-        | awk '{printf("iperf-%s pass %s %s\n", $NF,$6,$7)}' \
-        | tee -a "${RESULT_FILE}"
+    # Parse logfile.
+    if [ "${THREADS}" -eq 1 ]; then
+        grep -E "(sender|receiver)" "${LOGFILE}" \
+            | awk '{printf("iperf_%s pass %s %s\n", $NF,$7,$8)}' \
+            | tee -a "${RESULT_FILE}"
+    elif [ "${THREADS}" -gt 1 ]; then
+        grep -E "[SUM].*(sender|receiver)" "${LOGFILE}" \
+            | awk '{printf("iperf_%s pass %s %s\n", $NF,$6,$7)}' \
+            | tee -a "${RESULT_FILE}"
+    fi
 fi
-
-# Kill iperf test daemon if any.
-pkill iperf3 || true
