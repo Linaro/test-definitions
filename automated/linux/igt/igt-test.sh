7060089d869c98328629ca8873d123c2448bdc12
Arthur She <arthur.she@linaro.org>
2020-05-26 15:36:21 -0700

    igt: skip Chamelium test when running a given test list
    igt-gpu-tools could run tests other than Chamelium test.
    If we run a given test list, we don't need to check Chamelium status.
    
    Signed-off-by: Arthur She <arthur.she@linaro.org>

--- automated/linux/igt/igt-test.sh
+++ automated/linux/igt/igt-test.sh
@@ -57,8 +57,10 @@ if [ -z "${IGT_DIR}" ] || [ -z "${TEST_LIST}" ]; then
     usage
 fi
 
-if [ "${TEST_LIST}" == "CHAMELIUM" ] && [ -z "${CHAMELIUM_IP}" ] || [ -z "${HDMI_DEV_NAME}" ]; then
-    usage
+if [ "${TEST_LIST}" = "CHAMELIUM" ]; then
+    if [ -z "${CHAMELIUM_IP}" ] || [ -z "${HDMI_DEV_NAME}" ]; then
+        usage
+    fi
 fi
 
 TEST_SCRIPT="${IGT_DIR}/scripts/run-tests.sh"
