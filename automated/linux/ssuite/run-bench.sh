Linaro/test-definitions

commit afc64fd6f7880616d1171c3ec519fa6e78b19231

ssuite: Fix results parser
Adding ssuite-testcase-name-<max/min/avg/std> pass measurements units in sec

Signed-off-by: Naresh Kamboju <naresh.kamboju@linaro.org>

automated/linux/ssuite/run-bench.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# shellcheck disable=SC1091
 . ../../lib/sh-test-lib
 OUTPUT="$(pwd)/output"
 RESULT_FILE="${OUTPUT}/result.txt"
@@ -184,8 +185,13 @@ run_test() {
 	# throughput--<workload1_name>--<scheduler1_name> fail
 	#
 	# <app_name>-startup--<workload_name>--<scheduler1_name> pass <real number> sec
-	#<app_name>-startup--<workload_name>--<scheduler1_name> fail
-	mv "${OUTPUT}/result_list.txt" "${RESULT_FILE}"
+	# <app_name>-startup--<workload_name>--<scheduler1_name> fail
+
+	# test-case-name-<max/min/avg/std> pass value sec
+	awk '{ print $1 "-max"" " $2 " " $3 " " $7 }' "${OUTPUT}"/result_list.txt 2>&1 | tee -a  "${RESULT_FILE}"
+	awk '{ print $1 "-min"" " $2 " " $4 " " $7 }' "${OUTPUT}"/result_list.txt 2>&1 | tee -a  "${RESULT_FILE}"
+	awk '{ print $1 "-avg"" " $2 " " $5 " " $7 }' "${OUTPUT}"/result_list.txt 2>&1 | tee -a  "${RESULT_FILE}"
+	awk '{ print $1 "-std"" " $2 " " $6 " " $7 }' "${OUTPUT}"/result_list.txt 2>&1 | tee -a  "${RESULT_FILE}"
 }
 
 ! check_root && error_msg "This script must be run as root"

commit 7758fda36dc4e29f924a835692f76a275d4dcd42

ssuite: fix error in generation of result file
Commit afc64fd6f78 ("ssuite: Fix results parser") mistakenly reverses
min and max when generating result file. This commit fixes this issue.

Fixes: afc64fd6f78 ("ssuite: Fix results parser")
Signed-off-by: Paolo Valente <paolo.valente@linaro.org>

automated/linux/ssuite/run-bench.sh
@@ -204,8 +204,8 @@ run_test() {
 	# form. To get a compliant file, we pick each statistic and
 	# put it in a separate line in the destination result file
 	# (${RESULT_FILE}, which is then parsed by LAVA).
-	awk '{ print $1 "-max"" " $2 " " $3 " " $7 }' "${OUTPUT}"/result_list.txt 2>&1 | tee -a  "${RESULT_FILE}"
-	awk '{ print $1 "-min"" " $2 " " $4 " " $7 }' "${OUTPUT}"/result_list.txt 2>&1 | tee -a  "${RESULT_FILE}"
+	awk '{ print $1 "-min"" " $2 " " $3 " " $7 }' "${OUTPUT}"/result_list.txt 2>&1 | tee -a  "${RESULT_FILE}"
+	awk '{ print $1 "-max"" " $2 " " $4 " " $7 }' "${OUTPUT}"/result_list.txt 2>&1 | tee -a  "${RESULT_FILE}"
 	awk '{ print $1 "-avg"" " $2 " " $5 " " $7 }' "${OUTPUT}"/result_list.txt 2>&1 | tee -a  "${RESULT_FILE}"
 	awk '{ print $1 "-std"" " $2 " " $6 " " $7 }' "${OUTPUT}"/result_list.txt 2>&1 | tee -a  "${RESULT_FILE}"
 }