459a73333cca17019f65157ee8c10a3460215ff2
Naresh Kamboju <naresh.kamboju@linaro.org>
2020-07-27 18:04:36 +0530

    linux: Add fdisk test case
    Adding fdisk test to create partitions on block device by using
    fdisk command and format it with the given file system type.
    
    These test cases using library functions from the file
    automated/lib/sh-test-lib
    
    Signed-off-by: Naresh Kamboju <naresh.kamboju@linaro.org>

---
+++ automated/linux/fdisk/fdisk.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+export RESULT_FILE
+SKIP_INSTALL="false"
+
+DEVICE=""
+FILESYSTEM="ext4"
+SIZES=""
+
+usage() {
+    echo "Usage: $0 [-d </dev/sdb>]
+                    [-f <ext4>]
+                    [-s <true|false>]
+                    [-z <+10G +5G>]" 1>&2
+    exit 1
+}
+
+# create_disk_partitions "/dev/sdb" "+5G +5G"
+create_disk_partitions() {
+    echo
+    echo "Creating partition: ${DEVICE}"
+    skip_list="format-disk-partitions"
+    partition_disk "${DEVICE}" "${SIZES}"
+    exit_on_fail "create-disk-partitions" "${skip_list}"
+}
+
+# format_disk_partitions "/dev/sdb" "ext4"
+format_disk_partitions() {
+    echo
+    echo "Format partitions of: ${DEVICE}"
+    format_partitions "${DEVICE}" "${FILESYSTEM}"
+    exit_on_fail "format-disk-partitions"
+}
+
+while getopts "d:f:s:z:" arg; do
+   case "$arg" in
+     d) DEVICE="${OPTARG}";;
+     f) FILESYSTEM="${OPTARG}" ;;
+     # SKIP_INSTALL is true in case of Open Embedded builds
+     # SKIP_INSTALL is false in case of Debian builds
+     s) SKIP_INSTALL="${OPTARG}";;
+     z) SIZES="${OPTARG}";;
+     *) usage ;;
+  esac
+done
+
+# Test run.
+[ -b "${DEVICE}" ] || error_msg "Please specify a block device with '-d'"
+! check_root && error_msg "You need to be root to run this script."
+create_out_dir "${OUTPUT}"
+
+info_msg "About to run fdisk tests ..."
+info_msg "Output directory: ${OUTPUT}"
+
+pkgs="fdisk e2fsprogs dosfstools"
+install_deps "${pkgs}" "${SKIP_INSTALL}"
+
+create_disk_partitions
+format_disk_partitions
