57edd4b21ef5e09e667ba40378d03c1a4242f231
Anders Roxell <anders.roxell@linaro.org>
2020-09-01 14:30:05 +0200

    add rteval suite
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

---
+++ automated/linux/rteval/rteval.sh
@@ -0,0 +1,200 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+
+DOWNLOAD_KERNEL="https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-5.7.tar.xz"
+TEST_PROGRAM=rteval
+TEST_PROG_VERSION=
+TEST_GIT_URL=https://kernel.googlesource.com/pub/scm/utils/rteval/rteval.git
+TEST_DIR="$(pwd)/${TEST_PROGRAM}"
+SKIP_INSTALL="false"
+DURATION="10m"
+
+usage() {
+	echo "\
+	Usage: [sudo] ./rteval.sh [-d <DURATION>] [-v <TEST_PROG_VERSION>] [-u <S_URL>] [-p <S_PATH>] [-s <true|false>]
+
+	<DURATION>:
+	Time in long will the test be running. DURATION can be set
+	to Xs, Xm, Xh, Xd.
+	s - seconds,
+	m - minutes,
+	h - hours,
+	d - days
+	default: 10m
+
+	<TEST_PROG_VERSION>:
+	If this parameter is set, then the ${TEST_PROGRAM} is cloned. In
+	particular, the version of the suite is set to the commit
+	pointed to by the parameter. A simple choice for the value of
+	the parameter is, e.g., HEAD. If, instead, the parameter is
+	not set, then the suite present in TEST_DIR is used.
+
+	<TEST_PROG_URL>:
+	If this parameter is set, then the ${TEST_PROGRAM} is cloned
+	from the URL in TEST_PROG_URL. Otherwise it is cloned from the
+	standard repository for the suite. Note that cloning is done
+	only if TEST_PROG_VERSION is not empty
+
+	<TEST_DIR>:
+	If this parameter is set, then the S suite is cloned to or
+	looked for in TEST_DIR. Otherwise it is cloned to $(pwd)/${TEST_PROGRAM}
+
+	<SKIP_INSTALL>:
+	If you already have it installed into the rootfs.
+	default: false"
+}
+
+while getopts "d:hk:p:u:s:v:" opt; do
+	case $opt in
+		d)
+			DURATION="$OPTARG"
+			;;
+		k)
+			DOWNLOAD_KERNEL="$OPTARG"
+			;;
+		u)
+			TEST_GIT_URL="$OPTARG"
+			;;
+		p)
+			if [[ "$OPTARG" != '' ]]; then
+				TEST_DIR="$OPTARG"
+			fi
+			;;
+		s)
+			SKIP_INSTALL="${OPTARG}"
+			;;
+		v)
+			TEST_PROG_VERSION="$OPTARG"
+			;;
+		h)
+			usage
+			exit 0
+			;;
+		*)
+			usage
+			exit 1
+			;;
+	esac
+done
+
+install() {
+	dist=
+	dist_name
+	case "${dist}" in
+		debian|ubuntu)
+			pkgs="curl git python3-dev python3-schedutils python3-ethtool python3-lxml python3-dmidecode rt-tests sysstat xz-utils bzip2 tar numactl build-essential flex bison bc elfutils openssl libssl-dev cpio libelf-dev binutils linux-libc-dev keyutils libaio-dev attr libpcap-dev lksctp-tools zlib1g-dev util-linux"
+			install_deps "${pkgs}" "${SKIP_INSTALL}"
+			;;
+		fedora|centos)
+			pkgs="curl git-core python3-devel python3-schedutils python3-ethtool python3-lxml python3-dmidecode sysstat numactl gcc flex bison bc make elfutils elfutils-libelf-devel openssl-devel libaio-devel libattr-devel libcap-devel lksctp-tools-devel zlib-devel"
+			install_deps "${pkgs}" "${SKIP_INSTALL}"
+			;;
+		# When build do not have package manager
+		# Assume dependencies pre-installed
+		*)
+			echo "Unsupported distro: ${dist}! Package installation skipped!"
+			;;
+	esac
+	openssl req -new -nodes -utf8 -sha256 -days 36500 -batch -x509 -config x509.genkey -outform PEM -out kernel_key.pem -keyout kernel_key.pem
+
+}
+
+install_rt_tests() {
+	dist=
+	dist_name
+	case "${dist}" in
+		debian|ubuntu)
+			pkgs="git build-essential libnuma-dev python3-dev"
+			install_deps "${pkgs}" "${SKIP_INSTALL}"
+			;;
+		fedora|centos)
+			pkgs="git-core make automake gcc gcc-c++ kernel-devel numactl-devel"
+			install_deps "${pkgs}" "${SKIP_INSTALL}"
+			;;
+		# When build do not have package manager
+		# Assume dependencies pre-installed
+		*)
+			echo "Unsupported distro: ${dist}! Package installation skipped!"
+			;;
+	esac
+	git clone https://git.kernel.org/pub/scm/utils/rt-tests/rt-tests.git
+	pushd rt-tests || exit
+	git checkout v1.8
+	make && make install
+	popd || exit
+	rm -rf rt-tests
+}
+
+get_test_program() {
+	if [[ "$TEST_PROG_VERSION" != "" && ( ! -d "$TEST_DIR" || -d "$TEST_DIR"/.git ) ]];
+	then
+		if [[ -d "$TEST_DIR"/.git ]]; then
+			echo Using repository "$PATH"
+		else
+			git clone "$TEST_GIT_URL" "$TEST_DIR"
+		fi
+
+		cd "$TEST_DIR" || exit 1
+		if [[ "$TEST_PROG_VERSION" != "" ]]; then
+			if ! git reset --hard "$TEST_PROG_VERSION"; then
+				echo Failed to set ${TEST_PROGRAM} to commit "$TEST_PROG_VERSION", sorry
+				exit 1
+			fi
+		else
+			echo Using "$PATH"
+		fi
+
+	else
+		if [[ ! -d "$TEST_DIR" ]]; then
+			echo No ${TEST_PROGRAM} suite in "$TEST_DIR", sorry
+			exit 1
+		fi
+		echo Assuming ${TEST_PROGRAM} is pre-installed in "$TEST_DIR"
+		cd "$TEST_DIR" || exit 1
+	fi
+}
+
+run_test() {
+
+	ln -s /bin/dmesg /usr/bin/dmesg
+	pushd "$TEST_DIR" || exit 1
+	pushd loadsource || exit 1
+	curl -sSOL "${DOWNLOAD_KERNEL}"
+	popd || exit
+	sed -ie "s|linux-.*|$(basename "${DOWNLOAD_KERNEL}")|" Makefile
+	make install
+	echo D="${DURATION}"
+	#sed -ie "s|^verbose: .*|verbose: True|g" rteval.conf
+	sed -ie "s|^duration: .*|duration: ${DURATION}|g" rteval.conf
+	echo "dbench: external">>rteval.conf
+	date
+
+	cat rteval.conf
+
+	date
+	make runit D="${DURATION}" 2>&1 | tee "${RESULT_FILE}"
+	date
+	popd || exit
+}
+
+! check_root && error_msg "This script must be run as root"
+
+# Install and run test
+
+if ! command -v cyclictest > /dev/null; then
+	install_rt_tests
+fi
+
+if [ "${SKIP_INSTALL}" = "true" ] || [ "${SKIP_INSTALL}" = "True" ]; then
+	info_msg "suite installation skipped altogether"
+else
+	install
+fi
+
+get_test_program
+create_out_dir "${OUTPUT}"
+run_test
