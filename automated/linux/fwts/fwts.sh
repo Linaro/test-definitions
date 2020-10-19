9bdd20014de503d43763354f42655ecf83d090bb
Naresh Kamboju <naresh.kamboju@linaro.org>
2020-10-19 13:00:36 +0200

    fwts: fix test name parameter
    Signed-off-by: Naresh Kamboju <naresh.kamboju@linaro.org>
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- automated/linux/fwts/fwts.sh
+++ automated/linux/fwts/fwts.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 # shellcheck disable=SC1091
 . ../../lib/sh-test-lib
 OUTPUT="$(pwd)/output"
@@ -168,7 +170,11 @@ parse_fwts_test_results() {
 
 run_test() {
 
-	fwts "${TESTS}" - 2>&1 | tee -a "${RESULT_LOG}"
+	# Double quote to prevent globbing and word splitting.
+	# In this case we don't want to add extra quote since that can make the
+	# string get splitted.
+	# shellcheck disable=SC2086
+	fwts ${TESTS} - 2>&1 | tee -a "${RESULT_LOG}"
 	parse_fwts_test_results
 }
 
