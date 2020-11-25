051fb3bda6714e7c61b8acdc38399e869750bfab
Yongqin Liu <yongqin.liu@linaro.org>
2020-11-25 18:38:38 +0800

    boottest: update to print dmesg
    to debug why fastboot devices not found
    
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- automated/android/boottest/boottest.sh
+++ automated/android/boottest/boottest.sh
@@ -19,18 +19,28 @@ create_out_dir "${OUTPUT}"
 echo "BOOT_TO_CONSOLE pass" > ${OUTPUT}/boot_result.txt
 
 initialize_adb # ANDROID_SERIAL exported here
-lsusb -v |tee output/lsusb-v-before-adb-root.txt
-adb_root
 # wait till boot completed
 wait_boot_completed "${BOOT_TIMEOUT}"
+
+lsusb -v |tee output/lsusb-v-before-adb-root.txt
+adb_root
 lsusb -v |tee output/lsusb-v-before-reboot.txt
 adb shell "echo u > /proc/sysrq-trigger"
 adb shell "echo b > /proc/sysrq-trigger"
-sleep 30
+echo "BOOT_REBOOT pass" > ${OUTPUT}/boot_result.txt
+sleep 60
 lsusb -v |tee output/lsusb-v-before-fastboot.txt
 fastboot devices
-fastboot boot /lava-lxc/*boot*.img
-adb wait-for-device
-lsusb -v |tee output/lsusb-v-after-booted.txt
-wait_boot_completed "${BOOT_TIMEOUT}"
-echo "BOOT_AGAIN_AFTER_REBOOT pass" > ${OUTPUT}/boot_result.txt
+num_fastboot_devices="$(fastboot devices |wc -l)"
+if [ "${num_fastboot_devices}" -ne 1 ]; then
+    dmesg |tail
+    dmesg > output/dmesg.log
+    echo "BOOT_AGAIN_AFTER_REBOOT fail" > ${OUTPUT}/boot_result.txt
+    error_msg "No fastboot devices listed"
+else
+    timeout 300 fastboot boot /lava-lxc/*boot*.img
+    timeout 300 adb wait-for-device
+    lsusb -v |tee output/lsusb-v-after-booted.txt
+    wait_boot_completed "${BOOT_TIMEOUT}"
+    echo "BOOT_AGAIN_AFTER_REBOOT pass" > ${OUTPUT}/boot_result.txt
+fi
