Linaro/test-definitions

commit ce20d323206f80f4633377d70d485faea0aad216

ssuite: update to use the common git clone of the test
Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

automated/linux/ssuite/run-bench.sh
@@ -7,10 +7,11 @@ RESULT_FILE="${OUTPUT}/result.txt"
 
 TESTS="throughput replayed-gnome-term-startup"
 TEST_DEV=sda
+TEST_PROGRAM="S"
 FORMAT=no
-S_VERSION=
-S_URL=https://github.com/Algodev-github/S
-S_PATH="$(pwd)/S"
+TEST_PROG_VERSION=
+TEST_GIT_URL=https://github.com/Algodev-github/S
+TEST_DIR="$(pwd)/${TEST_PROGRAM}"
 SKIP_INSTALL="false"
 ONLY_READS=""
 NUM_REPETITIONS=5
@@ -18,7 +19,7 @@ NUM_REPETITIONS=5
 usage() {
 	echo "\
 	Usage: [sudo] ./run-bench.sh [-t <TESTS>] [-d <TEST_DEV>] [-f <FORMAT>]
-				     [-v <S_VERSION>] [-u <S_URL>] [-p <S_PATH>]
+				     [-v <TEST_PROG_VERSION>] [-u <TEST_GIT_URL>] [-p <TEST_DIR>]
 				     [-r <ONLY_READS>] [-s <true|false>]
 				     [-n NUM_REPETITIONS]
 
@@ -51,22 +52,22 @@ usage() {
 	the drive, and that fs is used for the test.
 	Default value: no
 
-	<S_VERSION>:
-	If this parameter is set, then the S suite is cloned. In
+	<TEST_PROG_VERSION>:
+	If this parameter is set, then the ${TEST_PROGRAM} suite is cloned. In
 	particular, the version of the suite is set to the commit
 	pointed to by the parameter. A simple choice for the value of
 	the parameter is, e.g., HEAD. If, instead, the parameter is
-	not set, then the suite present in S_PATH is used.
+	not set, then the suite present in TEST_DIR is used.
 
-	<S_URL>:
-	If this parameter is set, then the S suite is cloned
-	from the URL in S_URL. Otherwise it is cloned from the
+	<TEST_GIT_URL>:
+	If this parameter is set, then the ${TEST_PROGRAM} suite is cloned
+	from the URL in TEST_GIT_URL. Otherwise it is cloned from the
 	standard repository for the suite. Note that cloning is done
-	only if S_VERSION is not empty
+	only if TEST_PROG_VERSION is not empty
 
-	<S_PATH>:
-	If this parameter is set, then the S suite is cloned to or
-	looked for in S_PATH. Otherwise it is cloned to $(pwd)/S
+	<TEST_DIR>:
+	If this parameter is set, then the ${TEST_PROGRAM} suite is cloned to or
+	looked for in TEST_DIR. Otherwise it is cloned to $(pwd)/${TEST_PROGRAM}
 
 	<ONLY_READS>:
 	If this parameter is set to yes then only workloads made of
@@ -93,16 +94,16 @@ while getopts "ht:d:f:p:u:v:s:r:n:" opt; do
 			FORMAT="$OPTARG"
 			;;
 		v)
-			S_VERSION="$OPTARG"
+			TEST_PROG_VERSION="$OPTARG"
 			;;
 		u)
 			if [[ "$OPTARG" != '' ]]; then
-				S_URL="$OPTARG"
+				TEST_GIT_URL="$OPTARG"
 			fi
 			;;
 		p)
 			if [[ "$OPTARG" != '' ]]; then
-				S_PATH="$OPTARG"
+				TEST_DIR="$OPTARG"
 			fi
 			;;
 		s)
@@ -147,35 +148,6 @@ install() {
 			echo "Unsupported distro: ${dist}! Package installation skipped!"
 			;;
 	esac
-}
-
-get_ssuite() {
-	if [[ "$S_VERSION" != "" && ( ! -d "$S_PATH" || -d "$S_PATH"/.git ) ]];
-	then
-		if [[ -d "$S_PATH"/.git ]]; then
-			echo Using repository "$PATH"
-		else
-			git clone "$S_URL" "$S_PATH"
-		fi
-
-		cd "$S_PATH" || exit 1
-		if [[ "$S_VERSION" != "" ]]; then
-			if ! git reset --hard "$S_VERSION"; then
-				echo Failed to set S to commit "$S_VERSION", sorry
-				exit 1
-			fi
-		else
-			echo Using "$PATH"
-		fi
-
-	else
-		if [[ ! -d "$S_PATH" ]]; then
-			echo No S suite in "$S_PATH", sorry
-			exit 1
-		fi
-		echo Assuming S is pre-installed in "$S_PATH"
-		cd "$S_PATH" || exit 1
-	fi
 }
 
 run_test() {
@@ -225,6 +197,6 @@ if [ "${SKIP_INSTALL}" = "true" ] || [ "${SKIP_INSTALL}" = "True" ]; then
 else
 	install
 fi
-get_ssuite
+get_test_program "${TEST_GIT_URL}" "${TEST_DIR}" "${TEST_PROG_VERSION}" "${TEST_PROGRAM}"
 create_out_dir "${OUTPUT}"
 run_test "$TESTS" "$TEST_DEV" "$FORMAT"

automated/linux/ssuite/ssuite-bench.yaml
@@ -61,18 +61,18 @@ params:
         # of the suite is set to the commit pointed to by the
         # parameter. A simple choice for the value of the parameter
         # is, e.g., HEAD.  If, instead, the parameter is
-        # not set, then the suite present in S_PATH is used.
-        S_VERSION: ""
+        # not set, then the suite present in TEST_DIR is used.
+        TEST_PROG_VERSION: ""
 
         # If next parameter is set, then the S suite is cloned
-        # from the URL in S_URL. Otherwise it is cloned from the
+        # from the URL in TEST_GIT_URL. Otherwise it is cloned from the
         # standard repository for the suite. Note that cloning is done
-        # only if S_VERSION is not empty
-        S_URL: ""
+        # only if TEST_PROG_VERSION is not empty
+        TEST_GIT_URL: ""
 
         # If next parameter is set, then the S suite is cloned to or
-        # looked for in S_PATH. Otherwise it is cloned to $(pwd)/S
-        S_PATH: ""
+        # looked for in TEST_DIR. Otherwise it is cloned to $(pwd)/S
+        TEST_DIR: ""
 
         # If next parameter is set to yes then only workloads made of
         # only reads are generated in every benchmark.
@@ -84,5 +84,5 @@ params:
 run:
     steps:
         - cd ./automated/linux/ssuite/
-        - ./run-bench.sh -t "${TESTS}" -d "${TEST_DEV}" -f "${FORMAT}" -v "${S_VERSION}" -s "${SKIP_INSTALL}" -r "${ONLY_READS}" -p "${S_PATH}" -n "${NUM_REPETITIONS}" -u "${S_URL}"
+        - ./run-bench.sh -t "${TESTS}" -d "${TEST_DEV}" -f "${FORMAT}" -v "${TEST_PROG_VERSION}" -s "${SKIP_INSTALL}" -r "${ONLY_READS}" -p "${TEST_DIR}" -n "${NUM_REPETITIONS}" -u "${TEST_GIT_URL}"
         - ../../utils/send-to-lava.sh ./output/result.txt

commit 39b2b324e33aa72c0e41b3ca7b91de555e3db8ee

ssuite: run-bench: fix S_PATH to TEST_DIR
Fixes: ce20d323206f ("ssuite: update to use the common git clone of the test")
Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

automated/linux/ssuite/run-bench.sh
@@ -162,7 +162,7 @@ run_test() {
 
 	rm -f ${HOME_DIR}/.S-config.sh
 
-	cd "$S_PATH"/run_multiple_benchmarks/ || exit 1
+	cd "$TEST_DIR"/run_multiple_benchmarks/ || exit 1
 	./run_main_benchmarks.sh "$1" "" "" "" "${ONLY_READS}" "" \
 				 "${NUM_REPETITIONS}" "${OUTPUT}" 2>&1 |\
 	    tee -a "${OUTPUT}/log"