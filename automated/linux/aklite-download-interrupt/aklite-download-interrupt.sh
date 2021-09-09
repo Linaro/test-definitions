04f690e4087799b040f2f3e79236b4989ded0352
Milosz Wasilewski <milosz.wasilewski@foundries.io>
2021-09-09 10:49:48 +0100

    automated: linux: remove redundant code from aklite-download test
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@foundries.io>

--- automated/linux/aklite-download-interrupt/aklite-download-interrupt.sh
+++ automated/linux/aklite-download-interrupt/aklite-download-interrupt.sh
@@ -7,33 +7,6 @@
 OUTPUT="$(pwd)/output"
 RESULT_FILE="${OUTPUT}/result.txt"
 export RESULT_FILE
-DEVICE_NAME=$(</etc/hostname)
-export DEVICE_NAME
-TARGET=""
-
-usage() {
-	echo "\
-	Usage: $0
-		     -t <target> [-n <device name>]
-
-	-n <device name>
-		This is the name of the registered device
-	-t <target>
-		This is the target that is expected to be active
-	"
-}
-
-while getopts "n:t:h" opts; do
-	case "$opts" in
-		n) DEVICE_NAME="${OPTARG}";;
-		t) TARGET="${OPTARG}";;
-		h|*) usage ; exit 1 ;;
-	esac
-done
-
-if [ -z "${TARGET}" ]; then
-    warn_msg "TARGET variable is missing"
-fi
 
 ! check_root && error_msg "You need to be root to run this script."
 create_out_dir "${OUTPUT}"
