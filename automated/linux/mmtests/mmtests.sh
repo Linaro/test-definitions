73e029fa14bca17569a6ff283f45c85cdf1ab17e
mattiamasella03 <86245743+mattiamasella03@users.noreply.github.com>
2021-09-09 12:03:35 +0200

    Update mmtests.sh

--- automated/linux/mmtests/mmtests.sh
+++ automated/linux/mmtests/mmtests.sh
@@ -121,7 +121,7 @@ prepare_system() {
 	pushd "${TEST_DIR}" || exit
 	PERL_MM_USE_DEFAULT=1
 	export PERL_MM_USE_DEFAULT
-	cpan -F JSON Cpanel::JSON::XS List::BinarySearch
+	cpan -f -i JSON Cpanel::JSON::XS List::BinarySearch
 	AUTO_PACKAGE_INSTALL=yes
 	export AUTO_PACKAGE_INSTALL
 	DOWNLOADED=0
