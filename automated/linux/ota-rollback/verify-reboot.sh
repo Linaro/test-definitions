05e54bc7444fe1f4a41c4aa24103a5e3271f49c9
Milosz Wasilewski <milosz.wasilewski@foundries.io>
2021-09-23 12:26:33 +0100

    automated: linux: merge OTA rollback tests
    This commit unifies OTA rollback for kernel and firmware. Since SPL
    rollback can't be reliably tested in an automated way, the test only
    exercises kernel and u-boot corruption as a source of rollback.
    
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@foundries.io>

--- automated/linux/ota-rollback/verify-reboot.sh
+++ automated/linux/ota-rollback/verify-reboot.sh
@@ -7,11 +7,42 @@
 OUTPUT="$(pwd)/output"
 RESULT_FILE="${OUTPUT}/result.txt"
 export RESULT_FILE
+TYPE="kernel"
+UBOOT_VAR_TOOL=fw_printenv
+export UBOOT_VAR_TOOL
+UBOOT_VAR_SET_TOOL=fw_setenv
+export UBOOT_VAR_SET_TOOL
 
-if [ -z "${UBOOT_VAR_TOOL}" ]; then
-    UBOOT_VAR_TOOL=fw_printenv
-    export UBOOT_VAR_TOOL
-fi
+usage() {
+    echo "\
+    Usage: $0 [-type <kernel|uboot>]
+
+    -t <kernel|uboot>
+        This determines type of corruption test
+        performed:
+        kernel: corrupt OTA updated kernel binary
+        uboot: corrupt OTA updated u-boot binary
+    -u <u-boot variable read tool>
+        Set the name of the tool to read u-boot variables
+        On the unsecured systems it will usually be
+        fw_printenv. On secured systems it might be
+        fiovb_printenv
+    -s <u-boot variable set tool>
+        Set the name of the tool to set u-boot variables
+        On the unsecured systems it will usually be
+        fw_setenv. On secured systems it might be
+        fiovb_setenv
+    "
+}
+
+while getopts "t:u:s:h" opts; do
+    case "$opts" in
+        t) TYPE="${OPTARG}";;
+        u) UBOOT_VAR_TOOL="${OPTARG}";;
+        s) UBOOT_VAR_SET_TOOL="${OPTARG}";;
+        h|*) usage ; exit 1 ;;
+    esac
+done
 
 # the script works only on builds with aktualizr-lite
 # and lmp-device-auto-register
@@ -19,22 +50,33 @@ fi
 ! check_root && error_msg "You need to be root to run this script."
 create_out_dir "${OUTPUT}"
 
+ref_bootcount_after_reboot=4
+ref_rollback_after_reboot=1
+ref_bootupgrade_available_after_reboot=0
+ref_upgrade_available_after_reboot=0
+ref_fiovb_is_secondary_boot_after_reboot=0
+
 # check u-boot variables
 bootcount_after_reboot=$(uboot_variable_value bootcount)
 echo "Bootcount: ${bootcount_after_reboot}"
 # bootcount should be 4 despite the fact there was no boot failures in u-boot
-compare_test_value "bootcount_after_reboot" 4 "${bootcount_after_reboot}"
+compare_test_value "bootcount_after_reboot" "${ref_bootcount_after_reboot}" "${bootcount_after_reboot}"
 rollback_after_reboot=$(uboot_variable_value rollback)
 echo "Rollback: ${rollback_after_reboot}"
-compare_test_value "rollback_after_reboot" 1 "${rollback_after_reboot}"
+compare_test_value "rollback_after_reboot" "${ref_rollback_after_reboot}" "${rollback_after_reboot}"
 bootupgrade_available_after_reboot=$(uboot_variable_value bootupgrade_available)
-compare_test_value "bootupgrade_available_after_reboot" 0 "${bootupgrade_available_after_reboot}"
+compare_test_value "bootupgrade_available_after_reboot" "${ref_bootupgrade_available_after_reboot}" "${bootupgrade_available_after_reboot}"
 upgrade_available_after_reboot=$(uboot_variable_value upgrade_available)
-compare_test_value "upgrade_available_after_reboot" 0 "${upgrade_available_after_reboot}"
+compare_test_value "upgrade_available_after_reboot" "${ref_upgrade_available_after_reboot}" "${upgrade_available_after_reboot}"
 
 . /usr/lib/firmware/version.txt
+# shellcheck disable=SC2154
+ref_bootfirmware_version_after_reboot="${bootfirmware_version}"
+if [ "${TYPE}" = "uboot" ]; then
+    ref_bootfirmware_version_after_reboot=0
+fi
 bootfirmware_version_after_reboot=$(uboot_variable_value bootfirmware_version)
 # shellcheck disable=SC2154
-compare_test_value "bootfirmware_version_after_reboot" "${bootfirmware_version}" "${bootfirmware_version_after_reboot}"
+compare_test_value "bootfirmware_version_after_reboot" "${ref_bootfirmware_version_after_reboot}" "${bootfirmware_version_after_reboot}"
 fiovb_is_secondary_boot_after_reboot=$(uboot_variable_value fiovb.is_secondary_boot)
-compare_test_value "fiovb_is_secondary_boot_after_reboot" 0 "${fiovb_is_secondary_boot_after_reboot}"
+compare_test_value "fiovb_is_secondary_boot_after_reboot" "${ref_fiovb_is_secondary_boot_after_reboot}" "${fiovb_is_secondary_boot_after_reboot}"
--- automated/linux/ota-rollback/verify-rollback.sh
+++ automated/linux/ota-rollback/verify-rollback.sh
@@ -7,31 +7,73 @@
 OUTPUT="$(pwd)/output"
 RESULT_FILE="${OUTPUT}/result.txt"
 export RESULT_FILE
+TYPE="kernel"
+UBOOT_VAR_TOOL=fw_printenv
+export UBOOT_VAR_TOOL
+UBOOT_VAR_SET_TOOL=fw_setenv
+export UBOOT_VAR_SET_TOOL
 
-if [ -z "${UBOOT_VAR_TOOL}" ]; then
-    UBOOT_VAR_TOOL=fw_printenv
-    export UBOOT_VAR_TOOL
-fi
+usage() {
+    echo "\
+    Usage: $0 [-type <kernel|uboot>]
+
+    -t <kernel|uboot>
+        This determines type of corruption test
+        performed:
+        kernel: corrupt OTA updated kernel binary
+        uboot: corrupt OTA updated u-boot binary
+    -u <u-boot variable read tool>
+        Set the name of the tool to read u-boot variables
+        On the unsecured systems it will usually be
+        fw_printenv. On secured systems it might be
+        fiovb_printenv
+    -s <u-boot variable set tool>
+        Set the name of the tool to set u-boot variables
+        On the unsecured systems it will usually be
+        fw_setenv. On secured systems it might be
+        fiovb_setenv
+    "
+}
+
+while getopts "t:u:s:h" opts; do
+    case "$opts" in
+        t) TYPE="${OPTARG}";;
+        u) UBOOT_VAR_TOOL="${OPTARG}";;
+        s) UBOOT_VAR_SET_TOOL="${OPTARG}";;
+        h|*) usage ; exit 1 ;;
+    esac
+done
 
 ! check_root && error_msg "You need to be root to run this script."
 create_out_dir "${OUTPUT}"
 
+ref_bootcount_after_rollback=4
+ref_rollback_after_rollback=1
+ref_bootupgrade_available_after_rollback=0
+ref_upgrade_available_after_rollback=0
+ref_fiovb_is_secondary_boot_after_rollback=0
+
 # check u-boot variables to ensure rollback happend
 bootcount_after_rollback=$(uboot_variable_value bootcount)
-compare_test_value "bootcount_after_rollback" 4 "${bootcount_after_rollback}"
+compare_test_value "bootcount_after_rollback" "${ref_bootcount_after_rollback}" "${bootcount_after_rollback}"
 rollback_after_rollback=$(uboot_variable_value rollback)
-compare_test_value "rollback_after_rollback" 1 "${rollback_after_rollback}"
+compare_test_value "rollback_after_rollback" "${ref_rollback_after_rollback}" "${rollback_after_rollback}"
 bootupgrade_available_after_rollback=$(uboot_variable_value bootupgrade_available)
-compare_test_value "bootupgrade_available_after_rollback" 0 "${bootupgrade_available_after_rollback}"
+compare_test_value "bootupgrade_available_after_rollback" "${ref_bootupgrade_available_after_rollback}" "${bootupgrade_available_after_rollback}"
 upgrade_available_after_rollback=$(uboot_variable_value upgrade_available)
-compare_test_value "upgrade_available_after_rollback" 0 "${upgrade_available_after_rollback}"
+compare_test_value "upgrade_available_after_rollback" "${ref_upgrade_available_after_rollback}" "${upgrade_available_after_rollback}"
 
 . /usr/lib/firmware/version.txt
+# shellcheck disable=SC2154
+ref_bootfirmware_version_after_rollback="${bootfirmware_version}"
+if [ "${TYPE}" = "uboot" ]; then
+    ref_bootfirmware_version_after_rollback=0
+fi
 bootfirmware_version_after_rollback=$(uboot_variable_value bootfirmware_version)
 # shellcheck disable=SC2154
-compare_test_value "bootfirmware_version_after_rollback" "${bootfirmware_version}" "${bootfirmware_version_after_rollback}"
+compare_test_value "bootfirmware_version_after_rollback" "${ref_bootfirmware_version_after_rollback}" "${bootfirmware_version_after_rollback}"
 fiovb_is_secondary_boot_after_rollback=$(uboot_variable_value fiovb.is_secondary_boot)
-compare_test_value "fiovb_is_secondary_boot_after_rollback" 0 "${fiovb_is_secondary_boot_after_rollback}"
+compare_test_value "fiovb_is_secondary_boot_after_rollback" "${ref_fiovb_is_secondary_boot_after_rollback}" "${fiovb_is_secondary_boot_after_rollback}"
 
 # for now ignore /etc/os-release
 cat /etc/os-release
