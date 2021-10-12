d90576391887619906bd3afa9c30e61c1678a259
Anders Roxell <anders.roxell@linaro.org>
2021-10-12 12:27:15 +0200

    linux: gpiod: mount debugfs if needed
    Debugfs needs to be mounted before running gpiod.
    
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- automated/linux/gpiod/gpiod.sh
+++ automated/linux/gpiod/gpiod.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+. ../../lib/sh-test-lib
 GPIOD_PATH=${1:-"/opt/libgpiod/bin/"}
 
 export PATH="${GPIOD_PATH}:$PATH"
+
+trap 'exit_cleanup' EXIT
+trap '_warn "interrupted, cleaning up..."; exit_cleanup; exit 1' INT
+
+mount_debugfs
+
 gpiod-test 2>&1| tee tmp.txt
 sed 's/\[[0-9;]*m//g'  tmp.txt \
 	| grep '\[TEST\]' \
