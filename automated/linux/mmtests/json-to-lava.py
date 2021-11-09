5d10e65cde089c9f3afbd3e1d9c7ca13bb5ef40e
Federico Gelmetti <federico.gelmo@gmail.com>
2021-11-09 19:52:05 +0100

    mmtests: export multiple json files into lava format
    mmtests/json-to-lava: add support for altreports and multiple json files
    
    Change the structure of the LAVA data to push to squad as following:
      <module_name>_<op>_<iteration>_<sample> pass <value>
    
    The test name field in the second position had been dropped, since the
    name of the origin folder is always something generic like "benchmark",
    therefore useless
    
    The <op> field now has a double function. If not prefixed with a "$",
    it represent the operation which generated the relative data, as it was before.
    If the content starts with "$", it now represent additional data to add
    at the top level of the corresponding json file.
    E.g, the line:
      dbench4tput_$Cmd_0_ZGJlbmNoIC1EIC9ob2 ... lLXRpbWUgMQo= pass 0
    represent the _Cmd field in the json, which data in the <sample> spot
    can be decoded from b64 to obtain the original command string.
    
    mmtests/mmtests.sh: add an associative array, in which the altreports
    to be exported into LAVA must be added for each test case.
    If only the main report is available for any testcase, there is no need
    to add anything to the altreport_mappings array.
    
    Signed-off-by: Federico Gelmetti <federico.gelmo@gmail.com>

--- automated/linux/mmtests/json-to-lava.py
+++ automated/linux/mmtests/json-to-lava.py
@@ -2,10 +2,12 @@
 
 # Converts mmtests test results from JSON format to a list of strings
 # with the following structure so LAVA can understand it:
-# <module_name>-<test_name>-<op>-<iteration>-<sample> pass <value>
+# <module_name>_<op>_<iteration>_<sample> pass <value>
+# <module_name>_<$field>_<none>_<value> pass <0>
 
 import argparse
 import json
+from base64 import b64encode
 
 
 def main(args):
@@ -13,7 +15,6 @@ def main(args):
     with open(args.json_file) as f:
         module = json.load(f)
         module_name = module["_ModuleName"].replace("Extract", "").lower()
-        test_name = module["_TestName"]
         data = module["_ResultData"]
         operations = list(data)
         iterations = len(data[operations[0]])
@@ -22,8 +23,15 @@ def main(args):
                 op_data = data[op][i]
                 values = op_data["Values"]
                 samples = op_data["SampleNrs"]
+                op.replace("_", "-")
                 for val, sample in zip(values, samples):
-                    print(f"{module_name}-{test_name}-{op}-{i}-{sample} pass {val}")
+                    print(f"{module_name}_{op}_{i}_{sample} pass {val}")
+        if "_Cmd" in module:
+            string = ""
+            for i in range(len(module["_Cmd"])):
+                string = string + module["_Cmd"][i]
+            b64string = b64encode(bytes(string, "utf-8")).decode("utf-8")
+            print(f"{module_name}_$Cmd_0_{b64string} pass {0}")
     exit(exitcode)
 
 
