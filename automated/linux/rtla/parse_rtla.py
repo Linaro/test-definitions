1924e7e88254fd256afaca495672def0df7f6845
Daniel Wagner <wagi@monom.org>
2022-06-29 17:42:39 +0200

    rtla: Add rtla osnoise test
    The rtla is a meta-tool that includes a set of commands that aims to
    analyze the real-time properties of Linux. But instead of testing
    Linux as a black box, rtla leverages kernel tracing capabilities to
    provide precise information about the properties and root causes of
    unexpected results.
    
    Add osnoise, which gives information about the operating system noise.
    Transform the output into a rt-tests conform JSON format. This allows
    us to reuse the rt-tests infrastructure to parse and generate the
    final results.
    
    Use sane defaults, we can allow to modifye the more interesting
    command line options such as --priority later.
    
    Signed-off-by: Daniel Wagner <wagi@monom.org>

---
+++ automated/linux/rtla/parse_rtla.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+import argparse
+import platform
+import json
+
+# Transform rtla hist out data into the same format as rt-tests
+# framework is producing.
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-r",
+        "--result-file",
+        dest="result_file",
+        required=True,
+        default="./result.txt",
+        help="Specify test result file.",
+    )
+    parser.add_argument(
+        "-t",
+        "--test-name",
+        required=True,
+        help="Specify test name.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        dest="output",
+        help="Specify output file.",
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def get_sysinfo():
+    sysinfo = {}
+
+    uname = platform.uname()
+    sysinfo["sysname"] = uname.system
+    sysinfo["nodename"] = uname.node
+    sysinfo["release"] = uname.release
+    sysinfo["version"] = uname.version
+    sysinfo["machine"] = uname.machine
+
+    realtime = 0
+    try:
+        with open("/sys/kernel/realtime", "r") as rfl:
+            realtime = int(rfl.read(1))
+    except IOError:
+        pass
+    sysinfo["realtime"] = realtime
+
+    return sysinfo
+
+
+def parse_histogram(col, sel, i, hist):
+    if i not in hist:
+        hist[i] = {}
+        hist[i]["histogram"] = {}
+
+    if col[0].endswith(":"):
+        name = col[0][:-1]
+        hist[i][name] = int(col[sel])
+    else:
+        hist[i]["histogram"][col[0]] = int(col[sel])
+
+
+def parse_osnoise(result_file):
+    data = {}
+    threads = {}
+    for line in result_file.readlines():
+        if line.startswith(" "):
+            continue
+
+        col = line.split()
+
+        num_cols = len(col) - 1
+
+        for i, sel in zip(range(0, num_cols), range(1, len(col), 1)):
+            parse_histogram(col, sel, i, threads)
+
+    data["file_version"] = 2
+    data["return_code"] = 0
+    data["sysinfo"] = get_sysinfo()
+    data["num_threads"] = num_cols
+    data["resolution_in_ns"] = 0
+    data["thread"] = threads
+
+    return data
+
+
+def main(args):
+    with open(args.result_file, "r") as infile:
+        if args.test_name == "osnoise":
+            data = parse_osnoise(infile)
+
+    if args.output:
+        with open(args.output, "w") as outfile:
+            outfile.write(json.dumps(data, indent=2))
+    else:
+        print(json.dumps(data, indent=2))
+
+
+if __name__ == "__main__":
+    main(parse_args())
---
+++ automated/linux/rtla/rtla-osnoise.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+# RTLA provides a set of tools for the analysis of the kernel’s
+# realtime behavior on specific hardware.
+
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+
+OUTPUT="$(pwd)/output"
+TMPFILE="${OUTPUT}/rtla-osnoise.txt"
+LOGFILE="${OUTPUT}/rtla-osnoise.json"
+RESULT_FILE="${OUTPUT}/result.txt"
+
+DURATION="1m"
+BACKGROUND_CMD=""
+
+usage() {
+    echo "Usage: $0 [-d duration ] [-w background_cmd]" 1>&2
+    exit 1
+}
+
+while getopts ":d:w:" opt; do
+    case "${opt}" in
+        d) DURATION="${OPTARG}" ;;
+	w) BACKGROUND_CMD="${OPTARG}" ;;
+        *) usage ;;
+    esac
+done
+
+! check_root && error_msg "Please run this script as root."
+create_out_dir "${OUTPUT}"
+
+background_process_start bgcmd --cmd "${BACKGROUND_CMD}"
+
+# real-time priority FIFO:1, on all CPUs, for 900ms at each period (1s by default)
+rtla osnoise hist -P F:1 -r 900000 -d "${DURATION}" --no-header --trace \
+     -e osnoise:irq_noise \
+     --trigger hist:key=cpu,desc,duration.buckets=1000:sort=duration \
+     -e osnoise:thread_noise \
+     --trigger hist:key=cpu,comm,duration.buckets=1000:sort=duration \
+     -e osnoise:sample_threshold \
+     --trigger 'hist:key=cpu,duration.buckets=1000:sort=duration if interference == 0' \
+    | tee -a "${TMPFILE}"
+
+background_process_stop bgcmd
+
+mv osnoise_thread_noise_hist.txt "${OUTPUT}"
+mv osnoise_irq_noise_hist.txt "${OUTPUT}"
+mv osnoise_sample_threshold_hist.txt "${OUTPUT}"
+# Parse test log.
+./parse_rtla.py -t osnoise -r "${TMPFILE}" -o "${LOGFILE}"
+../../lib/parse_rt_tests_results.py rtla-osnoise "${LOGFILE}" \
+    | tee -a "${RESULT_FILE}"
