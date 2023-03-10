Linaro/test-definitions

commit 33dbc2733427eb818c61f70940a9de485931e918

upload-to-artifactorial.sh: Allow to check for failures and to print debug information
Change-Id: Ie011cb2cd899dac938066ca4eee7652b83ac38d4
Signed-off-by: Karsten Tausche <karsten@fairphone.com>

automated/utils/upload-to-artifactorial.sh
@@ -3,17 +3,30 @@
 ATTACHMENT=""
 ARTIFACTORIAL_URL=""
 ARTIFACTORIAL_TOKEN=""
+CURL_VERBOSE_FLAG=""
+FAILURE_RETURN_VALUE=0
 
 usage() {
-    echo "Usage: $0 [-a <attachment>] [-u <artifactorial_url>] [-t <artifactorial_token>]" 1>&2
+    echo "Usage: $0 [-a <attachment>] [-u <artifactorial_url>] [-t <artifactorial_token>] [-v] [-r]" 1>&2
+    echo "  -a attachment           Path to the file to upload" 1>&2
+    echo "  -u artifactorial_url    URL to the folder in Artifactorial to upload to" 1>&2
+    echo "  -t artifactorial_token  Access token for Artifactorial. This script will try to fetch" 1>&2
+    echo "                          the token from (lava_test_dir)/secrets. If this file exists," 1>&2
+    echo "                          it overrides this parameter." 1>&2
+    echo "  -v      Pass -v (verbose) flag to curl for debugging." 1>&2
+    echo "  -r      Report failure. If the upload fails and this flag is set, the script will exit" 1>&2
+    echo "          with return value 1. If the upload is skipped (no URL or no token specified)," 1>&2
+    echo "          this script will still return 0." 1>&2
     exit 1
 }
 
-while getopts ":a:u:t:" opt; do
+while getopts ":a:u:t:vr" opt; do
     case "${opt}" in
         a) ATTACHMENT="${OPTARG}" ;;
         u) ARTIFACTORIAL_URL="${OPTARG}" ;;
         t) ARTIFACTORIAL_TOKEN="${OPTARG}" ;;
+        v) CURL_VERBOSE_FLAG="-v" ;;
+        r) FAILURE_RETURN_VALUE=1 ;;
         *) usage ;;
     esac
 done
@@ -39,14 +52,18 @@ if which lava-test-reference; then
         which lava-test-case && lava-test-case "test-attachment" --result "skip"
         exit 0
     else
-        return=$(curl -F "path=@${ATTACHMENT}" -F "token=${ARTIFACTORIAL_TOKEN}" "${ARTIFACTORIAL_URL}")
+        return=$(curl "${CURL_VERBOSE_FLAG}" -F "path=@${ATTACHMENT}" -F "token=${ARTIFACTORIAL_TOKEN}" "${ARTIFACTORIAL_URL}")
     fi
 
-    if echo "${return}" | grep "$(basename "${ATTACHMENT}")"; then
+    attachmentBasename="$(basename "${ATTACHMENT}")"
+    if echo "${return}" | grep "${attachmentBasename}"; then
         lava-test-reference "test-attachment" --result "pass" --reference "${return}"
     else
         echo "test-attachment fail"
+        echo "Expected the basename of the attachment file (\"${attachmentBasename}\") to be part \
+of the Artifactorial URL, but curl returned \"${return}\"."
         which lava-test-case && lava-test-case "test-attachment" --result "fail"
+        exit "${FAILURE_RETURN_VALUE}"
     fi
 else
     echo "test-attachment skip"

commit 3868fa50fa0440f9eb538a7a09d732483382243b

utils: fix bad/illegal format or missing URL warning from curl
CURL_VERBOSE_FLAG="" by default. `curl "" ...` works but reports
`bad/illegal format or missing URL` warning. Removing the double quotes
on the variable fixes the issue.

The warning message can be different with old version curl. On Ubuntu
Bionic, 'curl: (3) <url> malformed' is reported.

Fixes: 33dbc2733427 ("upload-to-artifactorial.sh: Allow to check for failures and to print debug information")
Signed-off-by: Chase Qi <chase.qi@linaro.org>

automated/utils/upload-to-artifactorial.sh
@@ -52,7 +52,7 @@ if command -v lava-test-reference > /dev/null 2>&1; then
         command -v lava-test-case > /dev/null 2>&1 && lava-test-case "test-attachment" --result "skip"
         exit 0
     else
-        return=$(curl "${CURL_VERBOSE_FLAG}" -F "path=@${ATTACHMENT}" -F "token=${ARTIFACTORIAL_TOKEN}" "${ARTIFACTORIAL_URL}")
+        return=$(curl ${CURL_VERBOSE_FLAG} -F "path=@${ATTACHMENT}" -F "token=${ARTIFACTORIAL_TOKEN}" "${ARTIFACTORIAL_URL}")
     fi
 
     attachmentBasename="$(basename "${ATTACHMENT}")"