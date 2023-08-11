7154752e30a1b1549d7807eb616d070d7ed6174b
Anders Roxell <anders.roxell@linaro.org>
2023-08-11 10:53:21 +0200

    android: noninteractive-tradefed: readd install_deps
    Readd install_deps so the test can install the correct dependencies
    without having to find them.
    Add a python version check that errors out if the python version isn't
    >=3.8.
    
    Fixes: 2caa92bc01d8 ("noninteractive-tradefed: change to work with python3")
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- automated/android/noninteractive-tradefed/setup.sh
+++ automated/android/noninteractive-tradefed/setup.sh
@@ -5,15 +5,23 @@
 . ../../lib/sh-test-lib
 . ../../lib/android-test-lib
 
+SKIP_INSTALL=${1:-"false"}
+JDK="openjdk-11-jdk-headless"
 java_path="/usr/lib/jvm/java-11-openjdk-amd64/bin/java"
 if [ -n "${ANDROID_VERSION}" ] && echo "${ANDROID_VERSION}" | grep -q  "aosp-android14"; then
     # use openjdk-17 for Android14+ versions
+    JDK="openjdk-17-jdk-headless"
     java_path="/usr/lib/jvm/java-17-openjdk-amd64/bin/java"
 fi
+
+PKG_DEPS="aapt apt-utils bzip2 coreutils curl git lib32gcc-s1-amd64-cross libcurl4 protobuf-compiler psmisc python3 python-is-python3 python3-lxml python3-pexpect python3-protobuf python3-setuptools sudo tar unzip usbutils wget xz-utils zip "
 
 dist_name
 case "${dist}" in
     ubuntu)
+        dpkg --add-architecture i386
+        apt-get update -q
+        install_deps "${PKG_DEPS} ${JDK}" "${SKIP_INSTALL}"
         # make sure to use the right java version
         update-alternatives --set java ${java_path}
         ;;
@@ -22,6 +30,10 @@ case "${dist}" in
         ;;
 esac
 
+# Only aosp-master need the python version check.
+if [ -n "${ANDROID_VERSION}" ] && echo "${ANDROID_VERSION}" | grep -q  "aosp-master"; then
+    chech_python_version "$(python --verison)" "3.8" "Error"
+fi
 install_latest_adb
 initialize_adb
 adb_root
