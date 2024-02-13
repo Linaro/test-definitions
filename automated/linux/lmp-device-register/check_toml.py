ae479834d6e6524811bcdf63d0cda06b96025523
Milosz Wasilewski <milosz.wasilewski@foundries.io>
2024-02-13 11:50:38 +0000

    automated: linux: lmp-device-register check toml file
    When lmp-device-register runs it creates sota.toml file from the values
    returned by server. This patch adds a check to confirm whether the file
    is a valid TOML.
    
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@foundries.io>

---
+++ automated/linux/lmp-device-register/check_toml.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+
+
+def main(toml_file):
+    info = sys.version_info
+    if info.major >= 3 and info.minor >= 11:
+        import tomllib as tlib
+    else:
+        import toml as tlib
+
+    with open(toml_file, "rb") as f:
+        tlib.load(f)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--toml-file", required=True)
+    args = parser.parse_args()
+    main(args.toml_file)
