0363c184a88f29a050c532db69cae84bd7f0db13
Konstantin Belov <konstantin.belov@linaro.org>
2024-03-05 19:51:54 +0100

    automated: linux: mmtests: removed results processing
    Removed all parts related to results extraction and processing.
    
    Signed-off-by: Konstantin Belov <konstantin.belov@linaro.org>

--- automated/linux/mmtests/mmtests.sh
+++ automated/linux/mmtests/mmtests.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# Sysbench is a modular, cross-platform and multi-threaded benchmark tool.
-# This runs the CPU microbench which calculates primes up to a
-# certain value using varying numbers of instances.
-# In this configuration, it calculates primes up to 25000
-# using between 1 and 2*NUMCPUS threads.
+
+set +x
 
 # shellcheck disable=SC1091
 . ../../lib/sh-test-lib
@@ -15,17 +12,13 @@ TEST_GIT_URL=https://github.com/gormanm/mmtests
 TEST_DIR="$(pwd)/${TEST_PROGRAM}"
 SKIP_INSTALL="false"
 MMTESTS_MAX_RETRIES=10
-MMTESTS_TYPE_NAME=
 MMTESTS_CONFIG_FILE=
-
-declare -A altreport_mappings=( ["dbench4"]="tput latency opslatency")
-declare -A env_variable_mappings=( ["dbench4"]="DBENCH" )
 
 usage() {
 	echo "\
 	Usage: $0 [-s <true|false>] [-v <TEST_PROG_VERSION>]
 			[-u <TEST_GIT_URL>] [-p <TEST_DIR>]
-			[-c <MMTESTS_CONFIG_FILE>] [-t <MMTESTS_TYPE_NAME>] [-r <MMTESTS_MAX_RETRIES>]
+			[-c <MMTESTS_CONFIG_FILE>] [-r <MMTESTS_MAX_RETRIES>]
 
 	<TEST_PROG_VERSION>:
 	If this parameter is set, then the ${TEST_PROGRAM} suite is cloned. In
@@ -52,9 +45,6 @@ usage() {
 	Mmtests configuration file that describes how the benchmarks should be
 	configured and executed.
 
-	<MMTESTS_TYPE_NAME>:
-	Mtests test type, e.g. sysbenchcpu, iozone, sqlite, etc.
-
 	<MMTESTS_MAX_RETRIES>:
 	Maximum number of retries for the single benchamrk's source file download"
 	exit 1
@@ -75,9 +65,6 @@ while getopts "c:p:r:s:t:u:v:" opt; do
 			;;
 		s)
 			SKIP_INSTALL="${OPTARG}"
-			;;
-		t)
-			MMTESTS_TYPE_NAME="${OPTARG}"
 			;;
 		u)
 			if [[ "$OPTARG" != '' ]]; then
@@ -138,59 +125,8 @@ prepare_system() {
 
 run_test() {
 	pushd "${TEST_DIR}" || exit
-	info_msg "Running ${MMTESTS_TYPE_NAME} test..."
+	info_msg "Running ${MMTESTS_CONFIG_FILE} test..."
 	./run-mmtests.sh --no-monitor --config "${MMTESTS_CONFIG_FILE}" benchmark
-
-	MEMTOTAL_BYTES=$(free -b | grep Mem: | awk '{print $2}')
-	export MEMTOTAL_BYTES
-	NUMCPUS=$(grep -c '^processor' /proc/cpuinfo)
-	export NUMCPUS
-	NUMNODES=$(grep ^Node /proc/zoneinfo | awk '{print $2}' | sort | uniq | wc -l)
-	export NUMNODES
-	LLC_INDEX=$(find /sys/devices/system/cpu/ -type d -name "index*" | sed -e 's/.*index//' | sort -n | tail -1)
-	export LLC_INDEX
-	NUMLLCS=$(grep . /sys/devices/system/cpu/cpu*/cache/index"$LLC_INDEX"/shared_cpu_map | awk -F : '{print $NF}' | sort | uniq | wc -l)
-	export NUMLLCS
-
-	chmod u+x ./"${MMTESTS_CONFIG_FILE}"
-	eval 'source ./${MMTESTS_CONFIG_FILE}'
-
-	if [ "${MMTESTS_TYPE_NAME}" != "${MMTESTS}" ]; then
-		EXTRACT_NAMES="${MMTESTS}"
-	else
-		EXTRACT_NAMES="${MMTESTS_TYPE_NAME}"
-	fi
-
-	echo "test[s] to extract: ${EXTRACT_NAMES}"
-	for test in ${EXTRACT_NAMES}; do
-		echo "extraction for test: $test"
-		./bin/extract-mmtests.pl -d work/log/ -b "${test}" -n benchmark --print-json >> "../${MMTESTS_TYPE_NAME}_${test}.json"
-
-		altreports=${altreport_mappings[${test}]}
-		for altreport in ${altreports}; do
-			./bin/extract-mmtests.pl -d work/log/ -b "${test}" -n benchmark\
-			-a "${altreport}" --print-json > "../${MMTESTS_TYPE_NAME}_${test}${altreport}.json"
-		done
-	done
-
-	env_variables_prefix=${env_variable_mappings[${MMTESTS_TYPE_NAME}]}
-	if [ -z "$env_variables_prefix" ]; then
-		env_variables_prefix=${MMTESTS_TYPE_NAME^^}
-	fi
-	
-	vars=""
-	eval 'vars=${!'"$env_variables_prefix"'*}'
-	for variable in ${vars}; do
-		mykey=CONFIG_${variable}
-		mykey=${mykey//_/-}
-		myvalue=${!variable}
-		echo "$mykey":"$myvalue"
-		tmp=$(mktemp)
-		jq -c --arg key "$mykey" --arg value "$myvalue" '. += {($key):$value}' ../"${MMTESTS_TYPE_NAME}"_"${test}".json > "$tmp" && mv "$tmp" ../"${MMTESTS_TYPE_NAME}"_"${test}".json
-	done
-
-	chmod a+r "../$MMTESTS_TYPE_NAME"*".json"
-
 	popd || exit
 }
 
@@ -198,7 +134,7 @@ run_test() {
 
 # Test installation.
 if [ "${SKIP_INSTALL}" = "true" ] || [ "${SKIP_INSTALL}" = "True" ]; then
-	info_msg "${MMTESTS_TYPE_NAME} installation skipped"
+	info_msg "${MMTESTS_CONFIG_FILE} installation skipped"
 else
 	install
 fi
