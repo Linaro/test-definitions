eb372e663ee5e72ba604fb9803ea14d434f8269b
Anders Roxell <anders.roxell@linaro.org>
2025-02-19 10:24:58 +0100

    automated: linux: pmqtest: add iterations
    This will make it possible to run multiple iterations to gather multiple
    min/max/avg values.
    
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- automated/linux/pmqtest/pmqtest.sh
+++ automated/linux/pmqtest/pmqtest.sh
@@ -6,20 +6,22 @@
 
 TEST_DIR=$(dirname "$(realpath "$0")")
 OUTPUT="${TEST_DIR}/output"
-LOGFILE="${OUTPUT}/pmqtest.json"
+LOGFILE="${OUTPUT}/pmqtest"
 RESULT_FILE="${OUTPUT}/result.txt"
 DURATION="5m"
 BACKGROUND_CMD=""
+ITERATIONS=1
 
 usage() {
-    echo "Usage: $0 [-D duration] [-w background_cmd]" 1>&2
+    echo "Usage: $0 [-D duration] [-w background_cmd] [-i Iterations]" 1>&2
     exit 1
 }
 
-while getopts ":D:w:" opt; do
+while getopts ":D:w:i" opt; do
     case "${opt}" in
         D) DURATION="${OPTARG}" ;;
 	w) BACKGROUND_CMD="${OPTARG}" ;;
+        i) ITERATIONS="${OPTARG}" ;;
         *) usage ;;
     esac
 done
@@ -38,10 +40,19 @@ fi
 
 background_process_start bgcmd --cmd "${BACKGROUND_CMD}"
 
-"${binary}" -q -S -p 98 -D "${DURATION}" --json="${LOGFILE}"
+counter=0
+while [ ${counter} -lt ${ITERATIONS} ]; do
+    "${binary}" -q -S -p 98 -D "${DURATION}" --json="${LOGFILE}-${counter}.json"
+    counter=$((counter+1))
+done
 
 background_process_stop bgcmd
 
 # Parse test log.
-../../lib/parse_rt_tests_results.py pmqtest "${LOGFILE}" \
-    | tee -a "${RESULT_FILE}"
+counter=0
+while [ ${counter} -lt ${ITERATIONS} ]; do
+    cat "${LOGFILE}-${counter}.json"
+    ../../lib/parse_rt_tests_results.py pmqtest "${LOGFILE}-${counter}.json" \
+        | tee -a "${RESULT_FILE}"
+    counter=$((counter+1))
+done
