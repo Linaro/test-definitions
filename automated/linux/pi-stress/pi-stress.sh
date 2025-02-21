970fdbc85abdc7d649d6f5cc8c1503e05ee42d57
Anders Roxell <anders.roxell@linaro.org>
2025-02-21 19:52:41 +0100

    automated: linux: pi-stress: add iterations
    This will make it possible to run multiple iterations to gather multiple
    min/max/avg values.
    
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- automated/linux/pi-stress/pi-stress.sh
+++ automated/linux/pi-stress/pi-stress.sh
@@ -7,7 +7,7 @@
 . ../../lib/sh-test-lib
 
 OUTPUT="$(pwd)/output"
-LOGFILE="${OUTPUT}/pi-stress.json"
+LOGFILE="${OUTPUT}/pi-stress"
 RESULT_FILE="${OUTPUT}/result.txt"
 export RESULT_FILE
 
@@ -15,18 +15,20 @@ DURATION="5m"
 MLOCKALL="false"
 RR="false"
 BACKGROUND_CMD=""
+ITERATIONS=1
 
 usage() {
-    echo "Usage: $0 [-D runtime] [-m <true|false>] [-r <true|false>] [-w background_cmd]" 1>&2
+    echo "Usage: $0 [-D runtime] [-m <true|false>] [-r <true|false>] [-w background_cmd] [-i iterations]" 1>&2
     exit 1
 }
 
-while getopts ":D:m:r:w" opt; do
+while getopts ":D:m:r:w:i:" opt; do
     case "${opt}" in
         D) DURATION="${OPTARG}" ;;
         m) MLOCKALL="${OPTARG}" ;;
         r) RR="${OPTARG}" ;;
         w) BACKGROUND_CMD="${OPTARG}" ;;
+        i) ITERATIONS="${OPTARG}" ;;
         *) usage ;;
     esac
 done
@@ -57,10 +59,18 @@ background_process_start bgcmd --cmd "${BACKGROUND_CMD}"
 # test script. Catch and ignore it with trap.
 trap '' TERM
 # shellcheck disable=SC2086
-"${binary}" -q --duration "${DURATION}" ${MLOCKALL} ${RR} --json="${LOGFILE}"
+for i in $(seq ${ITERATIONS}); do
+    "${binary}" -q --duration "${DURATION}" ${MLOCKALL} ${RR} --json="${LOGFILE}-${i}.json"
+done
 
 background_process_stop bgcmd
 
 # Parse test log.
-../../lib/parse_rt_tests_results.py pi-stress "${LOGFILE}" \
-    | tee -a "${RESULT_FILE}"
+for i in $(seq ${ITERATIONS}); do
+    ../../lib/parse_rt_tests_results.py pi-stress "${LOGFILE}-${i}.json" \
+        | tee "${RESULT_FILE}"
+
+    if [ ${ITERATIONS} -ne 1 ]; then
+        sed -i "s|^|iteration-${i}-|g" "${RESULT_FILE}"
+    fi
+done
