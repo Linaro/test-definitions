1ccbfcf361d30e0b805495f6954b8ae9eb873c9f
Anders Roxell <anders.roxell@linaro.org>
2025-02-27 20:17:45 +0100

    automated: linux: signaltest: add iterations
    This will make it possible to run multiple iterations to gather multiple
    min/max/avg values.
    
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- automated/linux/signaltest/signaltest.sh
+++ automated/linux/signaltest/signaltest.sh
@@ -5,25 +5,27 @@
 . ../../lib/sh-test-lib
 
 OUTPUT="$(pwd)/output"
-LOGFILE="${OUTPUT}/signaltest.json"
+LOGFILE="${OUTPUT}/signaltest"
 RESULT_FILE="${OUTPUT}/result.txt"
 
 PRIORITY="98"
 THREADS="2"
 DURATION="1m"
 BACKGROUND_CMD=""
+ITERATIONS=1
 
 usage() {
-    echo "Usage: $0 [-r runtime] [-p priority] [-t threads] [-w background_cmd]" 1>&2
+    echo "Usage: $0 [-r runtime] [-p priority] [-t threads] [-w background_cmd] [-i iterations]" 1>&2
     exit 1
 }
 
-while getopts ":p:t:D:w:" opt; do
+while getopts ":p:t:D:w:i:" opt; do
     case "${opt}" in
         p) PRIORITY="${OPTARG}" ;;
         t) THREADS="${OPTARG}" ;;
         D) DURATION="${OPTARG}" ;;
         w) BACKGROUND_CMD="${OPTARG}" ;;
+        i) ITERATIONS="${OPTARG}" ;;
         *) usage ;;
     esac
 done
@@ -44,10 +46,18 @@ fi
 
 background_process_start bgcmd --cmd "${BACKGROUND_CMD}"
 
-"${binary}" -q -D "${DURATION}" -a -m -p "${PRIORITY}" -t "${THREADS}" --json="${LOGFILE}"
+for i in $(seq ${ITERATIONS}); do
+    "${binary}" -q -D "${DURATION}" -a -m -p "${PRIORITY}" -t "${THREADS}" --json="${LOGFILE}-${i}.json"
+done
 
 background_process_stop bgcmd
 
 # Parse test log.
-../../lib/parse_rt_tests_results.py signaltest "${LOGFILE}" \
-    | tee -a "${RESULT_FILE}"
+for i in $(seq ${ITERATIONS}); do
+    ../../lib/parse_rt_tests_results.py signaltest "${LOGFILE}-${i}.json" \
+        | tee "${RESULT_FILE}"
+
+    if [ ${ITERATIONS} -ne 1 ]; then
+        sed -i "s|^|iteration-${i}-|g" "${RESULT_FILE}"
+    fi
+done
