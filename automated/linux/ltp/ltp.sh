6ce3177fa5fed2418ffcccd338ba2b88949d4fda
Antonio Terceiro <antonio.terceiro@linaro.org>
2025-04-08 14:34:59 -0300

    automated: linux: ltp: skip logs for passing tests
    It's very unlikely that someone is going to look into passing tests.
    This helps reduce even more the demand for storage of test results.
    
    Signed-off-by: Antonio Terceiro <antonio.terceiro@linaro.org>

--- automated/linux/ltp/ltp.sh
+++ automated/linux/ltp/ltp.sh
@@ -161,9 +161,14 @@ parse_ltp_output() {
 }
 
 parse_ltp_json_results() {
+    local result
     jq -r '.results| .[]| "\(.test_fqn) \(.test.result)"'  "$1" \
         | sed 's/brok/fail/; s/conf/skip/'  >> "${RESULT_FILE}"
     for test_fqn in $(jq -r '.results| .[]| .test_fqn' "$1"); do
+      result="$(jq -r '.results | .[] | select(.test_fqn == "'"${test_fqn}"'") | .test.result' "$1")"
+      if [ "${result}" = pass ]; then
+        continue
+      fi
       jq -r '.results | .[] | select(.test_fqn == "'"${test_fqn}"'") | .test.log' "$1" > ${OUTPUT}/${test_fqn}.log
     done
 }
