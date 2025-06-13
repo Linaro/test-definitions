ab82a50ffc05ea4ae1e3ac359266302959462752
Stevan Radaković <stevan.radakovic@linaro.org>
2025-06-13 09:09:53 +0200

    Add virt environment

--- automated/linux/torizon/integration-tests.sh
+++ automated/linux/torizon/integration-tests.sh
@@ -8,10 +8,10 @@ set -x
 . ../../../../../../secrets > /dev/null 2>&1
 
 # temp hack for aarch64 geckodriver
-#curl -LO "https://github.com/mozilla/geckodriver/releases/download/v0.36.0/geckodriver-v0.36.0-linux-aarch64.tar.gz"
-#tar -xvf geckodriver-v0.36.0-linux-aarch64.tar.gz
-#mv geckodriver /usr/local/bin
-#chown root:root /usr/local/bin/geckodriver
+curl -LO "https://github.com/mozilla/geckodriver/releases/download/v0.36.0/geckodriver-v0.36.0-linux-aarch64.tar.gz"
+tar -xvf geckodriver-v0.36.0-linux-aarch64.tar.gz
+mv geckodriver /usr/local/bin
+chown root:root /usr/local/bin/geckodriver
 
 # install spire packages
 wget https://github.com/Linaro/SPIRE-CLI-S-/releases/download/0.2.0-alpha%2B006/staging-spire_0.2.0-alpha+006_linux_amd64.deb
@@ -23,6 +23,8 @@ dpkg -i staging-spire_0.2.0-alpha+019_linux_arm64.deb
 # clone baklava-integration repo and install required pip pkgs
 get_test_program "https://gitlab-ci-token:${GITLAB_TOKEN}@gitlab.com/LinaroLtd/lava/appliance/baklava-integration.git" "baklava-integration" "main"
 
+python3 -m venv venv
+source venv/bin/activate
 pip3 install -r requirements.txt
 
 export SPIRE_PAT_TOKEN LAVA_TOKEN LAVA_PASSWORD
