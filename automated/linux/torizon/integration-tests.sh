675d6e4699b87f6b0499a9e06ce965c7b81d5c96
Stevan Radaković <stevan.radakovic@linaro.org>
2025-06-16 19:55:45 +0200

    Add amd64 gecko driver installation

--- automated/linux/torizon/integration-tests.sh
+++ automated/linux/torizon/integration-tests.sh
@@ -8,17 +8,31 @@ set -x
 . ../../../../../../secrets > /dev/null 2>&1
 
 # temp hack for aarch64 geckodriver
-curl -LO "https://github.com/mozilla/geckodriver/releases/download/v0.36.0/geckodriver-v0.36.0-linux-aarch64.tar.gz"
-tar -xvf geckodriver-v0.36.0-linux-aarch64.tar.gz
+ARCH=$(uname -m)
+
+if [ "$ARCH" = "x86_64" ]; then
+    DRIVER="geckodriver-v0.36.0-linux64.tar.gz"
+    # install spire package
+    wget https://github.com/Linaro/SPIRE-CLI-S-/releases/download/0.2.0-alpha%2B006/staging-spire_0.2.0-alpha+006_linux_amd64.deb
+    dpkg -i staging-spire_0.2.0-alpha+006_linux_amd64.deb
+
+elif [ "$ARCH" = "aarch64" ] || [ "$ARCH" = "arm64" ]; then
+    DRIVER="geckodriver-v0.36.0-linux-aarch64.tar.gz"
+    # install spire package
+    wget https://github.com/Linaro/SPIRE-CLI-S-/releases/download/0.2.0-alpha%2B019/staging-spire_0.2.0-alpha+019_linux_arm64.deb
+    dpkg -i staging-spire_0.2.0-alpha+019_linux_arm64.deb
+
+else
+    echo "Unknown architecture: $ARCH"
+    exit 1
+fi
+
+# Download and install gecko driver
+curl -LO "https://github.com/mozilla/geckodriver/releases/download/v0.36.0/$DRIVER"
+tar -xvf $DRIVER
 mv geckodriver /usr/local/bin
 chown root:root /usr/local/bin/geckodriver
 
-# install spire packages
-wget https://github.com/Linaro/SPIRE-CLI-S-/releases/download/0.2.0-alpha%2B006/staging-spire_0.2.0-alpha+006_linux_amd64.deb
-dpkg -i staging-spire_0.2.0-alpha+006_linux_amd64.deb
-# also for arm64
-wget https://github.com/Linaro/SPIRE-CLI-S-/releases/download/0.2.0-alpha%2B019/staging-spire_0.2.0-alpha+019_linux_arm64.deb
-dpkg -i staging-spire_0.2.0-alpha+019_linux_arm64.deb
 
 # clone baklava-integration repo and install required pip pkgs
 get_test_program "https://gitlab-ci-token:${GITLAB_TOKEN}@gitlab.com/LinaroLtd/lava/appliance/baklava-integration.git" "baklava-integration" "main"
