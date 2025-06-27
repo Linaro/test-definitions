e19992972dffbef05db20f1fdd7ed77e19985af8
Yongqin Liu <yongqin.liu@linaro.org>
2025-06-27 12:44:33 +0800

    noninteractive-tradefed: support Android16 and the latest branches
    which requires to use JDK 21  to run the cts/vts packages.
    Otherwise the following error will be reported with JDK 17:
        java.lang.UnsupportedClassVersionError: \
            com/android/tradefed/targetprep/MultiSimPreparer has been compiled \
            by a more recent version of the Java Runtime (class file version 65.0), \
            this version of the Java Runtime only recognizes class file versions up to 61.0
    
    Tested on Android11, Android12, Android13, Android14, Android15
    and AOSP latest versions with the cts-lkft, vts-kernel tests
    
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- automated/android/noninteractive-tradefed/setup.sh
+++ automated/android/noninteractive-tradefed/setup.sh
@@ -5,22 +5,32 @@
 . ../../lib/sh-test-lib
 . ../../lib/android-test-lib
 
+function get_required_java_version() {
+    local android_version="${1}"
+    case "${android_version}" in
+        *android11*|*android12*|*android13*) echo "11" ;;
+        # use openjdk-17 for Android14+ versions
+        *android14*|*android15*|*aosp-main*) echo "17" ;;
+        # use openjdk-21 for Android16+ versions
+        *android16*|*aosp-latest*) echo "21" ;;
+        *) echo "21" ;;
+    esac
+}
 ## To enable running x86_64 binary on aarch64 host or inside container of it
 java_path_arch_str="amd64"
 if [ "X$(uname -m)" = "Xaarch64" ]; then
     java_path_arch_str="arm64"
 fi
-java_path="/usr/lib/jvm/java-11-openjdk-${java_path_arch_str}/bin/java"
-if [ -n "${ANDROID_VERSION}" ] && echo "${ANDROID_VERSION}" | grep -E -q "aosp-android14|aosp-main"; then
-    # use openjdk-17 for Android14+ versions
-    java_path="/usr/lib/jvm/java-17-openjdk-${java_path_arch_str}/bin/java"
-fi
+
+# shellcheck disable=SC2153
+java_version=$(get_required_java_version "${ANDROID_VERSION}")
+java_path="/usr/lib/jvm/java-${java_version}-openjdk-${java_path_arch_str}/bin/java"
 
 dist_name
 case "${dist}" in
     ubuntu)
         # make sure to use the right java version
-        update-alternatives --set java ${java_path}
+        update-alternatives --set java "${java_path}"
         ;;
     *)
         error_msg "Please use Ubuntu for CTS or VTS test."
