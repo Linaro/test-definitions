2eb22ecffcbfbaa34ed30e86f7afd8ea4e249674
Stevan Radaković <stevan.radakovic@linaro.org>
2025-07-03 16:10:57 +0200

    Add upgrade tests

---
+++ automated/linux/torizon/ostree-upgrade-tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $IS_CHECK -ne "True" ]; then
+    ostree admin status
+    ostree remote add $OSTREE_REMOTE_NAME https://ostree.lavacloud.io/
+    LATEST=$(ostree remote refs $OSTREE_REMOTE_NAME | grep laa-qemu | tail -n 1)
+    ostree pull $LATEST
+    ostree admin deploy --os=laa $LATEST
+    reboot
+else
+    LATEST=$(ostree remote refs $OSTREE_REMOTE_NAME | grep laa-qemu | tail -n 1 | awk -F'/' '{print $NF}')
+    ostree admin status | grep $LATEST && lava-test-case ostree-upgrade --result pass || lava-test-case ostree-upgrade --result fail
+fi
