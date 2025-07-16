dc9386401f05a2fa0493cca8dd61e1478c7bbd30
Stevan Radaković <stevan.radakovic@linaro.org>
2025-07-16 18:52:25 +0200

    Fix comparison

--- automated/linux/torizon/ostree-upgrade-tests.sh
+++ automated/linux/torizon/ostree-upgrade-tests.sh
@@ -3,14 +3,14 @@
 # Copyright (C) 2025 Linaro Ltd.
 set -x
 
-if [ $IS_CHECK != "True" ]; then
+if [ "$IS_CHECK" = "True" ]; then
+    LATEST=$(ostree remote refs $OSTREE_REMOTE_NAME | grep $OSTREE_REF | tail -n 1 | awk -F'/' '{print $NF}')
+    ostree admin status | grep $LATEST && lava-test-case ostree-upgrade --result pass || lava-test-case ostree-upgrade --result fail
+else
     ostree admin status
     ostree remote add $OSTREE_REMOTE_NAME https://ostree.lavacloud.io/
     LATEST=$(ostree remote refs $OSTREE_REMOTE_NAME | grep laa-qemu | tail -n 1)
     ostree pull $LATEST
     ostree admin deploy --os=laa $LATEST
     reboot
-else
-    LATEST=$(ostree remote refs $OSTREE_REMOTE_NAME | grep $OSTREE_REF | tail -n 1 | awk -F'/' '{print $NF}')
-    ostree admin status | grep $LATEST && lava-test-case ostree-upgrade --result pass || lava-test-case ostree-upgrade --result fail
 fi
