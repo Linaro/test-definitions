4bcb0ec3df5222caadc9845a42c7cb77feda7f81
Stevan Radaković <stevan.radakovic@linaro.org>
2025-07-19 14:52:08 +0200

    Fix latest build fetch for laa

--- automated/linux/torizon/ostree-upgrade-tests.sh
+++ automated/linux/torizon/ostree-upgrade-tests.sh
@@ -4,15 +4,14 @@
 set -x
 
 if [ "$IS_CHECK" = "True" ]; then
-    LATEST=$(ostree remote refs $OSTREE_REMOTE_NAME | grep $OSTREE_REF | grep -v dev | tail -n 1 | awk -F'/' '{print $NF}')
+    LATEST=$(ostree remote refs $OSTREE_REMOTE_NAME | grep $OSTREE_REF/ | grep -v dev | tail -n 1 | awk -F'/' '{print $NF}')
     ostree admin status | grep $LATEST && lava-test-case ostree-upgrade --result pass || lava-test-case ostree-upgrade --result fail
 else
     ostree admin status
     ostree remote add $OSTREE_REMOTE_NAME https://ostree.lavacloud.io/
-    LATEST=$(ostree remote refs $OSTREE_REMOTE_NAME | grep laa-qemu | grep -v dev | tail -n 1)
+    LATEST=$(ostree remote refs $OSTREE_REMOTE_NAME | grep $OSTREE_REF/ | grep -v dev | tail -n 1)
     ostree pull $LATEST
     ostree admin deploy --os=laa $LATEST
-#    reboot
 fi
 
 exit 0
