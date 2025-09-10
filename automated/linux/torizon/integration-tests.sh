bd02f06fd07770d62f27e0912dcd73ccc7f41374
Pawel Szymaszek <pawel.szymaszek@linaro.org>
2025-09-10 15:18:28 +0200

    automated: linux: torizon: send output.xml to fileserver for further processing
    In order to create a robotframework dashboard for laa remote testing, we need an output.xml file.
    One way to extract it from lava is to upload it to the fileserver using the parse-robot-framework.py script.
    
    - fix black linting errors
    - code adjustments after review
    - remove scp from parse script, let baklava-integration handle it

--- automated/linux/torizon/integration-tests.sh
+++ automated/linux/torizon/integration-tests.sh
@@ -43,7 +43,7 @@ python3 -m venv venv
 . venv/bin/activate
 pip3 install -r requirements.txt
 
-export SPIRE_PAT_TOKEN LAVA_TOKEN LAVA_PASSWORD
+export SPIRE_PAT_TOKEN LAVA_TOKEN LAVA_PASSWORD SSH_KEY
 
 # run tests
 robot --pythonpath . --exclude gitlab_pipeline --variable remote:"$IS_REMOTE" --outputdir=.. test/
