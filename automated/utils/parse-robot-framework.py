f30c5fedd94579f662a94e3c4f0cfd792803b299
Pawel Szymaszek <pawel.szymaszek@linaro.org>
2025-09-22 14:12:11 +0200

    automated: linux: torizon: integration: drop scp upload
    - Following the file transfer method change, all unnecessary code
      related to SSH has now been deleted
    
    Signed-off-by: Pawel Szymaszek <pawel.szymaszek@linaro.org>

--- automated/utils/parse-robot-framework.py
+++ automated/utils/parse-robot-framework.py
@@ -2,15 +2,9 @@
 # SPDX-License-Identifier: GPL-2.0-only
 # Copyright (C) 2024 Linaro Ltd.
 
-import io
-import os
 import argparse
 
-from datetime import datetime
-
-from paramiko import SSHClient, AutoAddPolicy, RSAKey
 from robot.api import ExecutionResult, ResultVisitor
-from scp import SCPClient
 
 
 def parse_args():
@@ -26,39 +20,8 @@ def parse_args():
     return args
 
 
-def send_output(result_file):
-    print("connecting to ssh server...")
-    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-    server = os.getenv("SSH_SERVER")
-    username = os.getenv("SSH_USERNAME")
-    pkey = RSAKey.from_private_key(io.StringIO(os.getenv("SSH_KEY")))
-
-    with SSHClient() as ssh:
-        try:
-            ssh.set_missing_host_key_policy(AutoAddPolicy())
-            ssh.connect(hostname=server, username=username, pkey=pkey)
-        except Exception as ex:
-            print(
-                f"Failed to make SSH connection to server due to the following exception: {ex}"
-            )
-
-        with SCPClient(ssh.get_transport()) as scp:
-            try:
-                scp.put(
-                    result_file,
-                    f"~/public_html/remote_robot_dash/output-{timestamp}.xml",
-                )
-                print(f"file {result_file} sent to fileserver")
-            except Exception as ex:
-                print(
-                    f"Failed to send {result_file} to server due to the following exception: {ex}"
-                )
-
-
 def main():
     result = ExecutionResult(args.result_file)
-
-    send_output(args.result_file)
 
     def get_all_tests(suite):
         for test in suite.tests:
